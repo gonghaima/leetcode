7c6216bb8a5aa186317e32ebaf8951b03bf8a27c
Steven Gong <steven.gong@iag.com.au>
2018-11-23 01:30:58 +1100

    isSymmetric worked for test data - todo: verify with leetcode

--- src/rules/binaryTree/isSymmetric/index.js
+++ src/rules/binaryTree/isSymmetric/index.js
@@ -1,50 +1,73 @@
-// export const isSymmetric = function(root) {
-//   if (root && root.left && root.right && root.left === root.right) {
-//     isSymmetric(root.left);
-//     isSymmetric(root.right);
-//   } else {
-//     return false;
-//   }
-
-//   return true;
-// };
-
 export const isSymmetric = function(root) {
   const startRoot = [root];
   let isSym = true;
 
   const compareTwoNode = (node1, node2) => {
-    if (
-      node1.left &&
-      node2.right &&
-      node1.left.val === node2.right.val &&
-      (node1.right && node2.left && node1.right.val === node2.left.val)
-    ) {
-    } else {
+    //compare two nodes
+    if (node1.val !== node2.val) {
       isSym = false;
+      return;
+    }
+    if (
+      (node1 && node1.left && node1.left.val) !==
+      (node2 && node2.right && node2.right.val)
+    ) {
+      isSym = false;
+      return;
+    }
+    if (
+      (node1 && node1.right && node1.right.val) !==
+      (node2 && node2.left && node2.left.val)
+    ) {
+      isSym = false;
+      return;
+    }
+    if (
+      ((node1 && node1.left && node1.left.val) === undefined ||
+        (node1 && node1.left && node1.left.val) === null) &&
+      (node2 && node2.right && node2.right.value)
+    ) {
+      isSym = false;
+      return;
+    }
+    if (
+      node1 &&
+      node1.left &&
+      node1.left.value &&
+      ((node1 && node1.right && node1.right.val === undefined) ||
+        (node1 && node1.right && node1.right.val === null))
+    ) {
+      isSym = false;
+      return;
     }
   };
 
   const checkSym = nodeList => {
-    //if only one in list
-    if (
-      nodeList.length === 1 &&
-      nodeList[0] &&
-      nodeList[0].left &&
-      nodeList[0].right &&
-      nodeList[0].left === nodeList[0].right
-    ) {
-    } else {
-      isSym = false;
-      return;
-    }
+    // if no sym, return
+    if (isSym === false) return;
 
+    let nodeLstCopy = nodeList.slice();
     while (nodeList.length > 1 && isSym === true) {
       const nodeFirst = nodeList.shift();
       const nodeLast = nodeList.pop();
-      isSym = compareTwoNode(nodeFirst, nodeLast);
+      compareTwoNode(nodeFirst, nodeLast);
+    }
+    if (isSym === true) {
+      let childNodeList = [];
+      nodeLstCopy.map(c => {
+        if (c.left) childNodeList.push(c.left);
+        if (c.right) childNodeList.push(c.right);
+      });
+      if (childNodeList.length > 0) checkSym(childNodeList);
     }
   };
+  //if only one in list
+  if (root && !root.left && !root.right) {
+    isSym = true;
+    return isSym;
+  }
 
-  return true;
+  checkSym([root.left, root.right]);
+
+  return isSym;
 };

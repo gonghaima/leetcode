81766672aaeedb346b21e956fc3287e7bba31bff
Steven Gong <steven.gong@iag.com.au>
2019-02-27 21:35:03 +1100

    Longest Common Prefix

---
+++ test/mock/longestCommonPrefix.Data.js
@@ -0,0 +1,19 @@
+export const inputData = {
+  case1: ['flower', 'flow', 'flight'],
+  case2: [],
+  case3: ['a'],
+  case4: ['c', 'c'],
+  case5: ['dog', 'racecar', 'car'],
+  case6: ['aa', 'aa']
+
+  // adcba
+};
+
+export const expected = {
+  case1: 'fl',
+  case2: '',
+  case3: 'a',
+  case4: 'c',
+  case5: '',
+  case6: 'aa'
+};
---
+++ test/rules/longestCommonPrefix.test.js
@@ -0,0 +1,10 @@
+import longestCommonPrefix from '../../src/rules/longestCommonPrefix';
+import { inputData, expected } from '../mock/longestCommonPrefix.Data';
+test('longestCommonPrefix function should return longest prefix from an array of strings', () => {
+  expect(longestCommonPrefix(inputData.case1)).toBe(expected.case1);
+  expect(longestCommonPrefix(inputData.case2)).toBe(expected.case2);
+  expect(longestCommonPrefix(inputData.case3)).toBe(expected.case3);
+  expect(longestCommonPrefix(inputData.case4)).toBe(expected.case4);
+  expect(longestCommonPrefix(inputData.case5)).toBe(expected.case5);
+  expect(longestCommonPrefix(inputData.case6)).toBe(expected.case6);
+});

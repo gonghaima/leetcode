44e02307ebb0b277716c2a5fd05dd5e1b7d72a2f
Steven Gong <steven.gong@iag.com.au>
2019-03-03 07:49:40 +1100

    hackerrank warm-up challenge 1

---
+++ test/mock/sockMerchantData.js
@@ -0,0 +1,7 @@
+export const inputData = {
+  case1: { n: 7, ar: [1, 2, 1, 2, 1, 3, 2] }
+};
+
+export const expected = {
+  case1: 2
+};
---
+++ test/rules/sockMerchant.test.js
@@ -0,0 +1,7 @@
+import sockMerchant from '../../src/rules/sockMerchant';
+import { inputData, expected } from '../mock/sockMerchantData';
+test('sockMerchant function should return correct pairs of sock from case1 data', () => {
+  expect(sockMerchant(inputData.case1.n, inputData.case1.ar)).toBe(
+    expected.case1
+  );
+});

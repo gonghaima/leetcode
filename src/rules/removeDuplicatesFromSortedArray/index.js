cbaab1e34cc26750dd221704cb6e48e4bdda8feb
Steven <gonghaima@hotmail.com>
2019-03-26 09:46:54 +1100

    removeDuplicatesFromSortedArray-fix: the num need to be referenced

--- src/rules/removeDuplicatesFromSortedArray/index.js
+++ src/rules/removeDuplicatesFromSortedArray/index.js
@@ -3,13 +3,16 @@ export default nums => {
     (i, c) => {
       if (c !== i.prevItem) {
         i.counter += 1;
+        i.allUnique.push(c);
         i.prevItem = c;
         return i;
       } else {
         return i;
       }
     },
-    { prevItem: null, counter: 0 }
+    { prevItem: null, counter: 0, allUnique: [] }
   );
+    nums.splice(0)
+  result.allUnique.map(n=>nums.push(n))
   return result.counter;
 };

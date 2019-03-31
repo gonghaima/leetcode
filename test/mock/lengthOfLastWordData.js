7fc1dca32c189156bd0d1ae2050aec2273e99101
Steven <gonghaima@hotmail.com>
2019-04-01 10:47:26 +1100

    lengthOfLastWord - acceptable answer

--- test/mock/lengthOfLastWordData.js
+++ test/mock/lengthOfLastWordData.js
@@ -1,5 +1,8 @@
 export const inputData = {
-  case1: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+  case1: "Hello World",
+  case2: "a",
+  case3: " ",
+  case4: "a "
 };
 
-export const expected = { case1: 5 };
+export const expected = { case1: 5, case2: 1, case3: 0, case4: 1 };
--- test/rules/lengthOfLastWord.test.js
+++ test/rules/lengthOfLastWord.test.js
@@ -1,5 +1,8 @@
 import lengthOfLastWord from "../../src/rules/lengthOfLastWord";
 import { inputData, expected } from "../mock/lengthOfLastWordData";
-test("lengthOfLastWord function should return correct sum result from case1 data", () => {
+test("lengthOfLastWord function should return correct sum result from cases data", () => {
   expect(lengthOfLastWord(inputData.case1)).toBe(expected.case1);
+  expect(lengthOfLastWord(inputData.case2)).toBe(expected.case2);
+  expect(lengthOfLastWord(inputData.case3)).toBe(expected.case3);
+  expect(lengthOfLastWord(inputData.case4)).toBe(expected.case4);
 });

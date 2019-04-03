e31bb18d5ab601bfc7927c0a0c123aa6831c4b6c
Steven <gonghaima@hotmail.com>
2019-04-04 01:21:34 +1100

    addBinary - initial setup

---
+++ test/mock/addBinaryData.js
@@ -0,0 +1,9 @@
+export const inputData = {
+  case1: { a: "11", b: "1" },
+  case2: { a: "1010", b: "1011" }
+};
+
+export const expected = {
+  case1: "100",
+  case2: "10101"
+};
---
+++ test/rules/addBinary.test.js
@@ -0,0 +1,10 @@
+import addBinary from "../../src/rules/addBinary";
+import { inputData, expected } from "../mock/addBinaryData";
+test("addBinary function should calculate correct result from cases data", () => {
+  expect(addBinary(inputData.case1.a, inputData.case1.b)).toEqual(
+    expected.case1
+  );
+  expect(addBinary(inputData.case2.a, inputData.case2.b)).toEqual(
+    expected.case2
+  );
+});

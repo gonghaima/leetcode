77f9ef77ef40f29b89d9ea9b66199c8f14ee9e02
Steven <gonghaima@hotmail.com>
2019-04-04 16:21:23 +1100

    addBinary - time-95.16% space-28.20%

--- src/rules/addBinary/index.js
+++ src/rules/addBinary/index.js
@@ -1,3 +1,62 @@
 export default (a, b) => {
-  return "";
+  const aLength = a.length;
+
+  const bLength = b.length;
+
+  let longerString;
+
+  let shorterString;
+
+  let reservedVal = 0;
+  let result = "";
+
+  if (aLength > bLength) {
+    longerString = Object.assign(a);
+    longerStringLength = aLength;
+    shorterString = Object.assign(b);
+    shorterStringLength = bLength;
+  } else {
+    longerString = Object.assign(b);
+    longerStringLength = bLength;
+    shorterString = Object.assign(a);
+    shorterStringLength = aLength;
+  }
+
+  let longerStringHolder = Object.assign(longerString);
+  let shorterStringHolder = Object.assign(shorterString);
+
+  const lastOne = s => s.substring(s.length - 1);
+  const remaining = s => s.substring(0, s.length - 1);
+
+  while (longerStringHolder.length !== 0) {
+    const lL = lastOne(longerStringHolder);
+    const lR = remaining(longerStringHolder);
+    longerStringHolder = lR;
+
+    const sL = lastOne(shorterStringHolder);
+    const sR = remaining(shorterStringHolder);
+    shorterStringHolder = sR;
+
+    let cTotal;
+    if (sL !== "") {
+      cTotal = Number(lL) + Number(sL) + reservedVal;
+    } else {
+      cTotal = Number(lL) + reservedVal;
+    }
+    if (cTotal === 0) {
+      result = "0" + result;
+      reservedVal = 0;
+    } else if (cTotal === 1) {
+      result = "1" + result;
+      reservedVal = 0;
+    } else if (cTotal === 2) {
+      result = "0" + result;
+      reservedVal = 1;
+    } else if (cTotal === 3) {
+      result = "1" + result;
+      reservedVal = 1;
+    }
+  }
+  if (reservedVal === 1) result = "1" + result;
+  return result;
 };

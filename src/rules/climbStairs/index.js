a4ba885f1730064e7da08f4473719448bc6c1986
Steven <gonghaima@hotmail.com>
2019-04-06 18:45:30 +1100

    claimb Stairs - works only on small dataset, time/space to be improved- WIP

--- src/rules/climbStairs/index.js
+++ src/rules/climbStairs/index.js
@@ -1,3 +1,41 @@
 export default n => {
-  return 999;
+  let containerArr = [];
+  containerArr.push(n - 1);
+  containerArr.push(n - 2);
+  let counter = 0;
+  let carryOn = true;
+  while (carryOn) {
+    let tempArr = [];
+    for (let i = 0; i < containerArr.length; i++) {
+      const element = containerArr[i];
+      if (element - 1 === 0) {
+        counter += 1;
+      } else if (element - 2 === 0) {
+        counter += 1;
+        tempArr.push(element - 1);
+      } else {
+        tempArr.push(element - 1);
+        tempArr.push(element - 2);
+      }
+    }
+    counter += 1;
+  }
 };
+
+// export default n => {
+//   let counter = 0;
+//   const keepCal = nm => {
+//     if (nm === 0) {
+//       counter += 1;
+//       return 0;
+//     } else if (nm <= 0) {
+//       return 0;
+//     }
+//     while (nm >= 0) {
+//       return keepCal(nm - 2) + keepCal(nm - 1);
+//     }
+//   };
+
+//   keepCal(n);
+//   return counter;
+// };
--- test/mock/climbStairsData.js
+++ test/mock/climbStairsData.js
@@ -1,9 +1,13 @@
 export const inputData = {
   case1: 2,
-  case2: 3
+  case2: 3,
+  case3: 4,
+  case4: 44
 };
 
 export const expected = {
   case1: 2,
-  case2: 3
+  case2: 3,
+  case3: 5,
+  case4: 5555
 };

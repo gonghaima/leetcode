3bda9bf33910bc3570e2dcb7f6c210b6f25281e0
Steven <gonghaima@hotmail.com>
2019-04-10 15:43:33 +1000

    renaming - initial draft

--- src/rules/binaryTree/levelorderTraversal2/index.js
+++ src/rules/binaryTree/levelorderTraversal2/index.js
@@ -1,3 +1,15 @@
 export default root => {
-  return null;
+  if (!root) return [];
+  let allArr = [];
+  let currentNodeList = [root];
+  while (currentNodeList.length > 0) {
+    let tempArr = [];
+    allArr.map(i => {
+      if (i.left) tempArr.push(i.left);
+      if (i.right) tempArr.push(i.right);
+      allArr.unshift(i.val);
+    });
+    currentNodeList = tempArr;
+  }
+  return allArr;
 };

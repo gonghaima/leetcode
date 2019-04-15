a9f4672dc4d61263244f3b5469bb54bea1a23648
Steven <gonghaima@hotmail.com>
2019-04-15 21:32:15 +1000

    solution1- time 43.85%, space 49.71%

---
+++ src/rules/listNode/linkedListCycle/solution1.js
@@ -0,0 +1,15 @@
+export default head => {
+  if (!head || !head.next) return false;
+
+  let slow = head;
+  let fast = head.next;
+
+  while (slow !== fast) {
+    if (fast === null || fast.next === null) {
+      return false;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return true;
+};
--- test/rules/ListNode/linkedListCycle.test.js
+++ test/rules/ListNode/linkedListCycle.test.js
@@ -1,7 +1,14 @@
 import linkedListCycle from "../../../src/rules/listNode/linkedListCycle";
+import solution1 from "../../../src/rules/listNode/linkedListCycle/solution1";
 import { inputData, expected } from "../../mock/listNode/linkedListCycleData";
 test("linkedListCycle function should determine if there is a cycle", () => {
   expect(linkedListCycle(inputData.case1)).toEqual(expected.case1);
   expect(linkedListCycle(inputData.case2)).toEqual(expected.case2);
   expect(linkedListCycle(inputData.case3)).toEqual(expected.case3);
+});
+
+test("solution1 function should determine if there is a cycle", () => {
+  expect(solution1(inputData.case1)).toEqual(expected.case1);
+  expect(solution1(inputData.case2)).toEqual(expected.case2);
+  expect(solution1(inputData.case3)).toEqual(expected.case3);
 });

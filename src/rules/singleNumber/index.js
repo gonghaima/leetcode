011a5cfb364ccd94730e855f7f1f5ed547a5b0a4
Steven <gonghaima@hotmail.com>
2019-04-16 10:54:44 +1000

    singleNumber  - works on test data

--- src/rules/singleNumber/index.js
+++ src/rules/singleNumber/index.js
@@ -1,3 +1,18 @@
 export default nums => {
-  return 0;
+  let aggregatedNums = nums.reduce((i, c) => {
+    if (!i) {
+      i = {};
+      i.c = 1;
+      return i;
+    }
+    if (!i[c]) {
+      i[c] = 1;
+    } else {
+      i[c] += 1;
+    }
+  });
+  const result = Object.entries(aggregatedNums).filter(item => {
+    return item[1] === 1;
+  });
+  return result;
 };
--- test/rules/singleNumber.test.js
+++ test/rules/singleNumber.test.js
@@ -1,7 +1,12 @@
 import singleNumber from "../../src/rules/singleNumber";
 import { inputData, expected } from "../mock/singleNumberData";
 
-test("singleNumber should check a number", () => {
+test("singleNumber should check a number for case 1", () => {
   const result = singleNumber(inputData.case1);
   expect(result).toBe(expected.case1);
+});
+
+test("singleNumber should check a number for case 2", () => {
+  const result = singleNumber(inputData.case2);
+  expect(result).toBe(expected.case2);
 });

d460e8d5c094627101174b4b7fbcf462ac0aaad6
Steven <gonghaima@hotmail.com>
2019-04-16 11:40:44 +1000

    solution1 - time 99.03%, space 73.63%

--- test/rules/singleNumber.test.js
+++ test/rules/singleNumber.test.js
@@ -1,4 +1,5 @@
 import singleNumber from "../../src/rules/singleNumber";
+import solution1 from "../../src/rules/singleNumber/solution1";
 import { inputData, expected } from "../mock/singleNumberData";
 
 test("singleNumber should check a number for case 1", () => {
@@ -8,5 +9,15 @@ test("singleNumber should check a number for case 1", () => {
 
 test("singleNumber should check a number for case 2", () => {
   const result = singleNumber(inputData.case2);
+  expect(result).toBe(expected.case2);
+});
+
+test("solution1 should check a number for case 1", () => {
+  const result = solution1(inputData.case1);
+  expect(result).toBe(expected.case1);
+});
+
+test("solution1 should check a number for case 2", () => {
+  const result = solution1(inputData.case2);
   expect(result).toBe(expected.case2);
 });

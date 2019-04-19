a4b3f544ecd6ee1ffcc1dd59cf645fecc8954df0
Steven <gonghaima@hotmail.com>
2019-04-19 15:09:01 +1000

    time 15%, space 11%

--- src/rules/pascalsTriangleII/index.js
+++ src/rules/pascalsTriangleII/index.js
@@ -1,15 +1,17 @@
 export default rowIndex => {
   let result = [];
+  let prevArr = [];
   //level
   for (let i = 0; i <= rowIndex; i++) {
     //items in each level
     let curArr = [];
     for (let k = 0; k < i + 1; k++) {
-      const val1 = (result[i - 1] && result[i - 1][k]) || 0;
-      const val2 = (result[i - 1] && result[i - 1][k - 1]) || 0;
+      const val1 = (prevArr && prevArr[k]) || 0;
+      const val2 = (prevArr && prevArr[k - 1]) || 0;
       curArr.push(val1 + val2 || 1);
     }
-    result.push(curArr);
+    prevArr = curArr;
+    if (i === rowIndex) return curArr;
   }
-  return result[rowIndex];
+  return [];
 };

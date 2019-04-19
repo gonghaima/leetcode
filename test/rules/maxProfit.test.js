857e94b468d2a2dd266d5c1bad71f650196c1831
Steven <gonghaima@hotmail.com>
2019-04-19 15:21:28 +1000

    test description update

--- test/rules/maxProfit.test.js
+++ test/rules/maxProfit.test.js
@@ -1,12 +1,12 @@
 import maxProfit from "../../src/rules/maxProfit";
 import { inputData, expected } from "../mock/maxProfitData";
 
-test("maxProfit should check a number in case1", () => {
+test("maxProfit should return max profit from a given list", () => {
   const result = maxProfit(inputData.case1);
   expect(result).toBe(expected.case1);
 });
 
-test("maxProfit should check a number in case2", () => {
+test("maxProfit should return 0, when there is no transaction available", () => {
   const result = maxProfit(inputData.case2);
   expect(result).toBe(expected.case2);
 });

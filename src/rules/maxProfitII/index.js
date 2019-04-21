150b5a4f2a8ac5d9e70dd8a7ae4757179b96d055
Steven <gonghaima@hotmail.com>
2019-04-22 05:43:13 +1000

    works on test data

--- src/rules/maxProfitII/index.js
+++ src/rules/maxProfitII/index.js
@@ -1,3 +1,44 @@
+/** the idea behind is that
+ * find the lowest price from the start point, then slice it to there.
+ * then, find the lowest price from start point, then slice it to there.
+ * calculate diff between high and low, add to total if greater than 0
+ *
+ * keep the above operation until no items in array
+ */
 export default prices => {
-  return 0;
+  let curA = prices.slice();
+  let total = 0;
+  // a = array,  p - position
+  const findLowest = a => {
+    if (a[0] >= a[1]) {
+      return findLowest(a.slice(1));
+    } else {
+      return a;
+    }
+  };
+
+  const findHighest = a => {
+    if (a[0] <= a[1]) {
+      return findHighest(a.slice(1));
+    } else {
+      return a;
+    }
+  };
+
+  while (curA.length > 0) {
+    curA = findLowest(curA);
+    const alVal = curA[0];
+
+    curA = curA.slice(1);
+    curA = findHighest(curA);
+    const ahval = curA[0];
+
+    const diff = ahval - alVal;
+    if (diff > 0) {
+      total += diff;
+    }
+    curA = curA.slice(1);
+  }
+
+  return total;
 };

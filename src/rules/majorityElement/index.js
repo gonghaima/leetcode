166865344c0e23837dbaebeca49060da486f6ba7
Steven <gonghaima@hotmail.com>
2019-04-30 13:21:03 +1000

    works - time 98.66%, space 97.09%

--- src/rules/majorityElement/index.js
+++ src/rules/majorityElement/index.js
@@ -1,3 +1,12 @@
 export default nums => {
-  return 999;
+  const stats = nums.reduce((acc, val) => {
+    acc[val] = (acc[val] || 0) + 1;
+    return acc;
+  }, {});
+
+  const result = Object.entries(stats).reduce((acc, val) => {
+    if (!acc) return val;
+    return acc[1] > val[1] ? acc : val;
+  });
+  return Number(result[0]);
 };

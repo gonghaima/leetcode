8f91679caa8d9aa12f961d9eb91a182556a8c7cd
Steven <gonghaima@hotmail.com>
2019-05-05 14:15:55 +1000

    unit test pass

--- src/rules/countPrimes/index.js
+++ src/rules/countPrimes/index.js
@@ -1,3 +1,17 @@
 export default n => {
-  return 999;
+  let counter = 0;
+  let finalResult = [];
+  if (n === 0 || n === 1) return 0;
+  if (n === 2) return 1;
+  if (n === 3) return 2;
+
+  counter += 2;
+  for (let i = 4; i <= n; i++) {
+    let isP = true;
+    for (let k = 2; k < i && isP === true; k++) {
+      if (i % k === 0) isP = false;
+    }
+    if (isP === true) finalResult.push(i);
+  }
+  return finalResult.length + 2;
 };

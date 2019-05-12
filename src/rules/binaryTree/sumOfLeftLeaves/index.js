cba243ece2d18162b3fdd0297322f02ed1d8c835
Steven <gonghaima@hotmail.com>
2019-05-12 18:46:21 +1000

    works on test data

--- src/rules/binaryTree/sumOfLeftLeaves/index.js
+++ src/rules/binaryTree/sumOfLeftLeaves/index.js
@@ -1,3 +1,17 @@
 export const sumOfLeftLeaves = function(root) {
-  return 999;
+  let sum = 0;
+  let currentNodes = [];
+  currentNodes.push(root);
+  while (currentNodes.length !== 0) {
+    let tmNodes = [];
+    currentNodes.map(cNode => {
+      if (cNode.left && cNode.left.left === null && cNode.left.right === null) {
+        sum += cNode.left.val;
+      }
+      if (cNode.left) tmNodes.push(cNode.left);
+      if (cNode.right) tmNodes.push(cNode.right);
+    });
+    currentNodes = tmNodes;
+  }
+  return sum;
 };

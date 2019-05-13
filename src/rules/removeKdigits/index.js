49309ebdf9836f886b52adbef29127f497898c6f
Steven <gonghaima@hotmail.com>
2019-05-13 16:23:59 +1000

    works on test data

--- src/rules/removeKdigits/index.js
+++ src/rules/removeKdigits/index.js
@@ -1,3 +1,57 @@
 export default (num, k) => {
-  return "9999";
+  const nums = num.split("");
+
+  /**get all combination util start */
+  var Util = function() {};
+
+  Util.getCombinations = function(array, size, start, initialStuff, output) {
+    if (initialStuff.length >= size) {
+      output.push(initialStuff);
+    } else {
+      var i;
+
+      for (i = start; i < array.length; ++i) {
+        Util.getCombinations(
+          array,
+          size,
+          i + 1,
+          initialStuff.concat(array[i]),
+          output
+        );
+      }
+    }
+  };
+
+  Util.getAllPossibleCombinations = function(array, size, output) {
+    Util.getCombinations(array, size, 0, [], output);
+  };
+
+  /**get all combination util end */
+
+  let combinations = [];
+  Util.getAllPossibleCombinations(nums, 4, combinations);
+
+  //combile and remove leading zero
+  const formatNum = arr => {
+    let res = [];
+    let leading = true;
+    arr.map(i => {
+      if (i === "0" && leading === true) {
+      } else {
+        leading = false;
+        res.push(i);
+      }
+    });
+    return res;
+  };
+
+  const result = combinations.reduce((i, c) => {
+    const cNum = Number(formatNum(c).join(""));
+    if (!i) {
+      i = cNum;
+    }
+    i = i > cNum ? cNum : i;
+    return i;
+  }, 0);
+  return result.toString();
 };
--- test/mock/removeKdigitsData.js
+++ test/mock/removeKdigitsData.js
@@ -1,7 +1,7 @@
 export const inputData = {
   case1: { num: "1432219", k: 3 },
   case2: { num: "10200", k: 1 },
-  case2: { num: "10", k: 2 }
+  case3: { num: "10", k: 2 }
 };
 
 export const expected = { case1: "1219", case2: "200", case3: "0" };

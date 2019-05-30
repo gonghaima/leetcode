8f4980668c984a987e45fe8fbc15dfe7be02d7bd
Steven <gonghaima@hotmail.com>
2019-05-31 02:14:42 +1000

    works on test case

--- src/rules/reconstructQueue/index.js
+++ src/rules/reconstructQueue/index.js
@@ -1,3 +1,46 @@
 export default people => {
-  return [[2]];
+  let resultArr = new Array(people.length);
+
+  let currentData = { minItem: {}, remaining: people };
+
+  const retrieveMin = arr =>
+    arr.reduce((acc, [h, k], idx, src) => {
+      if (!acc) return { minItem: { ix: idx, item: [h, k] }, remaining: [] };
+      if (acc.minItem.item[0] > h) {
+        acc.remaining.push(acc.minItem.item);
+        acc.minItem = { ix: idx, item: [h, k] };
+        return acc;
+      } else {
+        acc.remaining.push([h, k]);
+        return acc;
+      }
+    }, null);
+
+  while (currentData.remaining.length > 0) {
+    const tmp = retrieveMin(currentData.remaining);
+    let counter = 0;
+    let actualIdx = 0;
+
+    //find the postion
+    while (actualIdx < tmp.minItem.item[1]) {
+      if (
+        !resultArr[counter] ||
+        (resultArr[counter] && resultArr[counter][0] === tmp.minItem.item[0])
+      ) {
+        counter += 1;
+        actualIdx += 1;
+      } else {
+        counter += 1;
+      }
+    }
+
+    //skip defined position
+    while (resultArr[counter]) {
+      counter += 1;
+    }
+    resultArr[counter] = tmp.minItem.item;
+    currentData = tmp;
+  }
+
+  return resultArr;
 };

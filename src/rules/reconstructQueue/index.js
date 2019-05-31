57a27a3c8d4b39105d533dd64742b7f529f587a0
Steven <gonghaima@hotmail.com>
2019-05-31 13:00:12 +1000

    better solution - time 99.31%, space 87.26%

--- src/rules/reconstructQueue/index.js
+++ src/rules/reconstructQueue/index.js
@@ -1,46 +1,10 @@
 export default people => {
-  let resultArr = new Array(people.length);
+  people.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]));
+  //sort: decrease by height, if equal height increase by k
 
-  let currentData = { minItem: {}, remaining: people };
-
-  const retrieveMin = arr =>
-    arr.reduce((acc, [h, k], idx, src) => {
-      if (!acc) return { minItem: { ix: idx, item: [h, k] }, remaining: [] };
-      if (acc.minItem.item[0] > h) {
-        acc.remaining.push(acc.minItem.item);
-        acc.minItem = { ix: idx, item: [h, k] };
-        return acc;
-      } else {
-        acc.remaining.push([h, k]);
-        return acc;
-      }
-    }, null);
-
-  while (currentData.remaining.length > 0) {
-    const tmp = retrieveMin(currentData.remaining);
-    let counter = 0;
-    let actualIdx = 0;
-
-    //find the postion
-    while (actualIdx < tmp.minItem.item[1]) {
-      if (
-        !resultArr[counter] ||
-        (resultArr[counter] && resultArr[counter][0] === tmp.minItem.item[0])
-      ) {
-        counter += 1;
-        actualIdx += 1;
-      } else {
-        counter += 1;
-      }
-    }
-
-    //skip defined position
-    while (resultArr[counter]) {
-      counter += 1;
-    }
-    resultArr[counter] = tmp.minItem.item;
-    currentData = tmp;
+  let result = [];
+  for (let i = 0; i < people.length; i++) {
+    result.splice(people[i][1], 0, people[i]); //insert, people[i][1] means the position to insert
   }
-
-  return resultArr;
+  return result;
 };

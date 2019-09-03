ecb9caa8ace9e785465cc9e2ac06b94cdca8cbb3
Steven <gonghaima@hotmail.com>
2019-09-04 09:23:14 +1000

    wiggleMaxLength - works on test data

--- test/mock/wiggleMaxLengthData.js
+++ test/mock/wiggleMaxLengthData.js
@@ -1,3 +1,23 @@
+export const wiggle_inputData = {
+  case1: [1, 7, 4, 9, 2, 5],
+  case2: [1, 17, 5, 10, 13, 15, 10, 5, 16, 8],
+  case3: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+};
+
+export const wiggle_expected = {
+  case1: true,
+  case2: false,
+  case3: false
+};
+
+export const sub_comb_inputData = {
+  case1: { data: [1, 7, 4, 9, 2, 5], lt: 5 }
+};
+
+export const sub_comb_expected = {
+  case1: [[1, 7, 4, 9, 2], [7, 4, 9, 2, 5]]
+};
+
 export const inputData = {
   case1: [1, 7, 4, 9, 2, 5],
   case2: [1, 17, 5, 10, 13, 15, 10, 5, 16, 8],
--- test/rules/wiggleMaxLength.test.js
+++ test/rules/wiggleMaxLength.test.js
@@ -1,5 +1,17 @@
-import wiggleMaxLength from "../../src/rules/wiggleMaxLength";
-import { inputData, expected } from "../mock/wiggleMaxLengthData";
+import wiggleMaxLength, { isWiggle } from "../../src/rules/wiggleMaxLength";
+import {
+  inputData,
+  expected,
+  wiggle_inputData,
+  wiggle_expected,
+  sub_comb_inputData,
+  sub_comb_expected
+} from "../mock/wiggleMaxLengthData";
+
+// test("wiggle util should return true from a valid wiggle list", () => {
+//   const result = isWiggle(wiggle_inputData.case1);
+//   expect(result).toBe(wiggle_expected.case1);
+// });
 
 test("wiggleMaxLength should return max subsequences from a given list 1", () => {
   const result = wiggleMaxLength(inputData.case1);

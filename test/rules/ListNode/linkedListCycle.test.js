01892fa447ed409b9a0565b3c39ccd7bcb3d27c3
Steven <gonghaima@hotmail.com>
2019-11-29 20:07:59 +1100

    rename it back

--- test/rules/ListNode/linkedListCycle.test.js
+++ test/rules/ListNode/linkedListCycle.test.js
@@ -1,6 +1,6 @@
 import linkedListCycle from "../../../src/rules/listNode/linkedListCycle";
 import solution1 from "../../../src/rules/listNode/linkedListCycle/solution1";
-import { inputData, expected } from "../../mock/ListNode/linkedListCycleData__";
+import { inputData, expected } from "../../mock/ListNode/linkedListCycleData";
 test("linkedListCycle function should determine if there is a cycle", () => {
   expect(linkedListCycle(inputData.case1)).toEqual(expected.case1);
   expect(linkedListCycle(inputData.case2)).toEqual(expected.case2);

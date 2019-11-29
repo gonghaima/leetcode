5c5be4122ed6776608e5749e9f85e6fa4a017764
Steven <gonghaima@hotmail.com>
2019-11-29 15:52:14 +1100

    numIslands - initial setup

---
+++ test/rules/numIslands.test.js
@@ -0,0 +1,12 @@
+import numIslands from "../../src/rules/numIslands";
+import { inputData, expected } from "../mock/numIslandsData";
+
+test("numIslands should return the number of isolated island - case 1", () => {
+  const result = numIslands(inputData.case1);
+  expect(result).toBe(expected.case1);
+});
+
+test("numIslands should return the number of isolated island - case 2", () => {
+  const result = numIslands(inputData.case2);
+  expect(result).toBe(expected.case2);
+});

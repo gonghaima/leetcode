50c3fd35a405b8c623f189e43e624f2d34f7c4af
Steven <gonghaima@hotmail.com>
2019-12-02 15:30:11 +1100

    SqrtX removed - travis ci fix

--- test/rules/SqrtX.test.js
+++
@@ -1,7 +0,0 @@
-import sqrtX from "../../src/rules/sqrtX";
-import { inputData, expected } from "../mock/sqrtXData";
-test("sqrtX function should calculate correct result from cases data", () => {
-  expect(sqrtX(inputData.case1)).toEqual(expected.case1);
-  expect(sqrtX(inputData.case2)).toEqual(expected.case2);
-  expect(sqrtX(inputData.case3)).toEqual(expected.case3);
-});

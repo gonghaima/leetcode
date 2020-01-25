75565de2f25c4a3d311e27c6dbdfd16804812480
Steven <gonghaima@hotmail.com>
2020-01-26 00:12:32 +1100

    decompressRLElist - test case passed

--- src/rules/decompressRLElist/index.js
+++ src/rules/decompressRLElist/index.js
@@ -1,3 +1,10 @@
 export default nums => {
-  return [1, 2, 3];
+  let currentVal = nums;
+  let result = [];
+  while (currentVal.length > 1) {
+    const temp = currentVal.splice(0, 2);
+    const tempResult = Array(temp[0]).fill(temp[1]);
+    result = result.concat(tempResult);
+  }
+  return result;
 };
--- test/rules/decompressRLElist.test.js
+++ test/rules/decompressRLElist.test.js
@@ -3,5 +3,5 @@ import { inputData, expected } from "../mock/decompressRLElistData";
 
 test("decompressRLElist should return max profit from a given list", () => {
   const result = decompressRLElist(inputData.case1);
-  expect(result).toBe(expected.case1);
+  expect(result).toEqual(expected.case1);
 });

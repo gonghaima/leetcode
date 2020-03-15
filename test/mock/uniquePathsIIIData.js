cefd3b5ecfef30c72cd5e732af52c18f50a11e36
Steven <gonghaima@hotmail.com>
2020-03-15 20:53:22 +1100

    uniquePathsIII initial setup

---
+++ test/mock/uniquePathsIIIData.js
@@ -0,0 +1,11 @@
+export const inputData = {
+  case1: [[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 2, -1]],
+  case2: [[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 2]],
+  case3: [[0, 1], [2, 0]]
+};
+
+export const expected = {
+  case1: 2,
+  case2: 4,
+  case3: 0
+};
---
+++ test/rules/uniquePathsIII.test.js
@@ -0,0 +1,17 @@
+import uniquePathsIII from "../../src/rules/uniquePathsIII";
+import { inputData, expected } from "../mock/uniquePathsIIIData";
+
+test("uniquePathsIII should return correct path number for case 1", () => {
+  const result = uniquePathsIII(inputData.case1);
+  expect(result).toBe(expected.case1);
+});
+
+test("uniquePathsIII should return correct path number for case 2", () => {
+  const result = uniquePathsIII(inputData.case2);
+  expect(result).toBe(expected.case2);
+});
+
+test("uniquePathsIII should return correct path number for case 3", () => {
+  const result = uniquePathsIII(inputData.case3);
+  expect(result).toBe(expected.case3);
+});

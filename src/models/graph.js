1fb0fa16857ad75a7a4ab15213b3d8fe0a89ced3
Steven <gonghaima@hotmail.com>
2020-04-04 23:45:33 +1100

    addVertex and addEdge

--- src/models/graph.js
+++ src/models/graph.js
@@ -1,3 +1,5 @@
+//ref: https://www.geeksforgeeks.org/implementation-graph-javascript/
+
 export default class Graph {
   // defining vertex array and 
   // adjacent list 
@@ -8,9 +10,45 @@ export default class Graph {
 
   // functions to be implemented 
 
-  // addVertex(v) 
-  // addEdge(v, w) 
+  // addVertex(v) - It adds the vertex v as key to adjList and initialize its values with an array.
+  addVertex(v) {
+    // initialize the adjacent list with a null array 
+    this.AdjList.set(v, []);
+  }
+
+  // add edge to the graph - It adds an edge between the src and dest.
+  addEdge(v, w) {
+    // get the list for vertex v and put the 
+    // vertex w denoting edge between v and w 
+    this.AdjList.get(v).push(w);
+
+    // Since graph is undirected, 
+    // add an edge from w to v also 
+    this.AdjList.get(w).push(v);
+  }
+
   // printGraph() 
+  // Prints the vertex and adjacency list – It prints vertices and its adjacency list.
+  printGraph() {
+    // get all the vertices 
+    var get_keys = this.AdjList.keys();
+
+    // iterate over the vertices 
+    for (var i of get_keys) {
+      // great the corresponding adjacency list 
+      // for the vertex 
+      var get_values = this.AdjList.get(i);
+      var conc = "";
+
+      // iterate over the adjacency list 
+      // concatenate the values into a string 
+      for (var j of get_values)
+        conc += j + " ";
+
+      // print the vertex and its adjacency list 
+      console.log(i + " -> " + conc);
+    }
+  }
 
   // bfs(v) 
   // dfs(v)

66bf8a50f12b74cdd58beff82cf43f4a5d60e63a
Steven Gong <gonghaima@gmail.com>
2020-04-04 15:20:44 +1100

    Feature/graph (#6)
    * add missing code
    
    * start to work on graph class

---
+++ src/models/graph.js
@@ -0,0 +1,17 @@
+export default class Graph {
+  // defining vertex array and 
+  // adjacent list 
+  constructor(noOfVertices) {
+    this.noOfVertices = noOfVertices;
+    this.AdjList = new Map();
+  }
+
+  // functions to be implemented 
+
+  // addVertex(v) 
+  // addEdge(v, w) 
+  // printGraph() 
+
+  // bfs(v) 
+  // dfs(v)
+}

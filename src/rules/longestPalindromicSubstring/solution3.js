8c8525061616b6b47530bf6472bbc79e2af6faf1
Steven <gonghaima@hotmail.com>
2020-10-27 23:01:17 +1100

    faster solution

---
+++ src/rules/longestPalindromicSubstring/solution3.js
@@ -0,0 +1,23 @@
+export const longestPalindromeSolution3 = s => {
+  let maxPal = '';
+
+  for (let i = 0; i < s.length; i++) {
+    bubble(i, i); // odd palindrome
+    bubble(i, i + 1); // even palindrome
+  }
+
+  function bubble(left, right) {
+
+    while (left >= 0 && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    left++;
+    right--;
+
+    if (maxPal.length < right - left + 1) {
+      maxPal = s.slice(left, right + 1)
+    }
+  }
+  return maxPal;
+};
--- test/rules/longestPalindromicSubstring.test.js
+++ test/rules/longestPalindromicSubstring.test.js
@@ -6,6 +6,7 @@ import { inputData, expected } from '../mock/longestPalindromicSubstringData';
 
 import { longestPalindromeSolution1 } from '../../src/rules/longestPalindromicSubstring/solution1';
 import { longestPalindromeSolution2 } from '../../src/rules/longestPalindromicSubstring/solution2';
+import { longestPalindromeSolution3 } from '../../src/rules/longestPalindromicSubstring/solution3';
 
 //index1 doesn't work on all tests
 // test('loopMatch function should decide if it is palindrom', () => {
@@ -30,14 +31,26 @@ import { longestPalindromeSolution2 } from '../../src/rules/longestPalindromicSu
 //   expect(longestPalindromeSolution1(inputData.case6)).toBe(expected.case6);
 // });
 
-// solution2 test
-test('longestPalindromeSolution1 function should return longest palindrom', () => {
-  expect(longestPalindromeSolution2(inputData.case1)).toBe(expected.case1);
-  expect(expected.case2).toContain(longestPalindromeSolution2(inputData.case2));
-  expect(longestPalindromeSolution2(inputData.case3)).toBe(expected.case3);
-  expect(longestPalindromeSolution2(inputData.case4)).toBe(expected.case4);
-  expect(longestPalindromeSolution2(inputData.case5)).toBe(expected.case5);
-  expect(longestPalindromeSolution2(inputData.case6)).toBe(expected.case6);
-  expect(longestPalindromeSolution2(inputData.case7)).toBe(expected.case7);
-  expect(longestPalindromeSolution2(inputData.case8)).toBe(expected.case8);
+// // solution2 test
+// test('longestPalindromeSolution1 function should return longest palindrom', () => {
+//   expect(longestPalindromeSolution2(inputData.case1)).toBe(expected.case1);
+//   expect(expected.case2).toContain(longestPalindromeSolution2(inputData.case2));
+//   expect(longestPalindromeSolution2(inputData.case3)).toBe(expected.case3);
+//   expect(longestPalindromeSolution2(inputData.case4)).toBe(expected.case4);
+//   expect(longestPalindromeSolution2(inputData.case5)).toBe(expected.case5);
+//   expect(longestPalindromeSolution2(inputData.case6)).toBe(expected.case6);
+//   expect(longestPalindromeSolution2(inputData.case7)).toBe(expected.case7);
+//   expect(longestPalindromeSolution2(inputData.case8)).toBe(expected.case8);
+// });
+
+// solution3 test
+test('longestPalindromeSolution3 function should return longest palindrom', () => {
+  expect(longestPalindromeSolution3(inputData.case1)).toBe(expected.case1);
+  expect(expected.case2).toContain(longestPalindromeSolution3(inputData.case2));
+  expect(longestPalindromeSolution3(inputData.case3)).toBe(expected.case3);
+  expect(longestPalindromeSolution3(inputData.case4)).toBe(expected.case4);
+  expect(longestPalindromeSolution3(inputData.case5)).toBe(expected.case5);
+  expect(longestPalindromeSolution3(inputData.case6)).toBe(expected.case6);
+  expect(longestPalindromeSolution3(inputData.case7)).toBe(expected.case7);
+  expect(longestPalindromeSolution3(inputData.case8)).toBe(expected.case8);
 });

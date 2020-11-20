90208be5336cbf1267442de98a69572dcf633ac0
Steven <gonghaima@hotmail.com>
2020-11-21 10:20:00 +1100

    refactor

--- test/mock/findJudgeData.js
+++ test/mock/findJudgeData.js
@@ -1,17 +1,8 @@
-export const inputData = {
-  case1: { N: 2, trust: [[1, 2]] },
-  case2: { N: 3, trust: [[1, 3], [2, 3]] },
-  case3: { N: 3, trust: [[1, 3], [2, 3], [3, 1]] },
-  case4: { N: 3, trust: [[1, 2], [2, 3]] },
-  case5: { N: 4, trust: [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]] },
-  case6: { N: 1, trust: [] },
-};
-
-export const expected = {
-  case1: 2,
-  case2: 3,
-  case3: -1,
-  case4: -1,
-  case5: 3,
-  case6: 1,
-};
+export default {
+  case1: { input: { N: 2, trust: [[1, 2]] }, expected: 2 },
+  case2: { input: { N: 3, trust: [[1, 3], [2, 3]] }, expected: 3 },
+  case3: { input: { N: 3, trust: [[1, 3], [2, 3], [3, 1]] }, expected: -1 },
+  case4: { input: { N: 3, trust: [[1, 2], [2, 3]] }, expected: -1 },
+  case5: { input: { N: 4, trust: [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]] }, expected: 3 },
+  case6: { input: { N: 1, trust: [] }, expected: 1 }
+}

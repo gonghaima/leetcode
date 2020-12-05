a7cad74dcbc7ed6fcd43bb4942fa35c8c7fee182
Steven <gonghaima@hotmail.com>
2020-12-05 11:07:32 +1100

    union find solutin - my own solution

--- test/rules/findCircleNum.test.js
+++ test/rules/findCircleNum.test.js
@@ -1,5 +1,6 @@
 import findCircleNum from "../../src/rules/findCircleNum";
 import findCircleNumSolution1 from "../../src/rules/findCircleNum/solution1";
+import findCircleNumSolution2 from "../../src/rules/findCircleNum/unionFind";
 import { inputData, expected } from "../mock/findCircleNumData";
 
 test("findCircleNum should find the number of direct related students, for case 1", () => {
@@ -21,4 +22,24 @@ test("findCircleNumSolution1 should find the number of direct related students, 
 test("findCircleNumSolution1 should find the number of direct related students, for case 2", () => {
   const result = findCircleNumSolution1(inputData.case2);
   expect(result).toBe(expected.case2);
+});
+
+test("findCircleNumSolution2 should find the number of direct related students, for case 1", () => {
+  const result = findCircleNumSolution2(inputData.case1);
+  expect(result).toBe(expected.case1);
+});
+
+test("findCircleNumSolution2 should find the number of direct related students, for case 2", () => {
+  const result = findCircleNumSolution2(inputData.case2);
+  expect(result).toBe(expected.case2);
+});
+
+test("findCircleNumSolution2 should find the number of direct related students, for case 3", () => {
+  const result = findCircleNumSolution2(inputData.case3);
+  expect(result).toBe(expected.case3);
+});
+
+test("findCircleNumSolution2 should find the number of direct related students, for case 4", () => {
+  const result = findCircleNumSolution2(inputData.case4);
+  expect(result).toBe(expected.case4);
 });

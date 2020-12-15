07bbd4f4bbb56db48c0140867725def372323e40
Steven <gonghaima@hotmail.com>
2020-12-16 05:07:54 +1100

    union find - v2

--- test/rules/findRedundantConnection.test.js
+++ test/rules/findRedundantConnection.test.js
@@ -1,13 +1,33 @@
 import findRedundantConnection from "../../src/rules/findRedundantConnection";
+import findRedundantConnection_v2 from "../../src/rules/findRedundantConnection/unionFind_v2";
 import { inputData, expected } from "../mock/findRedundantConnectionData";
 
-test("findRedundantConnection should find the number of direct related students, for case 1", () => {
-  const result = findRedundantConnection(inputData.case1);
+// test("findRedundantConnection should find the number of direct related students, for case 1", () => {
+//   const result = findRedundantConnection(inputData.case1);
+//   expect(result).toEqual(expected.case1);
+// });
+
+// test("findRedundantConnection should find the number of direct related students, for case 2", () => {
+//   const result = findRedundantConnection(inputData.case2);
+//   expect(result[0]).toBe(expected.case2[0]);
+//   expect(result[1]).toBe(expected.case2[1]);
+// });
+
+
+test("findRedundantConnection_v2 should find the number of direct related students, for case 1", () => {
+  const result = findRedundantConnection_v2(inputData.case1);
   expect(result).toEqual(expected.case1);
 });
 
-test("findRedundantConnection should find the number of direct related students, for case 2", () => {
-  const result = findRedundantConnection(inputData.case2);
+test("findRedundantConnection_v2 should find the number of direct related students, for case 2", () => {
+  const result = findRedundantConnection_v2(inputData.case2);
   expect(result[0]).toBe(expected.case2[0]);
   expect(result[1]).toBe(expected.case2[1]);
+});
+
+test("findRedundantConnection_v2 should find the number of direct related students, for case 3", () => {
+  const result = findRedundantConnection_v2(inputData.case3);
+  console.log(result);
+  expect(result[0]).toBe(expected.case3[0]);
+  expect(result[1]).toBe(expected.case3[1]);
 });

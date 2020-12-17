008ed12305925f3d35be042fdc543c94dcc7893e
Steven <gonghaima@hotmail.com>
2020-12-17 13:06:44 +1100

    union find - v3

--- test/rules/findRedundantConnection.test.js
+++ test/rules/findRedundantConnection.test.js
@@ -1,5 +1,6 @@
 import findRedundantConnection from "../../src/rules/findRedundantConnection";
 import findRedundantConnection_v2 from "../../src/rules/findRedundantConnection/unionFind_v2";
+import findRedundantConnection_v3 from "../../src/rules/findRedundantConnection/unionFind_v3";
 import { inputData, expected } from "../mock/findRedundantConnectionData";
 
 // test("findRedundantConnection should find the number of direct related students, for case 1", () => {
@@ -14,19 +15,38 @@ import { inputData, expected } from "../mock/findRedundantConnectionData";
 // });
 
 
-test("findRedundantConnection_v2 should find the number of direct related students, for case 1", () => {
-  const result = findRedundantConnection_v2(inputData.case1);
-  expect(result).toEqual(expected.case1);
-});
+// test("findRedundantConnection_v2 should find the number of direct related students, for case 1", () => {
+//   const result = findRedundantConnection_v2(inputData.case1);
+//   expect(result).toEqual(expected.case1);
+// });
 
-test("findRedundantConnection_v2 should find the number of direct related students, for case 2", () => {
-  const result = findRedundantConnection_v2(inputData.case2);
-  expect(result[0]).toBe(expected.case2[0]);
-  expect(result[1]).toBe(expected.case2[1]);
-});
+// test("findRedundantConnection_v2 should find the number of direct related students, for case 2", () => {
+//   const result = findRedundantConnection_v2(inputData.case2);
+//   expect(result[0]).toBe(expected.case2[0]);
+//   expect(result[1]).toBe(expected.case2[1]);
+// });
 
-test("findRedundantConnection_v2 should find the number of direct related students, for case 3", () => {
-  const result = findRedundantConnection_v2(inputData.case3);
+// test("findRedundantConnection_v2 should find the number of direct related students, for case 3", () => {
+//   const result = findRedundantConnection_v2(inputData.case3);
+//   console.log(result);
+//   expect(result[0]).toBe(expected.case3[0]);
+//   expect(result[1]).toBe(expected.case3[1]);
+// });
+
+
+// test("findRedundantConnection_v3 should find the number of direct related students, for case 1", () => {
+//   const result = findRedundantConnection_v3(inputData.case1);
+//   expect(result).toEqual(expected.case1);
+// });
+
+// test("findRedundantConnection_v3 should find the number of direct related students, for case 2", () => {
+//   const result = findRedundantConnection_v3(inputData.case2);
+//   expect(result[0]).toBe(expected.case2[0]);
+//   expect(result[1]).toBe(expected.case2[1]);
+// });
+
+test("findRedundantConnection_v3 should find the number of direct related students, for case 3", () => {
+  const result = findRedundantConnection_v3(inputData.case3);
   console.log(result);
   expect(result[0]).toBe(expected.case3[0]);
   expect(result[1]).toBe(expected.case3[1]);

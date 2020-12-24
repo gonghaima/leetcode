9475f283b9c4f2e180dac175042c3b17e97495b1
Steven <gonghaima@hotmail.com>
2020-12-24 13:58:59 +1100

    removeStones - test case passes

--- src/rules/removeStones/index.js
+++ src/rules/removeStones/index.js
@@ -1,3 +1,65 @@
+// union find
+
+// util node builder
+class Node {
+  constructor(val, parent) {
+    this.val = val;
+    this.parent = parent;
+  }
+}
+
+// util findParent
+const findParentNode = (nd) => {
+  if (nd && nd.parent) {
+    return findParentNode(nd.parent);
+  } else {
+    return nd;
+  }
+}
+
 export default stones => {
-  return Number.MIN_SAFE_INTEGER;
+  // initialize node list
+  let nodes = {};
+  for (let x = 0; x < stones.length; x += 1) {
+    const [firstVal, secondVal] = stones[x];
+    nodes[stones[x]] = new Node(stones[x], null);
+  }
+
+  // add relations initiate edges
+  const nodeList = Object.entries(nodes);
+
+  for (let idx = 0; idx < nodeList.length; idx++) {
+    const [ref, nd] = nodeList[idx];
+    const [x, y] = ref.split(',')
+    for (let ix = idx + 1; (ix < nodeList.length) && nodeList.length > (idx + 1); ix++) {
+      const [ref1, nd1] = nodeList[ix];
+      const [a, b] = ref1.split(',')
+      if (a === x) {
+        if (b < y) {
+          nd.parent = nd1;
+        } else {
+          nd1.parent = nd;
+        }
+      }
+      if (b === y) {
+        if (a < x) {
+          nd.parent = nd1;
+        } else {
+          nd1.parent = nd;
+        }
+      }
+    }
+  }
+  //debugger;
+  //nodeList.length - 
+  // find nodes without parent
+  let cnt = 0;
+  nodeList.map(([_, nd]) => {
+    if (!nd.parent) cnt += 1;
+  })
+
+  // total nodes number substract root nodes
+  const result = nodeList.length - cnt;
+
+  return result;
 };

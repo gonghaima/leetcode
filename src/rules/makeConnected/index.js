a8fdaac73bfa99f0608c5e9e495abb1e8be23b23
Steven <gonghaima@hotmail.com>
2021-01-10 15:20:40 +1100

    makeConnected - unionFind

--- src/rules/makeConnected/index.js
+++ src/rules/makeConnected/index.js
@@ -1,3 +1,49 @@
+/********************************************************************************************************************************
+ * Runtime: 168 ms, faster than 60.71% of JavaScript online submissions for Number of Operations to Make Network Connected.     *
+ * Memory Usage: 53.4 MB, less than 80.36% of JavaScript online submissions for Number of Operations to Make Network Connected. *
+ ********************************************************************************************************************************/
+
 export default (n, connections) => {
-  return Number.MIN_VALUE;
+  const parents = {};
+  let parentsCount = 0;
+
+  let uniqueItems = new Set();
+
+  const find = (u) => {
+    if (parents[u] == null) {
+      parentsCount++;
+      parents[u] = u;
+    }
+    else if (parents[u] !== u) parents[u] = find(parents[u]); // path compression
+    return parents[u];
+  };
+
+  const union = (u, v) => {
+    const p1 = find(u);
+    const p2 = find(v);
+    if (p1 !== p2) {
+      parents[p1] = p2;
+      parentsCount--;
+    }
+  };
+
+  for (const [u, v] of connections) {
+    uniqueItems.add(u);
+    uniqueItems.add(v);
+    union(u, v);
+  }
+
+
+  const existingRequiredConnenctions = uniqueItems.size - 1;
+  const extraConnenctionsAvailable = connections.length - existingRequiredConnenctions;
+
+
+
+  const isolatedNodesCount = n - uniqueItems.size;
+  const totalParent = isolatedNodesCount + parentsCount;
+  const movesRequired = totalParent - 1;
+
+  const result = movesRequired > extraConnenctionsAvailable ? -1 : movesRequired;
+
+  return result;
 };
--- test/rules/makeConnected.test.js
+++ test/rules/makeConnected.test.js
@@ -6,3 +6,18 @@ test("makeConnected should find the largest possible number of moves for case 1"
   const result = makeConnected(inputData.case1.n, inputData.case1.connections);
   expect(result).toEqual(expected.case1);
 });
+
+test("makeConnected should find the largest possible number of moves for case 2", () => {
+  const result = makeConnected(inputData.case2.n, inputData.case2.connections);
+  expect(result).toEqual(expected.case2);
+});
+
+test("makeConnected should find the largest possible number of moves for case 3", () => {
+  const result = makeConnected(inputData.case3.n, inputData.case3.connections);
+  expect(result).toEqual(expected.case3);
+});
+
+test("makeConnected should find the largest possible number of moves for case 4", () => {
+  const result = makeConnected(inputData.case4.n, inputData.case4.connections);
+  expect(result).toEqual(expected.case4);
+});

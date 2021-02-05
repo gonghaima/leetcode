2b4d9166e141bf9f7b251070ea1c527512d10b1b
Steven <gonghaima@hotmail.com>
2021-02-05 23:08:25 +1100

    notes updated

--- src/rules/isOneBitCharacter/index.js
+++ src/rules/isOneBitCharacter/index.js
@@ -1,3 +1,11 @@
+/************************************************************************************************************
+ * Runtime: 76 ms, faster than 83.56% of JavaScript online submissions for 1-bit and 2-bit Characters.      *
+ * Memory Usage: 38.5 MB, less than 85.84% of JavaScript online submissions for 1-bit and 2-bit Characters. *
+ ************************************************************************************************************/
+
+
+// Increment Pointer
+
 export default (bits) => {
   while (bits.length > 1) {
     if (bits.shift() === 1) bits.shift();

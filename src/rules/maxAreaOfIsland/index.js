f3a16b641fb495780aadb9ca01ae25cdacfbeafd
Steven <gonghaima@hotmail.com>
2021-02-08 20:04:06 +1100

    maxAreaOfIsland - working for all area

--- src/rules/maxAreaOfIsland/index.js
+++ src/rules/maxAreaOfIsland/index.js
@@ -1,46 +1,48 @@
-/**
- * @param {character[][]} grid
- * @return {number}
- */
-const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+/****************************************************************************************************
+ * Runtime: 100 ms, faster than 81.07% of JavaScript online submissions for Max Area of Island.     *
+ * Memory Usage: 43.1 MB, less than 34.89% of JavaScript online submissions for Max Area of Island. *
+ ****************************************************************************************************/
 
 export default (grid) => {
-  const xLimit = grid.length;
-  const yLimit = grid[0].length;
-  let visited = new Set();
-  let max = 0;
-  
+  let markIsland = function (grid, x, y, visited, currentItems) {
+    //not valid, out boundary
+    if (x < 0 || x > grid.length - 1 || y < 0 || y > grid[x].length - 1) {
+      return;
+    }
+    // return if visited
+    if (visited[x][y] === true) {
+      return;
+    }
+    //mark as visited
+    visited[x][y] = true;
+    if (grid[x][y] === 0) {
+      return;
+    }
+    currentItems.push(1);
+    markIsland(grid, x - 1, y, visited, currentItems);
+    markIsland(grid, x + 1, y, visited, currentItems);
+    markIsland(grid, x, y - 1, visited, currentItems);
+    markIsland(grid, x, y + 1, visited, currentItems);
+
+  };
+
+  // initialize visited tracking array
+  let visited = [];
+  let currentItems = [];
+  let maxLength = 0;
   for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] === 1 && !visited.has(i + "" + j)) {
-        visited.add(i + "" + j);
+    visited[i] = [];
+  }
+  for (let x = 0; x < grid.length; x++) {
+    for (let y = 0; y < grid[x].length; y++) {
+      // if there is a land, not visited, increase counter & mark all connected as visited
+      if (!visited[x][y] && grid[x][y] === 1) {
+        currentItems.length = 0;
+        markIsland(grid, x, y, visited, currentItems);
         debugger;
-        let count=1;
-        const markDirection = (x, y) => {
-          visited.add(x + "" + y);
-          directions.map(([xDirection, yDirection]) => {
-            const currentX = x + xDirection;
-            const currentY = y + yDirection;
-            const visitedIndex = currentX + "" + currentY;
-            if (currentX >= 0 && currentX < xLimit && currentY >= 0 && currentY < yLimit
-              && !visited.has(visitedIndex)
-              && grid[currentX][currentY] === 1) {
-              count += 1;
-              visited.add(visitedIndex);
-              markDirection(currentX, currentY, count);
-            } else {
-              visited.add(visitedIndex);
-            }
-          })
-        };
-        markDirection(i, j, count);
-        debugger;
-        max = max > count ? max : count;
-      } else {
-        visited.add(i + "" + j);
+        maxLength = maxLength > currentItems.length ? maxLength : currentItems.length;
       }
     }
-
   }
-  return max;
+  return maxLength;
 };

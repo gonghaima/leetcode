9f76045b7a72455b39ddd08f6f63223ff94ec54d
Steven <gonghaima@hotmail.com>
2021-02-10 18:52:43 +1100

    largestSubmatrix - initial setup

---
+++ src/rules/largestSubmatrix/index.js
@@ -0,0 +1,8 @@
+/****************************************************************************************************
+ * Runtime: 100 ms, faster than 81.07% of JavaScript online submissions for Max Area of Island.     *
+ * Memory Usage: 43.1 MB, less than 34.89% of JavaScript online submissions for Max Area of Island. *
+ ****************************************************************************************************/
+
+export default (matrix) => {
+  return Number.MIN_VALUE;
+};
---
+++ test/rules/largestSubmatrix.test.js
@@ -0,0 +1,18 @@
+import largestSubmatrix from "../../src/rules/largestSubmatrix";
+
+import { inputData, expected } from "../mock/largestSubmatrixData";
+
+test("largestSubmatrix should return the number of isolated island - case 1", () => {
+  const result = largestSubmatrix(inputData.case1);
+  expect(result).toBe(expected.case1);
+});
+
+// test("largestSubmatrix should return the number of isolated island - case 2", () => {
+//   const result = largestSubmatrix(inputData.case2);
+//   expect(result).toBe(expected.case2);
+// });
+
+// test("largestSubmatrix should return the number of isolated island - case 3", () => {
+//   const result = largestSubmatrix(inputData.case3);
+//   expect(result).toBe(expected.case3);
+// });

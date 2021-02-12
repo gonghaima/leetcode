c307b646555e212500ae7464c22fcbdf7b267903
Steven <gonghaima@hotmail.com>
2021-02-13 10:29:37 +1100

    numEnclaves - test case passes

--- test/rules/numEnclaves.test.js
+++ test/rules/numEnclaves.test.js
@@ -6,3 +6,9 @@ test("numEnclaves should return the number of isolated island - case 1", () => {
   const result = numEnclaves(inputData.case1);
   expect(result).toBe(expected.case1);
 });
+
+test("numEnclaves should return the number of isolated island - case 2", () => {
+  const result = numEnclaves(inputData.case2);
+  expect(result).toBe(expected.case2);
+});
+

a3a750e271d6f81a4b2efef909641f1e6b4555ca
Steven <gonghaima@hotmail.com>
2021-02-14 14:09:54 +1100

    simplified code

--- src/rules/numEnclaves/bfs-mine-enhanced.js
+++ src/rules/numEnclaves/bfs-mine-enhanced.js
@@ -1,4 +1,12 @@
+/****************************************************************************************************
+ * Runtime: 172 ms, faster than 9.09% of JavaScript online submissions for Number of Enclaves.      *
+ * Memory Usage: 48.2 MB, less than 11.36% of JavaScript online submissions for Number of Enclaves. *
+ ****************************************************************************************************/
 
+/*******************************************
+ * 1. color all edge/connected land into 0 *
+ * 2. count 1s in total                    *
+ *******************************************/
 export default (A) => {
   if (!A) { return 0; }
   let rows = A.length; if (rows == 0) { return 0; }
@@ -15,7 +23,7 @@ export default (A) => {
   for (let row = 0; row < rows; ++row) {
     for (let col = 0; col < cols; ++col) {
       if (A[row][col] === 1 && onEdge(row, col)) {
-        if (onEdge(row, col)) edgeGroup.add(islandcount);
+        if (row === 0 && col === 2) debugger;
         A[row][col] = 0; // mark as visited
         let us = [], neighbors = [];
         neighbors.push([row, col]);
@@ -24,6 +32,7 @@ export default (A) => {
           us.map(([r, c]) => {
             triverseParams.map(([a, b]) => {
               const [x, y] = [a + r, b + c];
+              if (x === 0 && y === 2) debugger;
               if (inBoundary(x, y) && A[x][y] === 1) {
                 neighbors.push([x, y]); A[x][y] = 0;
               }
@@ -34,12 +43,5 @@ export default (A) => {
     }
   }
 
-  // loop islandGroup, find groups not in dedge, add their value together 
-  // let total = 0;
-  // Object.entries(islandGroup).map(([key, val]) => {
-  //   if (!edgeGroup.has(Number(key))) {
-  //     total += val;
-  //   }
-  // });
-  return A.map(arr=>arr.reduce((a,b)=>a+b)).reduce((a,b)=>a+b);
+  return A.map(arr => arr.reduce((a, b) => a + b)).reduce((a, b) => a + b);
 };

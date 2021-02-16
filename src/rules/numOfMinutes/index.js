562a0ae5e2cb9cde25944b22cd16b2492fef478f
Steven <gonghaima@hotmail.com>
2021-02-16 22:40:52 +1100

    accepted answer

--- src/rules/numOfMinutes/index.js
+++ src/rules/numOfMinutes/index.js
@@ -1,3 +1,30 @@
-export default (A) => {
-  return Number.MIN_VALUE;
-}
+/**
+ * @param {number} n
+ * @param {number} headID
+ * @param {number[]} manager
+ * @param {number[]} informTime
+ * @return {number}
+ */
+
+/*********************************************************************************************************************
+ *  Runtime: 660 ms, faster than 14.94% of JavaScript online submissions for Time Needed to Inform All Employees.    *
+ * Memory Usage: 79.3 MB, less than 34.48% of JavaScript online submissions for Time Needed to Inform All Employees. *
+ *********************************************************************************************************************/
+
+
+const numOfMinutes = function (n, headID, manager, informTime) {
+  const employeesMap = new Map(Array.from(manager, (_, i) => [i, []]));
+  for (const [i, head] of manager.entries()) {
+    if (head !== -1) {
+      employeesMap.get(head).push(i);
+    }
+  }
+  return dfs(headID);
+
+  function dfs(head) {
+    // time required to reach to an employee  + time for him/her to reach direct subordinates
+    return Math.max(0, ...employeesMap.get(head).map(dfs)) + informTime[head];
+  }
+};
+
+export default numOfMinutes;
--- test/rules/numOfMinutes.test.js
+++ test/rules/numOfMinutes.test.js
@@ -3,7 +3,32 @@ import numOfMinutes from "../../src/rules/numOfMinutes";
 import { inputData, expected } from "../mock/numOfMinutesData";
 
 test("numOfMinutes should return the number of isolated island - case 1", () => {
-  const result = numOfMinutes(inputData.case1);
+  const { n, headID, manager, informTime} = inputData.case1;
+  const result = numOfMinutes(n, headID, manager, informTime);
   expect(result).toBe(expected.case1);
+});
+
+test("numOfMinutes should return the number of isolated island - case 2", () => {
+  const { n, headID, manager, informTime } = inputData.case2;
+  const result = numOfMinutes(n, headID, manager, informTime);
+  expect(result).toBe(expected.case2);
+});
+
+test("numOfMinutes should return the number of isolated island - case 3", () => {
+  const { n, headID, manager, informTime } = inputData.case3;
+  const result = numOfMinutes(n, headID, manager, informTime);
+  expect(result).toBe(expected.case3);
+});
+
+test("numOfMinutes should return the number of isolated island - case 4", () => {
+  const { n, headID, manager, informTime } = inputData.case4;
+  const result = numOfMinutes(n, headID, manager, informTime);
+  expect(result).toBe(expected.case4);
+});
+
+test("numOfMinutes should return the number of isolated island - case 5", () => {
+  const { n, headID, manager, informTime } = inputData.case5;
+  const result = numOfMinutes(n, headID, manager, informTime);
+  expect(result).toBe(expected.case5);
 });
 

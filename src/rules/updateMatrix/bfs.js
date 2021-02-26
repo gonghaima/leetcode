3c22d9944b5ee2b2654b484cbfbff52de71859c5
Steven <gonghaima@hotmail.com>
2021-02-27 10:41:00 +1100

    bfs - accepted 39.7%, 55.9

--- src/rules/updateMatrix/bfs.js
+++ src/rules/updateMatrix/bfs.js
@@ -2,8 +2,64 @@
  * @param {number[][]} matrix
  * @return {number[][]}
  */
+
+/*****************************************************************************************
+ * Runtime: 224 ms, faster than 39.76% of JavaScript online submissions for 01 Matrix.   *
+ * Memory Usage: 49 MB, less than 55.91% of JavaScript online submissions for 01 Matrix. *
+ *****************************************************************************************/
+
 var updateMatrix = function (matrix) {
-  return [[]];
+  const row = matrix.length;
+  const column = matrix[0].length;
+  // const visited = new Array(row).fill(0).map(()=>new Array(column).fill(false));
+  const visited = [...Array(row)].map(x => Array(column).fill(false))
+  // const visited = (new Array(row)).map(() => {
+  //   const ar = (new Array(column)).map(()=>false);
+  //   return ar;
+  // });
+  let counter = 1;
+  let next = [];
+  // keep a visited tracker
+  // keep a counter from 1
+  //find all 0s , add as visited, also add as next
+  for (let r = 0; r < row; r++) {
+    for (let c = 0; c < column; c++) {
+      if (matrix[r][c] === 0) {
+        visited[r][c] = true;
+        next.push([r, c]);
+      }
+    }
+  }
+
+  const directions = [[0, -1], [1, 0], [0, 1], [-1, 0]];
+  const isValid = (r, c) => r > -1 && r < row && c > -1 && c < column;
+
+  //While next>0
+  //for each adjacency, if not visited, 
+  //set value to counter, add to tempNext
+  //set next to tempNext
+
+  while (next.length > 0) {
+    const tempForNext = [];
+
+    next.map(([r, c]) => {
+
+      directions.map(([rDir, cDir]) => {
+        const adjR = r + rDir;
+        const adjC = c + cDir;
+        if (isValid(adjR, adjC) && !visited[adjR][adjC]) {
+          visited[adjR][adjC] = true;
+          matrix[adjR][adjC] = counter;
+          tempForNext.push([adjR, adjC]);
+        }
+      });
+    });
+    counter += 1;
+    next = tempForNext;
+  }
+
+  //return matrix
+  return matrix;
 };
 
 export default updateMatrix;

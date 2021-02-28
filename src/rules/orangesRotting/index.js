1c47ce020848882dfeb9c72db57e333e601f4be6
Steven <gonghaima@hotmail.com>
2021-02-28 20:57:00 +1100

    bfs

--- src/rules/orangesRotting/index.js
+++ src/rules/orangesRotting/index.js
@@ -1,18 +1,34 @@
-
+/*************************************************************************************************
+ * Runtime: 100 ms, faster than 42.94% of JavaScript online submissions for Rotting Oranges.     *
+ * Memory Usage: 42.4 MB, less than 17.80% of JavaScript online submissions for Rotting Oranges. *
+ *************************************************************************************************/
 
 var orangesRotting = function (grid) {
-  if (grid[0][0] === 0) return -1;
-
-
-
   const rows = grid.length;
   const cols = grid[0].length;
+
+  // if all 0s, return 0
+  let total = 0;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      total += grid[i][j];
+    }
+  };
+  if (total === 0) return 0;
+
   const directions = [[0, -1], [1, 0], [0, 1], [-1, 0]];
   const isValid = (r, c) => r >= 0 && r < rows & c >= 0 && c < cols;
-  let counter = 0;
+  let counter = -1;
   let next = [];
-  next.push([0, 0]);
 
+  // finding the starting node, where value === 2
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 2) next.push([i, j]);
+    }
+  }
+
+  // update to rotten, track the step count
   while (next.length) {
     counter += 1;
     let tempNext = [];
@@ -21,7 +37,7 @@ var orangesRotting = function (grid) {
         const newR = cR + r;
         const newC = cC + c;
         if (isValid(newR, newC) && grid[newR][newC] === 1) {
-          grid[newR, newC] = 2;
+          grid[newR][newC] = 2;
           tempNext.push([newR, newC]);
         }
       })
@@ -29,10 +45,16 @@ var orangesRotting = function (grid) {
     next = tempNext;
   }
 
-  const noFreshLeft = 0;
-  grid.map(c => c.map(([r, c]) => {
-    if (grid[r][c] === 1) noFreshLeft += 1;
-  }));
+  let noFreshLeft = 0;
+
+  //check if any 1s left in updated grid
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 1) noFreshLeft += 1;
+    }
+  }
+
+  // if still 1s left, return -1,  otherwise return counter
   return noFreshLeft > 0 ? -1 : counter;
 }
 
--- test/rules/orangesRotting.test.js
+++ test/rules/orangesRotting.test.js
@@ -1,5 +1,5 @@
 import orangesRotting from "../../src/rules/orangesRotting";
-import { inputData, expected } from "../mock/maxDistanceData";
+import { inputData, expected } from "../mock/orangesRottingData";
 
 test("orangesRotting should return the distance for case1", () => {
   const result = orangesRotting(inputData.case1);
@@ -13,6 +13,17 @@ test("orangesRotting should return the distance for case2", () => {
 
 test("orangesRotting should return the distance for case3", () => {
   const result = orangesRotting(inputData.case3);
-  expect(result).toBe(expected.case2);
+  expect(result).toBe(expected.case3);
 });
+
+test("orangesRotting should return the distance for case4", () => {
+  const result = orangesRotting(inputData.case4);
+  expect(result).toBe(expected.case4);
+});
+
+test("orangesRotting should return the distance for case5", () => {
+  const result = orangesRotting(inputData.case5);
+  expect(result).toBe(expected.case5);
+});
+
 

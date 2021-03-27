566d22b0f516a925109ab8481647506495c051a9
Steven <gonghaima@hotmail.com>
2021-03-27 12:02:36 +1100

    longestSubarray - initial setup

---
+++ src/rules/longestSubarray/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} limit
+ * @return {number}
+ */
+var longestSubarray = function (nums, limit) {
+    return Infinity;
+};
+
+export default longestSubarray;
---
+++ test/rules/longestSubarray.test.js
@@ -0,0 +1,17 @@
+
+import longestSubarray from "../../src/rules/longestSubarray";
+import { inputData, expected } from "../mock/longestSubarrayData";
+
+test("longestSubarray should return expected result for case1", () => {
+  const result = longestSubarray(inputData.case1.nums, inputData.case1.limit);
+  expect(result).toEqual(expected.case1);
+});
+test("longestSubarray should return expected result for case2", () => {
+  const result = longestSubarray(inputData.case2.nums, inputData.case2.limit);
+  expect(result).toEqual(expected.case2);
+});
+
+test("longestSubarray should return expected result for case3", () => {
+  const result = longestSubarray(inputData.case3.nums, inputData.case3.limit);
+  expect(result).toEqual(expected.case3);
+});

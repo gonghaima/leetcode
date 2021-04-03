b7f94b29095fbb19dada2e350698b9fdfdcd78b4
Steven <gonghaima@hotmail.com>
2021-04-03 13:21:03 +1100

    sliding window - my solution

--- src/rules/numberOfSubstrings/index.js
+++ src/rules/numberOfSubstrings/index.js
@@ -2,8 +2,46 @@
  * @param {string} s
  * @return {number}
  */
+// my solution - sliding window
+
+/**************************************************************************************************************************************
+ * Runtime: 6124 ms, faster than 6.25% of JavaScript online submissions for Number of Substrings Containing All Three Characters.     *
+ * Memory Usage: 46.2 MB, less than 14.06% of JavaScript online submissions for Number of Substrings Containing All Three Characters. *
+ **************************************************************************************************************************************/
+
 var numberOfSubstrings = function (s) {
-    return Infinity;
+    let left = 0;
+    let counter = 0;
+    const allOccurrence = [...new Set(s)];
+    if (allOccurrence.length < 3) return 0;
+    const currentArr = [];
+
+    const valid = () => {
+        let isValid = true;
+        for (let idx = 0; isValid && idx < allOccurrence.length; idx++) {
+            const element = allOccurrence[idx];
+            if (currentArr.indexOf(element) === -1) isValid = false;
+        }
+        return isValid;
+    }
+
+    for (let right = 0; right < s.length; right++) {
+        currentArr.push(s[right]);
+        if (valid()) {
+            // if match, means the rest (between right and end) would be all meet the requirement
+            counter += (s.length - right);
+            left++;
+            currentArr.shift();
+            while (valid()) {
+                counter += (s.length - right);
+                left++;
+                currentArr.shift();
+            }
+        }
+    }
+    return counter;
 };
+
+// 
 
 export default numberOfSubstrings;
--- test/mock/numberOfSubstringsData.js
+++ test/mock/numberOfSubstringsData.js
@@ -1,11 +1,13 @@
 export const inputData = {
   case1: "abcabc",
   case2: "aaacb",
-  case3: "abc"
+  case3: "abc",
+  case4: "abab",
 };
 
 export const expected = {
   case1: 10,
   case2: 3,
-  case3: 1
+  case3: 1,
+  case4: 0
 };

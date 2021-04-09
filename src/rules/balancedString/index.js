388e39172d85cf0c6cbb61d3c011769c470b9098
Steven <gonghaima@hotmail.com>
2021-04-09 10:24:47 +1000

    sliding window

--- src/rules/balancedString/index.js
+++ src/rules/balancedString/index.js
@@ -3,20 +3,38 @@
  * @return {number}
  */
 
-var balancedString = function (s) {
-    const ref = "QWER";
-    const count = [0, 0, 0, 0];
-    for (const char of s) {
-        count[ref.indexOf(char)] += 1;
-    }
+/*************************************************************************************************************************
+ * Runtime: 100 ms, faster than 84.62% of JavaScript online submissions for Replace the Substring for Balanced String.   *
+ * Memory Usage: 40 MB, less than 92.31% of JavaScript online submissions for Replace the Substring for Balanced String. *
+ *************************************************************************************************************************/
 
-    let max = 0, min = 0;
-    count.forEach(num => {
-        max = num > max ? num : max;
-        min = num < min ? num : min;
-    })
-    if (max === min) return 0;
-    return max - min - 1;
+// sliding window
+
+var balancedString = function (s) {
+    let map = {
+        "Q": 0,
+        "W": 0,
+        "E": 0,
+        "R": 0
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]]++
+    };
+    let limit = Math.floor(s.length / 4);
+    let ans = Infinity;
+    let start = 0;
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]]--;
+
+        while (start <= s.length && map["Q"] <= limit && map["W"] <= limit && map["E"] <= limit && map["R"] <= limit) {
+            ans = Math.min(ans, i - start + 1)
+            map[s[start]]++
+            start++
+        }
+    };
+
+    return ans;
 };
 
 export default balancedString;
--- test/mock/balancedStringData.js
+++ test/mock/balancedStringData.js
@@ -3,11 +3,19 @@ export const inputData = {
   case2: "QQWE",
   case3: "QQQW",
   case4: "QQQQ",
+  case5: "RQQERWEWWREQEQWR",
+  case6: "RQRERREWEEWWQWRRRWQQEQQQ",
+  case7: "WWQQRRRRQRQQ",
+  case8: "WWEQERQWQWWRWWERQWEQ"
 };
 
 export const expected = {
   case1: 0,
   case2: 1,
   case3: 2,
-  case4: 3
+  case4: 3,
+  case5: 0,
+  case6: 2,
+  case7: 4,
+  case8: 4
 };

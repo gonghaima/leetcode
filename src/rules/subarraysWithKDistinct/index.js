64ed7c45e5e2d3dde4b7fe34cc2835fbff3eb142
Steven <gonghaima@hotmail.com>
2021-04-18 13:38:58 +1000

    subarraysWithKDistinct - initial setup

---
+++ src/rules/subarraysWithKDistinct/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+var subarraysWithKDistinct = function (A, K) {
+    return Infinity;
+};
+export default subarraysWithKDistinct;
---
+++ test/mock/subarraysWithKDistinctData.js
@@ -0,0 +1,9 @@
+export const inputData = {
+  case1: { A: [1, 2, 1, 2, 3], K: 2 },
+  case2: { A: [1, 2, 1, 3, 4], K: 3 }
+};
+
+export const expected = {
+  case1: 7,
+  case2: 3
+};

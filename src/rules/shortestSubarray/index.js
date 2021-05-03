83914db46a9abd851c6543038f286c045b9206bb
Steven <gonghaima@hotmail.com>
2021-05-03 20:55:01 +1000

    Monotonic Deque Solution

--- src/rules/shortestSubarray/index.js
+++ src/rules/shortestSubarray/index.js
@@ -3,23 +3,38 @@
  * @param {number} K
  * @return {number}
  */
-var shortestSubarray = function (A, K) {
-    let left = 0, currentTotal = null, shortest = Number.MAX_VALUE;
-    for (let right = 0; right < A.length; right++) {
-        if (currentTotal === null) currentTotal = 0;
-        currentTotal += A[right];
-        if (currentTotal < K) {
-            continue;
-        } else {
-            while (currentTotal >= K) {
-                shortest = Math.min(shortest, right - left + 1);
-                currentTotal -= A[left];
-                left++;
-            }
-        }
 
+/***********************************************************************************************************************
+ * Runtime: 272 ms, faster than 63.64% of JavaScript online submissions for Shortest Subarray with Sum at Least K.     *
+ * Memory Usage: 48.4 MB, less than 36.36% of JavaScript online submissions for Shortest Subarray with Sum at Least K. *
+ ***********************************************************************************************************************/
+
+// https://www.youtube.com/watch?v=gSV4IniBMh0
+
+var shortestSubarray = function (A, K) {
+    let n = A.length;
+    let len = Number.MAX_VALUE;
+
+    let prefixSum = new Array(n + 1);
+    prefixSum[0] = 0;
+
+    //Calculate the prefix sum 
+    for (let i = 1; i < n + 1; i++)
+        prefixSum[i] = A[i - 1] + prefixSum[i - 1];
+    let dq = [];
+
+    //Maintain a monotone queue
+    for (let i = 0; i < n + 1; i++) {
+        while (dq.length && (prefixSum[i] - prefixSum[dq[0]]) >= K) {
+            len = Math.min(len, i - dq[0]);
+            dq.shift();
+        }
+        while (dq.length && prefixSum[i] < prefixSum[dq[dq.length - 1]]) {
+            dq.pop();
+        }
+        dq.push(i);
     }
-    return shortest === Number.MAX_VALUE ? -1 : shortest;
+    return len == Number.MAX_VALUE ? -1 : len;
 };
 
 export default shortestSubarray;
--- test/rules/shortestSubarray.test.js
+++ test/rules/shortestSubarray.test.js
@@ -2,24 +2,24 @@
 import shortestSubarray from "../../src/rules/shortestSubarray";
 import { inputData, expected } from "../mock/shortestSubarrayData";
 
-// test("shortestSubarray should return expected result for case1", () => {
-//   const result = shortestSubarray(inputData.case1.A, inputData.case1.K);
-//   expect(result).toEqual(expected.case1);
-// });
-// test("shortestSubarray should return expected result for case2", () => {
-//   const result = shortestSubarray(inputData.case2.A, inputData.case2.K);
-//   expect(result).toEqual(expected.case2);
-// });
+test("shortestSubarray should return expected result for case1", () => {
+  const result = shortestSubarray(inputData.case1.A, inputData.case1.K);
+  expect(result).toEqual(expected.case1);
+});
+test("shortestSubarray should return expected result for case2", () => {
+  const result = shortestSubarray(inputData.case2.A, inputData.case2.K);
+  expect(result).toEqual(expected.case2);
+});
 
-// test("shortestSubarray should return expected result for case3", () => {
-//   const result = shortestSubarray(inputData.case3.A, inputData.case3.K);
-//   expect(result).toEqual(expected.case3);
-// });
+test("shortestSubarray should return expected result for case3", () => {
+  const result = shortestSubarray(inputData.case3.A, inputData.case3.K);
+  expect(result).toEqual(expected.case3);
+});
 
-// test("shortestSubarray should return expected result for case4", () => {
-//   const result = shortestSubarray(inputData.case4.A, inputData.case4.K);
-//   expect(result).toEqual(expected.case4);
-// });
+test("shortestSubarray should return expected result for case4", () => {
+  const result = shortestSubarray(inputData.case4.A, inputData.case4.K);
+  expect(result).toEqual(expected.case4);
+});
 
 test("shortestSubarray should return expected result for case5", () => {
   const result = shortestSubarray(inputData.case5.A, inputData.case5.K);

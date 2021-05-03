9baf86f9f9a3e0b6b288c82e94a9228a2375ef48
Steven <gonghaima@hotmail.com>
2021-05-03 13:41:47 +1000

    backup my solution

--- test/mock/shortestSubarrayData.js
+++ test/mock/shortestSubarrayData.js
@@ -1,11 +1,15 @@
 export const inputData = {
   case1: { A: [1], K: 1 },
   case2: { A: [1, 2], K: 4 },
-  case3: { A: [2, -1, 2], K: 3 }
+  case3: { A: [2, -1, 2], K: 3 },
+  case4: { A: [84, -37, 32, 40, 95], K: 167 },
+  case5: { A: [77, 19, 35, 10, -14], K: 19 },
 };
 
 export const expected = {
   case1: 1,
   case2: -1,
-  case3: 3
+  case3: 3,
+  case4: 3,
+  case5: 1
 };

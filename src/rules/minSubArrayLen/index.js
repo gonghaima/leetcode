9eadf9bfaf2b209563ce4851605abb849a9e14dd
Steven <gonghaima@hotmail.com>
2021-05-07 11:01:16 +1000

    sliding window

--- src/rules/minSubArrayLen/index.js
+++ src/rules/minSubArrayLen/index.js
@@ -3,8 +3,28 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+/***********************************************************************************************************
+ * Runtime: 84 ms, faster than 40.81% of JavaScript online submissions for Minimum Size Subarray Sum.      *
+ * Memory Usage: 39.2 MB, less than 89.64% of JavaScript online submissions for Minimum Size Subarray Sum. *
+ ***********************************************************************************************************/
+
 var minSubArrayLen = function (target, nums) {
-    return Infinity;
+    let left = 0, minSize = Infinity, total = 0;
+    for (let right = 0; right < nums.length; right++) {
+        const currentVal = nums[right];
+        total += currentVal;
+        if (total < target) {
+            continue;
+        } else {
+            while (total >= target) {
+                minSize = Math.min(minSize, right - left + 1);
+                total -= nums[left];
+                left++;
+            }
+        }
+    }
+    return minSize === Infinity ? 0 : minSize;
 };
 
 export default minSubArrayLen;

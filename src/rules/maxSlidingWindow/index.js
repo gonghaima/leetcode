94855ff87efe567a332f464b0bd0dd18ab926865
Steven <gonghaima@hotmail.com>
2021-05-09 20:30:18 +1000

    sliding window

--- src/rules/maxSlidingWindow/index.js
+++ src/rules/maxSlidingWindow/index.js
@@ -4,8 +4,31 @@
  * @return {number[]}
  */
 
+/********************************************************************************************************
+ * Runtime: 352 ms, faster than 80.75% of JavaScript online submissions for Sliding Window Maximum.     *
+ * Memory Usage: 68.2 MB, less than 50.62% of JavaScript online submissions for Sliding Window Maximum. *
+ ********************************************************************************************************/
+
+//https://leetcode.com/problems/sliding-window-maximum/discuss/871317/Clear-thinking-process-with-PICTURE-brute-force-to-mono-deque-pythonjavajavascript
+
 var maxSlidingWindow = function (nums, k) {
-    return [];
+    const q = [];  // stores *indices*
+    const res = [];
+    for (let i = 0; i < nums.length; i++) {
+        while (q && nums[q[q.length - 1]] <= nums[i]) {
+            q.pop();
+        }
+        q.push(i);
+        // remove first element if it's outside the window
+        if (q[0] === i - k) {
+            q.shift();
+        }
+        // if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
+        if (i >= k - 1) {
+            res.push(nums[q[0]]);
+        }
+    }
+    return res;
 };
 
 export default maxSlidingWindow;

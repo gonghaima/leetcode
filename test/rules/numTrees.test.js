286d8fe060398d14c2dcfcde59beb32691c070af
Steven <gonghaima@hotmail.com>
2021-05-19 12:05:56 +1000

    numTrees - initial setup

---
+++ test/rules/numTrees.test.js
@@ -0,0 +1,14 @@
+
+import numTrees from "../../src/rules/numTrees";
+
+import { inputData, expected } from "../mock/minWindowData";
+
+test("numTrees should return expected result for case1", () => {
+  const result = numTrees(inputData.case1);
+  expect(result).toEqual(expected.case1);
+});
+
+test("numTrees should return expected result for case2", () => {
+  const result = numTrees(inputData.case2);
+  expect(result).toEqual(expected.case2);
+});

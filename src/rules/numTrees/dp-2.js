7c1aeb5eec22aadc864e5881f24a0fcfeabea2eb
Steven <gonghaima@hotmail.com>
2021-05-21 15:32:25 +1000

    math solution

--- src/rules/numTrees/dp-2.js
+++ src/rules/numTrees/dp-2.js
@@ -3,23 +3,21 @@
  * @return {number}
  */
 
-
 /************************************************************************************************************
  * Runtime: 92 ms, faster than 20.48% of JavaScript online submissions for Unique Binary Search Trees.      *
  * Memory Usage: 37.9 MB, less than 95.98% of JavaScript online submissions for Unique Binary Search Trees. *
  ************************************************************************************************************/
 
-// https://leetcode.com/problems/unique-binary-search-trees/discuss/276140/Javascript-SIMPLEST-Just-math-no-loops.-Faster-than-97.
+// https://leetcode.com/problems/unique-binary-search-trees/discuss/703049/Python-Math-oneliner-O(n)-using-Catalan-number-explained
+
+function factorial(num) {
+    if (num <= 0)
+        return 1;
+    else
+        return num * factorial(num - 1);
+}
 
 var numTrees = function (n) {
-    const trees = (lo, hi) => {
-        if (lo >= hi) return 1;
-        let total = 0;
-        for (let i = lo; i <= hi; i++)
-            total += trees(lo, i - 1) * trees(i + 1, hi);
-        return total;
-    }
-    return trees(1, n);
+    return factorial(2 * n) / (factorial(n + 1) * factorial(n));
 };
-
 export default numTrees;

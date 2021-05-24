98d0c1aea21b17597cdbe7497b77c96b7728bedf
Steven <gonghaima@hotmail.com>
2021-05-24 17:59:57 +1000

    ref

--- src/rules/maxProfit/kadane.js
+++ src/rules/maxProfit/kadane.js
@@ -6,6 +6,8 @@
  * Memory Usage: 48.7 MB, less than 64.89% of JavaScript online submissions for Best Time to Buy and Sell Stock. *
  *****************************************************************************************************************/
 
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/39038/Kadane's-Algorithm-Since-no-one-has-mentioned-about-this-so-far-%3A)-(In-case-if-interviewer-twists-the-input)
+
 const maxProfit = prices => {
   let maxCur = 0, maxSoFar = 0;
   for (let i = 1; i < prices.length; i++) {

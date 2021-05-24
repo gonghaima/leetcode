791714c99de0ce8d1f62d5e69b8f9ecd393a2043
Steven <gonghaima@hotmail.com>
2021-05-24 17:58:16 +1000

    notes

--- src/rules/maxProfit/kadane.js
+++ src/rules/maxProfit/kadane.js
@@ -1,6 +1,11 @@
 
 // Kadane's Algorithm
 
+/*****************************************************************************************************************
+ * Runtime: 100 ms, faster than 71.02% of JavaScript online submissions for Best Time to Buy and Sell Stock.     *
+ * Memory Usage: 48.7 MB, less than 64.89% of JavaScript online submissions for Best Time to Buy and Sell Stock. *
+ *****************************************************************************************************************/
+
 const maxProfit = prices => {
   let maxCur = 0, maxSoFar = 0;
   for (let i = 1; i < prices.length; i++) {

be91b9a3194df9bba5da17ae0665df142006f1a6
Steven <gonghaima@hotmail.com>
2021-05-24 18:47:51 +1000

    dp

--- src/rules/maximumSubarray/kadane.js
+++ src/rules/maximumSubarray/kadane.js
@@ -1,16 +1,15 @@
-/***************************************************************************************************
- * Runtime: 88 ms, faster than 53.09 % of JavaScript online submissions for Maximum Subarray.      *
- * Memory Usage: 39.4 MB, less than 25.63 % of JavaScript online submissions for Maximum Subarray. *
- ***************************************************************************************************/
 
-const maxSubArray = function (nums) {
-  if (!nums.length) return 0;
-  let subArrSum = nums[0], maxSum = nums[0];
+
+/**************************************************************************************************
+ * Runtime: 76 ms, faster than 95.60% of JavaScript online submissions for Maximum Subarray.      *
+ * Memory Usage: 39.4 MB, less than 34.34% of JavaScript online submissions for Maximum Subarray. *
+ **************************************************************************************************/
+
+var maxSubArray = function (nums) {
   for (let i = 1; i < nums.length; i++) {
-    subArrSum = Math.max(nums[i], subArrSum + nums[i])
-    maxSum = Math.max(maxSum, subArrSum);
+    nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
   }
-  return maxSum;
+  return Math.max(...nums)
 };
 
 export default maxSubArray;

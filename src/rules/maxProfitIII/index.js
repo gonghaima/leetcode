130f7dda5514190d59ff1af17d74cda5542beeac
Steven <gonghaima@hotmail.com>
2021-05-28 20:22:00 +1000

    O(n) solution

--- src/rules/maxProfitIII/index.js
+++ src/rules/maxProfitIII/index.js
@@ -2,8 +2,34 @@
  * @param {number[]} prices
  * @return {number}
  */
+
+/*********************************************************************************************************************
+ * Runtime: 108 ms, faster than 75.94% of JavaScript online submissions for Best Time to Buy and Sell Stock III.     *
+ * Memory Usage: 49.5 MB, less than 84.91% of JavaScript online submissions for Best Time to Buy and Sell Stock III. *
+ *********************************************************************************************************************/
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/discuss/39611/Is-it-Best-Solution-with-O(n)-O(1).
+
+// OJ: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii
+// Author: github.com/lzl124631x
+// Time: O(N)
+// Space: O(1)
+// Ref: https://discuss.leetcode.com/topic/5934/is-it-best-solution-with-o-n-o-1
+
 var maxProfit = function (prices) {
-  return Infinity;
+  let oneBuyOneSell = 0;
+  let twoBuyTwoSell = 0;
+  let oneBuy = Number.POSITIVE_INFINITY
+  let twoBuy = Number.POSITIVE_INFINITY;
+
+  for (let i = 0; i < prices.length; i++) {
+    const p = prices[i];
+    oneBuy = Math.min(oneBuy, p);
+    oneBuyOneSell = Math.max(oneBuyOneSell, p - oneBuy);
+    twoBuy = Math.min(twoBuy, p - oneBuyOneSell);
+    twoBuyTwoSell = Math.max(twoBuyTwoSell, p - twoBuy);
+  }
+  return twoBuyTwoSell;
 };
 
 export default maxProfit;

6baed2c9fc96089c32138693f9536c06caee3f43
Steven <gonghaima@hotmail.com>
2021-05-30 11:42:04 +1000

    maxProfitIV - initial setup

---
+++ src/rules/maxProfitIV/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} k
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfitIV = function (k, prices) {
+  return NaN;
+};
+
+export default maxProfitIV;
---
+++ test/rules/maxProfitIV.test.js
@@ -0,0 +1,14 @@
+import maxProfitIV from "../../src/rules/maxProfitIV";
+import { inputData, expected } from "../mock/maxProfitIVData";
+
+test("maxProfitIV should return max profit from case1", () => {
+  const { k, prices } = inputData.case1;
+  const result = maxProfitIV(k, prices);
+  expect(result).toBe(expected.case1);
+});
+
+test("maxProfitIV should return max profit from case2", () => {
+  const { k, prices } = inputData.case1;
+  const result = maxProfitIV(k, prices);
+  expect(result).toBe(expected.case2);
+});

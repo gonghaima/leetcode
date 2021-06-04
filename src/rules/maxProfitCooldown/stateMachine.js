0b70bd809c926d2b59b4f791f47bed3383992688
Steven <gonghaima@hotmail.com>
2021-06-04 22:58:24 +1000

    notes update

--- src/rules/maxProfitCooldown/stateMachine.js
+++ src/rules/maxProfitCooldown/stateMachine.js
@@ -16,11 +16,7 @@ var maxProfitCooldown = function (prices) {
   if (n <= 1)
     return 0;
   let noStock = new Array(prices.length);
-  // hold[i] is the max profit on day i with the state of holding stock
-  // hold[i] = Math.max(hold[i-1], empty[i-1]-prices[i])
   let inHand = new Array(prices.length);
-  // sold[i] is the max profit on day i with the state of having stock sold on day i
-  // sold[i] = hold[i-1] + prices[i]
   let sold = new Array(prices.length);
 
   noStock[0] = 0;

323daee61dec99db33181beb40614e40185d9679
Steven <gonghaima@hotmail.com>
2021-06-07 22:19:48 +1000

    maxSumOfThreeSubarrays-initial setup

---
+++ src/rules/maxSumOfThreeSubarrays/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSumOfThreeSubarrays = function (nums, k) {
+  return [];
+};
+
+export default maxSumOfThreeSubarrays;
---
+++ test/mock/maxSumOfThreeSubarraysData.js
@@ -0,0 +1,9 @@
+export const inputData = {
+  case1: { nums: [1, 2, 1, 2, 6, 7, 5, 1], k: 2 },
+  case2: { nums: [1, 2, 1, 2, 1, 2, 1, 2, 1], k: 2 }
+};
+
+export const expected = {
+  case1: [0, 3, 5],
+  case2: [0, 2, 4]
+};

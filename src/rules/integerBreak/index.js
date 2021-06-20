e831a01934ae73fe5c6d68f951067a79f9e61e1a
Steven Gong <gonghaima@hotmail.com>
2021-06-20 16:34:46 +1000

    dp

--- src/rules/integerBreak/index.js
+++ src/rules/integerBreak/index.js
@@ -2,8 +2,23 @@
  * @param {number} n
  * @return {number}
  */
+
+/***********************************************************************************************
+ *   RUNTIME: 76 MS, FASTER THAN 72.65% OF JAVASCRIPT ONLINE SUBMISSIONS FOR INTEGER BREAK.    *
+ * MEMORY USAGE: 38.6 MB, LESS THAN 29.85% OF JAVASCRIPT ONLINE SUBMISSIONS FOR INTEGER BREAK. *
+ ***********************************************************************************************/
+
+// break into groups of 3 till less10
+
+// https://leetcode.com/problems/integer-break/discuss/80689/A-simple-explanation-of-the-math-part-and-a-O(n)-solution
+
 var integerBreak = function (n) {
-    return Infinity;
+    const less10 = [0, 0, 1, 2, 4, 6, 9, 12, 18, 27];
+    const f = (n) => {
+        if (n < 10) return less10[n];
+        return f(n - 3) * 3;
+    }
+    return f(n);
 };
 
 export default integerBreak;
--- test/rules/integerBreak.test.js
+++ test/rules/integerBreak.test.js
@@ -1,15 +1,15 @@
 import integerBreak from "../../src/rules/integerBreak";
 import { inputData, expected } from "../mock/integerBreakData";
 
-// test("integerBreak should return correct result for case1", () => {
-//   const result = integerBreak(inputData.case1);
-//   expect(result).toEqual(expected.case1);
-// });
+test("integerBreak should return correct result for case1", () => {
+  const result = integerBreak(inputData.case1);
+  expect(result).toEqual(expected.case1);
+});
 
-// test("integerBreak should return correct result for case2", () => {
-//   const result = integerBreak(inputData.case2);
-//   expect(result).toEqual(expected.case2);
-// });
+test("integerBreak should return correct result for case2", () => {
+  const result = integerBreak(inputData.case2);
+  expect(result).toEqual(expected.case2);
+});
 
 test("integerBreak should return correct result for case3", () => {
   const result = integerBreak(inputData.case3);

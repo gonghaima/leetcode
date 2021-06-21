d1dbaca109f6defcd59574b4fdb290094186cee4
Steven Gong <gonghaima@hotmail.com>
2021-06-21 20:36:31 +1000

    countNumbersWithUniqueDigits - initial setup

---
+++ src/rules/countNumbersWithUniqueDigits/countNumbersWithUniqueDigits.md
@@ -0,0 +1,24 @@
+# [357. Count Numbers with Unique Digits](https://leetcode.com/problems/count-numbers-with-unique-digits/)
+
+Medium
+
+Given an integer n, return the count of all numbers with unique digits, x, where 0 <= x < 10n.
+
+Example 1:
+
+```s
+Input: n = 2
+Output: 91
+Explanation: The answer should be the total numbers in the range of 0 ≤ x < 100, excluding 11,22,33,44,55,66,77,88,99
+```
+
+Example 2:
+
+```s
+Input: n = 0
+Output: 1
+```
+
+Constraints:
+
+- 0 <= n <= 8
---
+++ test/rules/countNumbersWithUniqueDigits.test.js
@@ -0,0 +1,12 @@
+import countNumbersWithUniqueDigits from "../../src/rules/countNumbersWithUniqueDigits";
+import { inputData, expected } from "../mock/countNumbersWithUniqueDigitsData";
+
+test("countNumbersWithUniqueDigits should return correct result for case1", () => {
+  const result = countNumbersWithUniqueDigits(inputData.case1);
+  expect(result).toEqual(expected.case1);
+});
+
+test("countNumbersWithUniqueDigits should return correct result for case2", () => {
+  const result = countNumbersWithUniqueDigits(inputData.case2);
+  expect(result).toEqual(expected.case2);
+});

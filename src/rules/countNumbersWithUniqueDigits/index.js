720da175559108d39e2c70bd0b08f95e08c5c514
Steven Gong <gonghaima@hotmail.com>
2021-06-22 14:43:32 +1000

    my solution - not verified yet

--- src/rules/countNumbersWithUniqueDigits/index.js
+++ src/rules/countNumbersWithUniqueDigits/index.js
@@ -3,7 +3,16 @@
  * @return {number}
  */
 var countNumbersWithUniqueDigits = function (n) {
-    return Infinity;
+    if (n === 0) return 1;
+    let result = {};
+    let prev = 9;
+    for (let i = 2; i <= n; i++) {
+        const tmp = Math.pow(10, 2) - prev;
+        result[i] = tmp;
+        prev = tmp;
+    };
+
+    return result[n];
 };
 
 export default countNumbersWithUniqueDigits;

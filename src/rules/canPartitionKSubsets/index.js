1b9eaa33c363eae52f067a331d92c567dc127832
Steven Gong <gonghaima@hotmail.com>
2021-07-02 10:23:13 +1000

    canPartitionKSubsets - initial setup

---
+++ src/rules/canPartitionKSubsets/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var canPartitionKSubsets = function (nums, k) {
+    return undefined;
+};
+
+export default canPartitionKSubsets;

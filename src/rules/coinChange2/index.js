6b9abbd0c9ec6df4950d511723bbb08ce751b4e5
Steven Gong <gonghaima@hotmail.com>
2021-07-09 20:24:25 +1000

    coinChange2 - initial setup

---
+++ src/rules/coinChange2/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var coinChange2 = function (amount, coins) {
+    return Infinity;
+};
+
+export default coinChange2;
---
+++ test/rules/coinChange2.test.js
@@ -0,0 +1,21 @@
+import coinChange2 from "../../src/rules/coinChange2";
+import { inputData, expected } from "../mock/coinChange2Data";
+
+test("coinChange2 should return correct result for case1", () => {
+  const { amount, coins } = inputData.case1;
+  const result = coinChange2(amount, coins);
+  expect(result).toEqual(expected.case1);
+});
+
+test("coinChange2 should return correct result for case2", () => {
+  const { amount, coins } = inputData.case2;
+  const result = coinChange2(amount, coins);
+  expect(result).toEqual(expected.case2);
+});
+
+test("coinChange2 should return correct result for case3", () => {
+  const { amount, coins } = inputData.case3;
+  const result = coinChange2(amount, coins);
+  expect(result).toEqual(expected.case3);
+});
+

240fa9b2efd3697294ea75aa3adef3c165696f41
Steven Gong <gonghaima@hotmail.com>
2021-07-13 15:30:26 +1000

    checkRecord - initial setup

---
+++ src/rules/checkRecord/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function (s) {
+    return null;
+};
+
+export default checkRecord;
---
+++ test/mock/checkRecordData.js
@@ -0,0 +1,9 @@
+export const inputData = {
+  case1: "PPALLP",
+  case2: "PPALLL",
+};
+
+export const expected = {
+  case1: true,
+  case2: false,
+};

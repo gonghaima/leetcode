e2b73a2524a8be791718a3a2a441dcac49d55e25
Steven Gong <gonghaima@hotmail.com>
2021-07-30 18:47:35 +1000

    findLongestChain - initial setup

---
+++ src/rules/findLongestChain/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[][]} pairs
+ * @return {number}
+ */
+var findLongestChain = function (pairs) {
+    return Infinity;
+};
+
+export default findLongestChain;
---
+++ test/mock/findLongestChainData.js
@@ -0,0 +1,9 @@
+export const inputData = {
+  case1: [[1, 2], [2, 3], [3, 4]],
+  case2: [[1, 2], [7, 8], [4, 5]]
+};
+
+export const expected = {
+  case1: 2,
+  case2: 3
+};

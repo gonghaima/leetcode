ccb555f21b21c114431ec838f21d2b69e531ab48
Steven Gong <gonghaima@hotmail.com>
2021-07-31 18:20:20 +1000

    greedy

--- test/rules/findLongestChain.test.js
+++ test/rules/findLongestChain.test.js
@@ -3,15 +3,15 @@ import findLongestChain from "../../src/rules/findLongestChain";
 
 import { inputData, expected } from "../mock/findLongestChainData";
 
-test("findLongestChain should return expected result for case1", () => {
-  const result = findLongestChain(inputData.case1);
-  expect(result).toEqual(expected.case1);
-});
+// test("findLongestChain should return expected result for case1", () => {
+//   const result = findLongestChain(inputData.case1);
+//   expect(result).toEqual(expected.case1);
+// });
 
-test("findLongestChain should return expected result for case2", () => {
-  const result = findLongestChain(inputData.case2);
-  expect(result).toEqual(expected.case2);
-});
+// test("findLongestChain should return expected result for case2", () => {
+//   const result = findLongestChain(inputData.case2);
+//   expect(result).toEqual(expected.case2);
+// });
 
 test("findLongestChain should return expected result for case3", () => {
   const result = findLongestChain(inputData.case3);

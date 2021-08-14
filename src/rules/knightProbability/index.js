76200cbe7a117006eadebacc8125f0d14231907e
Steven Gong <gonghaima@hotmail.com>
2021-08-14 19:01:29 +1000

    knightProbability - initial setup

---
+++ src/rules/knightProbability/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @param {number} row
+ * @param {number} column
+ * @return {number}
+ */
+var knightProbability = function (n, k, row, column) {
+    return Infinity;
+};
+
+export default knightProbability;
---
+++ test/rules/knightProbability.test.js
@@ -0,0 +1,16 @@
+
+import knightProbability from "../../src/rules/knightProbability";
+
+import { inputData, expected } from "../mock/knightProbabilityData";
+
+test("knightProbability should return expected result for case1", () => {
+  const { n, k, row, column } = inputData.case1;
+  const result = knightProbability(n, k, row, column);
+  expect(result).toEqual(expected.case1);
+});
+
+test("knightProbability should return expected result for case2", () => {
+  const { n, k, row, column } = inputData.case2;
+  const result = knightProbability(n, k, row, column);
+  expect(result).toEqual(expected.case2);
+});

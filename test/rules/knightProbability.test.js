991c8464ee9a82397c582401367f5bb468b00fab
Steven <gonghaima@hotmail.com>
2021-08-15 18:31:28 +1000

    mine - not working for big data set

--- test/rules/knightProbability.test.js
+++ test/rules/knightProbability.test.js
@@ -9,8 +9,14 @@ test("knightProbability should return expected result for case1", () => {
   expect(result).toEqual(expected.case1);
 });
 
-test("knightProbability should return expected result for case2", () => {
+test("knightProbability should retu`rn expected result for case2", () => {
   const { n, k, row, column } = inputData.case2;
   const result = knightProbability(n, k, row, column);
   expect(result).toEqual(expected.case2);
 });
+
+// test("knightProbability should retu`rn expected result for case3", () => {
+//   const { n, k, row, column } = inputData.case3;
+//   const result = knightProbability(n, k, row, column);
+//   expect(result).toEqual(expected.case3);
+// });

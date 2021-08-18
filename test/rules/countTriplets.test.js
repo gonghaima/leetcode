d8d8119ba5d684185afe424441ae045a43939d5c
Steven <gonghaima@hotmail.com>
2021-08-18 11:18:02 +1000

    countTriplets - intial setup

---
+++ test/rules/countTriplets.test.js
@@ -0,0 +1,12 @@
+import countTriplets from "../../src/rules/countTriplets";
+import { inputData, expected } from "../mock/countTripletsData";
+
+test("countTriplets should return correct result for case1", () => {
+  const result = countTriplets(inputData.case1);
+  expect(result).toEqual(expected.case1);
+});
+
+test("countTriplets should return correct result for case2", () => {
+  const result = countTriplets(inputData.case2);
+  expect(result).toEqual(expected.case2);
+});

f95824f6fea5b8230a9e6a82c859a5c79a038c3d
Steven <gonghaima@hotmail.com>
2021-08-28 14:39:31 +1000

    O(1) solution

--- test/rules/divisorGame.test.js
+++ test/rules/divisorGame.test.js
@@ -4,11 +4,16 @@ import divisorGame from "../../src/rules/divisorGame";
 import { inputData, expected } from "../mock/divisorGameData";
 
 test("divisorGame should return expected result for case1", () => {
-  const result = divisorGame(inputData.case1);
-  expect(result).toEqual(expected.case1);
+const result = divisorGame(inputData.case1);
+expect(result).toEqual(expected.case1);
 });
 
 test("divisorGame should return expected result for case2", () => {
   const result = divisorGame(inputData.case2);
   expect(result).toEqual(expected.case2);
+});
+
+test("divisorGame should return expected result for case3", () => {
+  const result = divisorGame(inputData.case3);
+  expect(result).toEqual(expected.case3);
 });

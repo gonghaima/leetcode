ad51514f3fc6ae0eadf18c9112ea771eb357f2a7
Steven <gonghaima@hotmail.com>
2021-09-04 13:53:47 +1000

    notes update

--- src/rules/maxSumAfterPartitioning/buttomUp.js
+++ src/rules/maxSumAfterPartitioning/buttomUp.js
@@ -4,29 +4,33 @@
  * @return {number}
  */
 
-// https://leetcode.com/problems/partition-array-for-maximum-sum/discuss/290863/JavaC%2B%2BPython-DP-O(K)-Space
+// https://leetcode.com/problems/partition-array-for-maximum-sum/discuss/1192230/Java-Bottom-Up-DP-Solution
 
+/*****************************************************************************************************************
+ *   Runtime: 149 ms, faster than 21.05% of JavaScript online submissions for Partition Array for Maximum Sum.   *
+ * Memory Usage: 38.9 MB, less than 94.74% of JavaScript online submissions for Partition Array for Maximum Sum. *
+ *****************************************************************************************************************/
 
 //  Time O(NK)
 //  Space O(N)
 
 var maxSumAfterPartitioning = function (arr, k) {
   const dp = new Array(arr.length + 1).fill(null);
-        for(let i = 1; i <= arr.length; i++){
-            let dpMax = Number.MIN_VALUE;
-            let max= Number.MIN_VALUE;
-            for(let j = 0; j < k; j++){
-                if(i - j - 1 >= 0){
-                    max = Math.max(max, arr[i-j-1]);
-                    dpMax = Math.max(dpMax, dp[i-j-1] + (j + 1)*max );
-                }
-                else{
-                    break;
-                }
-            }
-            dp[i] = dpMax;
-        }
-        return dp[arr.length];
+  for (let i = 1; i <= arr.length; i++) {
+    let dpMax = Number.MIN_VALUE;
+    let max = Number.MIN_VALUE;
+    for (let j = 0; j < k; j++) {
+      if (i - j - 1 >= 0) {
+        max = Math.max(max, arr[i - j - 1]);
+        dpMax = Math.max(dpMax, dp[i - j - 1] + (j + 1) * max);
+      }
+      else {
+        break;
+      }
+    }
+    dp[i] = dpMax;
+  }
+  return dp[arr.length];
 };
 
 export default maxSumAfterPartitioning;

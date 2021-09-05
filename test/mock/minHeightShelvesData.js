53593fa857c0a5e66c5bb5f6f95e24a9499088a0
Steven <gonghaima@hotmail.com>
2021-09-05 11:33:54 +1000

    minHeightShelves - initial setup

---
+++ test/mock/minHeightShelvesData.js
@@ -0,0 +1,9 @@
+export const inputData = {
+  case1: { books: [[1, 1], [2, 3], [2, 3], [1, 1], [1, 1], [1, 1], [1, 2]], shelf_width: 4 },
+  case2: { books: [[1, 3], [2, 4], [3, 2]], shelfWidth: 6 }
+};
+
+export const expected = {
+  case1: 6,
+  case2: 4
+};

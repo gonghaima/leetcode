9f924dc0d18e554a8a5e2705c565269d31b5d5fc
Steven <gonghaima@hotmail.com>
2021-09-11 21:16:41 +1000

    dp

--- src/rules/longestSubsequence/index.js
+++ src/rules/longestSubsequence/index.js
@@ -3,8 +3,23 @@
  * @param {number} difference
  * @return {number}
  */
+
+// https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/discuss/398188/5-lines.-OMG-can't-believe!!!
+// https://www.youtube.com/watch?v=1THU4Aa9akQ
+
+/***********************************************************************************************************************************
+ *  Runtime: 256 ms, faster than 10.53% of JavaScript online submissions for Longest Arithmetic Subsequence of Given Difference.   *
+ * Memory Usage: 61.5 MB, less than 7.89% of JavaScript online submissions for Longest Arithmetic Subsequence of Given Difference. *
+ ***********************************************************************************************************************************/
+
 var longestSubsequence = function (arr, difference) {
-    return Infinity;
+    const dp = {};
+    let longest = 0;
+    for (const item of arr) {
+        dp[item] = (dp[item - difference] || 0) + 1;
+        longest = Math.max(longest, dp[item]);
+    }
+    return longest;
 };
 
 export default longestSubsequence;
--- test/rules/longestSubsequence.test.js
+++ test/rules/longestSubsequence.test.js
@@ -2,19 +2,25 @@
 import longestSubsequence from "../../src/rules/longestSubsequence";
 import { inputData, expected } from "../mock/longestSubsequenceData";
 
-test("longestSubsequence should return expected result for case1", () => {
-  const { arr, difference } = inputData.case1;
-  const result = longestSubsequence(arr, difference);
-  expect(result).toEqual(expected.case1);
-});
-test("longestSubsequence should return expected result for case2", () => {
-  const { arr, difference } = inputData.case2;
-  const result = longestSubsequence(arr, difference);
-  expect(result).toEqual(expected.case2);
-});
+// test("longestSubsequence should return expected result for case1", () => {
+//   const { arr, difference } = inputData.case1;
+//   const result = longestSubsequence(arr, difference);
+//   expect(result).toEqual(expected.case1);
+// });
+// test("longestSubsequence should return expected result for case2", () => {
+//   const { arr, difference } = inputData.case2;
+//   const result = longestSubsequence(arr, difference);
+//   expect(result).toEqual(expected.case2);
+// });
 
 test("longestSubsequence should return expected result for case3", () => {
   const { arr, difference } = inputData.case3;
   const result = longestSubsequence(arr, difference);
   expect(result).toEqual(expected.case3);
 });
+
+// test("longestSubsequence should return expected result for case4", () => {
+//   const { arr, difference } = inputData.case4;
+//   const result = longestSubsequence(arr, difference);
+//   expect(result).toEqual(expected.case4);
+// });

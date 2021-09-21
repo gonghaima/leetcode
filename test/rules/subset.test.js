69146bc37d308dbaf8988cb9e5e68d3c17e24e28
Steven <gonghaima@hotmail.com>
2021-09-21 20:38:57 +1000

    backtrack

--- test/rules/subset.test.js
+++ test/rules/subset.test.js
@@ -4,7 +4,7 @@ import { inputData, expected } from "../mock/subsetsData";
 
 test("subsets should return expected result for case1", () => {
   const result = subsets(inputData.case1);
-  expect(result).toEqual(expected.case1);
+  expect(result.sort()).toEqual(expected.case1.sort());
 });
 test("subsets should return expected result for case2", () => {
   const result = subsets(inputData.case2);

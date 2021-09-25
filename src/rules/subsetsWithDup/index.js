a38a735b42ba6956ed504088b777cf57fa35febc
Steven <gonghaima@hotmail.com>
2021-09-25 10:43:29 +1000

    dp solution

--- src/rules/subsetsWithDup/index.js
+++ src/rules/subsetsWithDup/index.js
@@ -2,8 +2,28 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+/********************************************************************************************
+ *   Runtime: 76 ms, faster than 85.95% of JavaScript online submissions for Subsets II.    *
+ * Memory Usage: 41.8 MB, less than 16.54% of JavaScript online submissions for Subsets II. *
+ ********************************************************************************************/
+
+// idea from the subsets solution
+
 var subsetsWithDup = function (nums) {
-    return [];
+    let res = [[]];
+    let visited = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        res.forEach(item => {
+            const newItem = [...item, nums[i]].sort();
+            const newItemIndex = newItem.toString();
+            if (!visited.has(newItemIndex)) {
+                res.push(newItem);
+                visited.add(newItemIndex);
+            }
+        });
+    }
+    return res;
 };
 
 export default subsetsWithDup;

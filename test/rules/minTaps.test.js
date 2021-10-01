37cedb5a8701fc034ea7c976104ba6f5cf33f874
Steven <gonghaima@hotmail.com>
2021-10-01 20:17:00 +1000

    minTaps - initial setup

---
+++ test/rules/minTaps.test.js
@@ -0,0 +1,36 @@
+
+import minTaps from "../../src/rules/minTaps";
+import { inputData, expected } from "../mock/minTapsData";
+
+test("minTaps should return expected result for case1", () => {
+  const { n, ranges } = inputData.case1;
+  const result = minTaps(n, ranges);
+  expect(result).toEqual(expected.case1);
+});
+
+test("minTaps should return expected result for case2", () => {
+  const { n, ranges } = inputData.case2;
+  const result = minTaps(n, ranges);
+  expect(result).toEqual(expected.case2);
+});
+
+test("minTaps should return expected result for case3", () => {
+  const { n, ranges } = inputData.case3;
+  const result = minTaps(n, ranges);
+  expect(result).toEqual(expected.case3);
+});
+
+test("minTaps should return expected result for case4", () => {
+  const { n, ranges } = inputData.case4;
+  const result = minTaps(n, ranges);
+  expect(result).toEqual(expected.case4);
+});
+
+test("minTaps should return expected result for case5", () => {
+  const { n, ranges } = inputData.case5;
+  const result = minTaps(n, ranges);
+  expect(result).toEqual(expected.case5);
+});
+
+
+

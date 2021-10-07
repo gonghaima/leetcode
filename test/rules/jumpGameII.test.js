ef88e2cc1b4507f91fd24906f06e1ff74d8aed11
Steven <gonghaima@hotmail.com>
2021-10-07 15:27:33 +1100

    rename

--- test/rules/jumpGameII.test.js
+++ test/rules/jumpGameII.test.js
@@ -1,5 +1,5 @@
 // import jump from "../../src/rules/jump/bfs";
-import jump from "../../src/rules/jump/interative";
+import jump from "../../src/rules/jump/iterative";
 import { inputData, expected } from "../mock/jumpData";
 
 test("jump should return the distance for case1", () => {

2abd648644b52d434e99f58e963ff8a330ab20ca
Steven <gonghaima@hotmail.com>
2021-10-09 10:35:20 +1100

    bf with memo

--- test/rules/jumpGameII.test.js
+++ test/rules/jumpGameII.test.js
@@ -1,5 +1,5 @@
 // import jump from "../../src/rules/jump/bfs";
-import jump from "../../src/rules/jump/bruteForce";
+import jump from "../../src/rules/jump/bruteForce-memo";
 import { inputData, expected } from "../mock/jumpData";
 
 test("jump should return the distance for case1", () => {

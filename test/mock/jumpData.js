99e4873eae4bbe2aab12dc2f63a14e9570752b4c
Steven <gonghaima@hotmail.com>
2021-10-13 10:49:35 +1100

    my thoughts based on bfs idea, case5 not passing, leave it for now

--- test/mock/jumpData.js
+++ test/mock/jumpData.js
@@ -1,9 +1,15 @@
 export const inputData = {
   case1: [2, 3, 1, 1, 4],
   case2: [2, 3, 0, 1, 4],
+  case3: [0],
+  case4: [1,2],
+  case5: [1,2,3]
 };
 
 export const expected = {
   case1: 2,
   case2: 2,
+  case3: 0,
+  case4: 1,
+  case5: 2,
 };

1115bc4d6214da6fd0c6812ffdc410e2a82f1bb3
Steven <gonghaima@hotmail.com>
2021-10-15 12:58:09 +1100

    dp

--- src/rules/jump/index.js
+++ src/rules/jump/index.js
@@ -2,7 +2,21 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
+// https://leetcode.com/problems/jump-game/discuss/20900/Simplest-O(N)-solution-with-constant-space
+// O(n) time    O(1) space
+
+/*****************************************************************************************
+ *  Runtime: 88 ms, faster than 75.36% of JavaScript online submissions for Jump Game.   *
+ * Memory Usage: 43 MB, less than 59.33% of JavaScript online submissions for Jump Game. *
+ *****************************************************************************************/
+
 var canJump = function (nums) {
-  return null;
+  const n = nums.length;
+  let last = n - 1;
+  for (let i = n - 2; i >= 0; i--) {
+    if (i + nums[i] >= last) last = i;
+  }
+  return last <= 0;
 };
 export default canJump;
--- test/rules/jumpGame.test.js
+++ test/rules/jumpGame.test.js
@@ -1,4 +1,4 @@
-import jump from "../../src/rules/jump/myThoughts";
+import jump from "../../src/rules/jump";
 import { inputData, expected } from "../data/jumpData";
 
 test("jump should return the distance for case1", () => {
@@ -6,17 +6,17 @@ test("jump should return the distance for case1", () => {
   expect(result).toBe(expected.case1);
 });
 
-// test("jump should return the distance for case2", () => {
-//   const result = jump(inputData.case2);
-//   expect(result).toBe(expected.case2);
-// });
+test("jump should return the distance for case2", () => {
+  const result = jump(inputData.case2);
+  expect(result).toBe(expected.case2);
+});
 
-// test("jump should return the distance for case3", () => {
-//   const result = jump(inputData.case3);
-//   expect(result).toBe(expected.case3);
-// });
+test("jump should return the distance for case3", () => {
+  const result = jump(inputData.case3);
+  expect(result).toBe(expected.case3);
+});
 
-// test("jump should return the distance for case4", () => {
-//   const result = jump(inputData.case4);
-//   expect(result).toBe(expected.case4);
-// });
+test("jump should return the distance for case4", () => {
+  const result = jump(inputData.case4);
+  expect(result).toBe(expected.case4);
+});

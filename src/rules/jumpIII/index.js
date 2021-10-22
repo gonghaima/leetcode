7a0bcc38c45652b0e5cc7678323c6304449e2004
Steven <gonghaima@hotmail.com>
2021-10-22 14:14:22 +1100

    dfs

--- src/rules/jumpIII/index.js
+++ src/rules/jumpIII/index.js
@@ -4,65 +4,19 @@
  * @return {boolean}
  */
 
-// union find
+// dfs
+
+// time O(n),  space O(n)
+
+// https://leetcode.com/problems/jump-game-iii/discuss/465602/JavaC%2B%2BPython-1-Line-Recursion
+
+/***********************************************************************************************
+ *   Runtime: 119 ms, faster than 36.40% of JavaScript online submissions for Jump Game III.   *
+ * Memory Usage: 49.4 MB, less than 54.81% of JavaScript online submissions for Jump Game III. *
+ ***********************************************************************************************/
 
 var canReach = function (arr, start) {
-  // index with value 0
-  let end = null;
-
-
-  // build graph
-  const graph = {};
-  for (let i = 0; i < arr.length; i++) {
-    const itemValue = arr[i];
-
-    // set end index
-    if (itemValue === 0) {
-      end = i;
-      continue;
-    }
-
-    const prevIndex = i - itemValue >= 0 ? i - itemValue : null;
-    const nextIndex = (i + itemValue) <= (arr.length - 1) ? (i + itemValue) : null;
-    if (!graph[i] && graph[i] !== 0) {
-      graph[i] = [];
-    }
-
-    if (i === 5);
-    if (prevIndex) graph[i].push(prevIndex);
-    if (nextIndex) graph[i].push(nextIndex);
-  }
-  // path compression
-
-  // find the ancestor of the start
-  // bfs - find all parents & including ancestors - if index is end index
-  ;
-  let reachable = false;
-
-  let visited = new Set();
-  let next = [start];
-  ;
-
-  while (next.length && !reachable) {
-    ;
-    let tmp = [];
-    for (let i = 0; i < next.length; i++) {
-      const parentIdx = next[i];
-      const parents = graph[parentIdx];
-      const parentsSet = new Set(parents);
-
-      if (parentsSet.has(end)) reachable = true;
-      parents.map(parent => {
-        if (!visited.has(parent)) {
-          tmp.push(parent);
-          visited.add(parent);
-        }
-      })
-    };
-    next = tmp;;
-  };
-
-  return reachable;
-};
+  return 0 <= start && start < arr.length && arr[start] >= 0 && ((arr[start] = -arr[start]) === 0 || canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]));
+}
 
 export default canReach;

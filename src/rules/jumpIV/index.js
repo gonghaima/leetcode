6b37eca5daaf5200a990b590bb097cb00def1345
Steven <gonghaima@hotmail.com>
2021-10-29 11:29:07 +1100

    bfs - pass all cases

--- src/rules/jumpIV/index.js
+++ src/rules/jumpIV/index.js
@@ -2,8 +2,66 @@
  * @param {number[]} arr
  * @return {number}
  */
+
+/**********************************************************************************************
+ *  Runtime: 208 ms, faster than 100.00% of JavaScript online submissions for Jump Game IV.   *
+ * Memory Usage: 70.4 MB, less than 64.91% of JavaScript online submissions for Jump Game IV. *
+ **********************************************************************************************/
+
+
 var minJumps = function (arr) {
-  return Infinity;
+  let valuePositionsMap = getValuePositionsMap(arr);
+  let mySet = new Set();
+  mySet.add(0);
+  let aoArray = [0];
+  let counter = 0;
+  while (!mySet.has(arr.length - 1)) {
+    counter++;
+    let newArray = [];
+    for (let currP of aoArray) {
+      for (let item of findAllOptions(arr, currP, mySet, valuePositionsMap)) {
+        newArray.push(item);
+        mySet.add(item);
+        if (item == arr.length - 1) {
+          break;
+        }
+      }
+      if (mySet.has(arr.length - 1)) {
+        break;
+      }
+    }
+    aoArray = newArray;
+  }
+  return counter;
 };
 
+function getValuePositionsMap(arr) {
+  let result = new Map();
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (!result.has(arr[i])) {
+      result.set(arr[i], []);
+    }
+    result.get(arr[i]).push(i);
+  }
+  return result;
+}
+
+function findAllOptions(arr, currP, mySet, valuePositionsMap) {
+  let result = [];
+  if (currP > 0 && arr[currP - 1] != arr[currP] && !mySet.has(currP - 1)) {
+    result.push(currP - 1);
+  }
+  if (currP < arr.length - 1 && arr[currP + 1] != arr[currP] && !mySet.has(currP + 1)) {
+    result.push(currP + 1);
+  }
+  if (valuePositionsMap.has(arr[currP])) {
+    for (let i of valuePositionsMap.get(arr[currP])) {
+      if (i != currP && !mySet.has(i)) {
+        result.push(i);
+      }
+    }
+    valuePositionsMap.delete(arr[currP]);
+  }
+  return result;
+}
 export default minJumps;

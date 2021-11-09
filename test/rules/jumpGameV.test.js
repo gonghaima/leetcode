8edc08bd7e29f8ffcc75f7358951e4dc265ce1b5
Steven <gonghaima@hotmail.com>
2021-11-09 19:21:57 +1100

    monotoneStack1

--- test/rules/jumpGameV.test.js
+++ test/rules/jumpGameV.test.js
@@ -1,4 +1,4 @@
-import jump from "../../src/rules/jumpV/segmentTree";
+import jump from "../../src/rules/jumpV/monotoneStack1";
 import { inputData, expected } from "../data/jumpVData";
 
 test("jump should return the distance for case1", () => {
@@ -7,32 +7,32 @@ test("jump should return the distance for case1", () => {
   expect(result).toBe(expected.case1);
 });
 
-// test("jump should return the distance for case2", () => {
-//   const { arr, d } = inputData.case2;
-//   const result = jump(arr, d);
-//   expect(result).toBe(expected.case2);
-// });
+test("jump should return the distance for case2", () => {
+  const { arr, d } = inputData.case2;
+  const result = jump(arr, d);
+  expect(result).toBe(expected.case2);
+});
 
-// test("jump should return the distance for case3", () => {
-//   const { arr, d } = inputData.case3;
-//   const result = jump(arr, d);
-//   expect(result).toBe(expected.case3);
-// });
+test("jump should return the distance for case3", () => {
+  const { arr, d } = inputData.case3;
+  const result = jump(arr, d);
+  expect(result).toBe(expected.case3);
+});
 
-// test("jump should return the distance for case4", () => {
-//   const { arr, d } = inputData.case4;
-//   const result = jump(arr, d);
-//   expect(result).toBe(expected.case4);
-// });
+test("jump should return the distance for case4", () => {
+  const { arr, d } = inputData.case4;
+  const result = jump(arr, d);
+  expect(result).toBe(expected.case4);
+});
 
-// test("jump should return the distance for case5", () => {
-//   const { arr, d } = inputData.case5;
-//   const result = jump(arr, d);
-//   expect(result).toBe(expected.case5);
-// });
+test("jump should return the distance for case5", () => {
+  const { arr, d } = inputData.case5;
+  const result = jump(arr, d);
+  expect(result).toBe(expected.case5);
+});
 
-// test("jump should return the distance for case6", () => {
-//   const { arr, d } = inputData.case6;
-//   const result = jump(arr, d);
-//   expect(result).toBe(expected.case6);
-// });
+test("jump should return the distance for case6", () => {
+  const { arr, d } = inputData.case6;
+  const result = jump(arr, d);
+  expect(result).toBe(expected.case6);
+});

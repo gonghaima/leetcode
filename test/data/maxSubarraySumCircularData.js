5db50e0227fe75e5508a561424cd7cefbbd5f0b3
Steven <gonghaima@hotmail.com>
2021-11-18 21:18:54 +1100

    dp

--- test/data/maxSubarraySumCircularData.js
+++ test/data/maxSubarraySumCircularData.js
@@ -1,9 +1,15 @@
 export const inputData = {
   case1: [1, -2, 3, -2],
   case2: [5, -3, 5],
+  case3: [3, -1, 2, -1],
+  case4: [3, -2, 2, -3],
+  case5: [-2, -3, -1],
 };
 
 export const expected = {
   case1: 3,
   case2: 10,
+  case3: 4,
+  case4: 3,
+  case5: -1,
 };
--- test/rules/maxSubarraySumCircular.test.js
+++ test/rules/maxSubarraySumCircular.test.js
@@ -1,12 +1,27 @@
-import maxSubarraySumCircular from "../../src/rules/maxSubarraySumCircular";
-import { inputData, expected } from "../data/maxSubarraySumCircularData";
+import maxSubarraySumCircular from '../../src/rules/maxSubarraySumCircular';
+import { inputData, expected } from '../data/maxSubarraySumCircularData';
 
-test("maxSubarraySumCircular should return correct result for case1", () => {
+test('maxSubarraySumCircular should return correct result for case1', () => {
   const result = maxSubarraySumCircular(inputData.case1);
   expect(result).toEqual(expected.case1);
 });
 
-test("maxSubarraySumCircular should return correct result for case2", () => {
+test('maxSubarraySumCircular should return correct result for case2', () => {
   const result = maxSubarraySumCircular(inputData.case2);
   expect(result).toEqual(expected.case2);
+});
+
+test('maxSubarraySumCircular should return correct result for case3', () => {
+  const result = maxSubarraySumCircular(inputData.case3);
+  expect(result).toEqual(expected.case3);
+});
+
+test('maxSubarraySumCircular should return correct result for case4', () => {
+  const result = maxSubarraySumCircular(inputData.case4);
+  expect(result).toEqual(expected.case4);
+});
+
+test('maxSubarraySumCircular should return correct result for case5', () => {
+  const result = maxSubarraySumCircular(inputData.case5);
+  expect(result).toEqual(expected.case5);
 });

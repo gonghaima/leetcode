0e78f015befbb4de59cfa5b8054042c69edff480
Steven <gonghaima@hotmail.com>
2021-11-21 10:18:32 +1100

    my dp solution - three test cases pass

--- test/data/jumpVIIData.js
+++ test/data/jumpVIIData.js
@@ -1,9 +1,11 @@
 export const inputData = {
   case1: { s: '011010', minJump: 2, maxJump: 3 },
   case2: { s: '01101110', minJump: 2, maxJump: 3 },
+  case3: { s: '01', minJump: 1, maxJump: 1 },
 };
 
 export const expected = {
   case1: true,
   case2: false,
+  case3: false,
 };

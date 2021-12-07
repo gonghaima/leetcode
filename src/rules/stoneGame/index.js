2eeb55bc019a2db875e9cbdfdeb147f80d9399cc
Steven <gonghaima@hotmail.com>
2021-12-07 13:32:51 +1100

    stoneGame - initial setup

---
+++ src/rules/stoneGame/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+ var stoneGame = function(piles) {
+    return null;
+};
+
+export default stoneGame;
---
+++ test/rules/stoneGame.test.js
@@ -0,0 +1,12 @@
+import stoneGame from '../../src/rules/stoneGame';
+import { inputData, expected } from '../data/stoneGameData';
+
+test('stoneGame should return expected result for case1', () => {
+  const result = stoneGame(inputData.case1);
+  expect(result).toEqual(expected.case1);
+});
+
+test('stoneGame should return expected result for case2', () => {
+  const result = stoneGame(inputData.case2);
+  expect(result).toEqual(expected.case2);
+});

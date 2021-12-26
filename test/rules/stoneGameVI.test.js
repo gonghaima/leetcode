a90cbc33847af4c9e966da096d3a50b646c177e0
Steven <gonghaima@hotmail.com>
2021-12-27 02:09:55 +1100

    solution based on hint

--- test/rules/stoneGameVI.test.js
+++ test/rules/stoneGameVI.test.js
@@ -2,16 +2,19 @@ import stoneGameVI from '../../src/rules/stoneGameVI';
 import { inputData, expected } from '../data/stoneGameVIData';
 
 test('stoneGameVI should return expected result for case1', () => {
-  const result = stoneGameVI(inputData.case1);
+  const {aliceValues, bobValues} = inputData.case1;
+  const result = stoneGameVI(aliceValues, bobValues);
   expect(result).toEqual(expected.case1);
 });
 
 test('stoneGameVI should return expected result for case2', () => {
-  const result = stoneGameVI(inputData.case2);
+  const {aliceValues, bobValues} = inputData.case2;
+  const result = stoneGameVI(aliceValues, bobValues);
   expect(result).toEqual(expected.case2);
 });
 
 test('stoneGameVI should return expected result for case3', () => {
-  const result = stoneGameVI(inputData.case3);
+  const {aliceValues, bobValues} = inputData.case3;
+  const result = stoneGameVI(aliceValues, bobValues);
   expect(result).toEqual(expected.case3);
 });

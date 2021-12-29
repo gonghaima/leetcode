1b5d48335c3f839076c25a4ce82a8e9d228d42b7
Steven <gonghaima@hotmail.com>
2021-12-30 08:13:42 +1100

    thought

--- src/rules/stoneGameVII/index.js
+++ src/rules/stoneGameVII/index.js
@@ -3,6 +3,11 @@
  * @return {number}
  */
 var stoneGameVII = function(stones) {
+  /**
+   * Alice:  aliceTotal+= Max(arr[0], arr[arr.length-1]);
+   * Bob: bobTotal+= Max(dp(arr));
+   * return aliceTotal -bobTotal;
+   */
   return NaN;
 };
 

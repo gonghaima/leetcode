a715d32e0ac2a0a54de719958842d4bdd3a1d76c
Steven <gonghaima@hotmail.com>
2022-01-06 20:19:58 +1100

    dp

--- src/rules/stoneGameVIII/index.js
+++ src/rules/stoneGameVIII/index.js
@@ -2,12 +2,24 @@
  * @param {number[]} stones
  * @return {number}
  */
+
+/************************************************************************************************
+ * Runtime: 143 ms, faster than 100.00% of JavaScript online submissions for Stone Game VIII.   *
+ * Memory Usage: 50 MB, less than 100.00% of JavaScript online submissions for Stone Game VIII. *
+ ************************************************************************************************/
+
+// https://leetcode.com/problems/stone-game-viii/discuss/1224640/C%2B%2BPython-DP-O(N)-time-O(1)-space-(2-lines-1-line)
+
 var stoneGameVIII = function(stones) {
   /**
    * if dp is the maxValue from a given array
    * diff = max[stones] - max[remainingStonesArray]
    */
-  return NaN;
+  const N = stones.length;
+  for (let i = 1; i < N; ++i) stones[i] += stones[i - 1]; // now A[i] is the prefix sum, i.e. prefix[i]
+  let dp = stones[N - 1]; // dp[N - 2] = prefix[N - 1]
+  for (let i = N - 2; i > 0; --i) dp = Math.max(dp, stones[i] - dp); // dp[i - 1] = max(dp[i], A[i] - dp[i])
+  return dp; // dp[0]
 };
 
 export default stoneGameVIII;

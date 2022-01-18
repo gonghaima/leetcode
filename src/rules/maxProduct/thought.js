b6ffb1e8935a7e371e6b913c5d34065da69fae26
Steven <gonghaima@hotmail.com>
2022-01-18 18:05:35 +1100

    thought

--- src/rules/maxProduct/thought.js
+++ src/rules/maxProduct/thought.js
@@ -3,6 +3,8 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
+  // greedy
+
   // just two base cases, either take current, or not take current
   // dp[i] = Math.max(dp[i-1]+dp[i], dp[i])
   // finally return dp[nums.length]

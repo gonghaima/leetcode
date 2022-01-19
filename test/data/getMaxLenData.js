8ad0e12982cfd8c18578cb281473aa93d0d85c8b
Steven <gonghaima@hotmail.com>
2022-01-19 20:44:26 +1100

    dp

--- test/data/getMaxLenData.js
+++ test/data/getMaxLenData.js
@@ -1,9 +1,11 @@
 export const inputData = {
-  case1: [2, 3, -2, 4],
-  case2: [-2, 0, -1],
+  case1: [1, -2, -3, 4],
+  case2: [0, 1, -2, -3, -4],
+  case3: [-1, -2, -3, 0, 1],
 };
 
 export const expected = {
-  case1: 6,
-  case2: 0,
+  case1: 4,
+  case2: 3,
+  case3: 2,
 };

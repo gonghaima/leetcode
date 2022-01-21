852e99ae4cd4423de541dbfa69f807c0d41a4274
Steven <gonghaima@hotmail.com>
2022-01-21 20:04:33 +1100

    dp

--- src/rules/wordBreak/index.js
+++ src/rules/wordBreak/index.js
@@ -3,7 +3,27 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
+
+// https://leetcode.com/problems/word-break/discuss/43790/Java-implementation-using-DP-in-two-ways
+
+/********************************************************************************************
+ *   Runtime: 113 ms, faster than 31.39% of JavaScript online submissions for Word Break.   *
+ * Memory Usage: 40.4 MB, less than 77.20% of JavaScript online submissions for Word Break. *
+ ********************************************************************************************/
+
 var wordBreak = function(s, wordDict) {
-  return null;
+  const f = new Array(s.length + 1).fill(false);
+  const dict = new Set(wordDict);
+  f[0] = true;
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (f[j] && dict.has(s.substring(j, i))) {
+        f[i] = true;
+        break;
+      }
+    }
+  }
+
+  return f[s.length];
 };
 export default wordBreak;
--- test/rules/wordBreak.test.js
+++ test/rules/wordBreak.test.js
@@ -2,16 +2,19 @@ import wordBreak from '../../src/rules/wordBreak';
 import { inputData, expected } from '../data/wordBreakData';
 
 test('wordBreak should return expected result for case1', () => {
-  const result = wordBreak(inputData.case1);
+  const { s, wordDict } = inputData.case1;
+  const result = wordBreak(s, wordDict);
   expect(result).toEqual(expected.case1);
 });
 
 test('wordBreak should return expected result for case2', () => {
-  const result = wordBreak(inputData.case2);
+  const { s, wordDict } = inputData.case2;
+  const result = wordBreak(s, wordDict);
   expect(result).toEqual(expected.case2);
 });
 
 test('wordBreak should return expected result for case3', () => {
-  const result = wordBreak(inputData.case3);
+  const { s, wordDict } = inputData.case3;
+  const result = wordBreak(s, wordDict);
   expect(result).toEqual(expected.case3);
 });

b9003283fd1dd0ff57887998417fc139b33bc3f9
Steven <gonghaima@hotmail.com>
2022-01-23 07:55:43 +1100

    uglyNumberII - initial setup

--- src/rules/uglyNumberII/index.js
+++ src/rules/uglyNumberII/index.js
@@ -3,6 +3,19 @@
  * @return {number}
  */
 var nthUglyNumber = function(n) {
-  return NaN;
+  if(n <= 0) return false; // get rid of corner cases 
+  if(n == 1) return true; // base case
+  let t2 = 0, t3 = 0, t5 = 0; //pointers for 2, 3, 5
+  // vector<int> k(n);
+  const k = Array(n).fill(0)
+  k[0] = 1;
+  for(let i  = 1; i < n ; i ++)
+  {
+      k[i] = Math.min(k[t2]*2,Math.min(k[t3]*3,k[t5]*5));
+      if(k[i] == k[t2]*2) t2++; 
+      if(k[i] == k[t3]*3) t3++;
+      if(k[i] == k[t5]*5) t5++;
+  }
+  return k[n-1];
 };
 export default nthUglyNumber;

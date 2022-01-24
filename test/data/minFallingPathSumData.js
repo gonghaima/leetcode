9582a5076ca467923e97f920f05e2fdf6fa719ec
Steven <gonghaima@hotmail.com>
2022-01-24 20:08:20 +1100

    minFallingPathSum - initial setup

---
+++ test/data/minFallingPathSumData.js
@@ -0,0 +1,16 @@
+export const inputData = {
+  case1: [
+    [2, 1, 3],
+    [6, 5, 4],
+    [7, 8, 9],
+  ],
+  case2: [
+    [-19, 57],
+    [-40, -5],
+  ],
+};
+
+export const expected = {
+  case1: 13,
+  case2: -59,
+};

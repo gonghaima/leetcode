ff29149a0b52463f690f012812d7413709dc1be6
Steven <gonghaima@hotmail.com>
2022-01-26 20:20:12 +1100

    dp

--- test/rules/matrixBlockSum.test.js
+++ test/rules/matrixBlockSum.test.js
@@ -4,11 +4,11 @@ import { inputData, expected } from '../data/matrixBlockSumData';
 test('matrixBlockSum should return max profit from case1', () => {
   const { mat, k } = inputData.case1;
   const result = matrixBlockSum(mat, k);
-  expect(result).toBe(expected.case1);
+  expect(result.sort()).toEqual(expected.case1.sort());
 });
 
 test('matrixBlockSum should return max profit from case2', () => {
   const { mat, k } = inputData.case2;
   const result = matrixBlockSum(mat, k);
-  expect(result).toBe(expected.case2);
+  expect(result.sort()).toEqual(expected.case2.sort());
 });

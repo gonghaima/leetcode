4363cf4b620ad7bfdcf4d68ee2c27afb7a78513d
Steven <gonghaima@hotmail.com>
2022-01-27 20:23:35 +1100

    uniquePaths - dp

--- src/rules/uniquePaths/index.js
+++ src/rules/uniquePaths/index.js
@@ -3,8 +3,22 @@
  * @param {number} n
  * @return {number}
  */
+
+// https://leetcode.com/problems/unique-paths/discuss/22954/C%2B%2B-DP
+
+/*******************************************************************************************
+ *  Runtime: 64 ms, faster than 98.52% of JavaScript online submissions for Unique Paths.  *
+ * Memory Usage: 42 MB, less than 5.05% of JavaScript online submissions for Unique Paths. *
+ *******************************************************************************************/
+
 var uniquePaths = function(m, n) {
-  return NaN;
+  const cur = new Array(n).fill(1);
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      cur[j] += cur[j - 1];
+    }
+  }
+  return cur[n - 1];
 };
 
 export default uniquePaths;

d7de71272875c2fb64e96fec01552e512340d180
Steven <gonghaima@hotmail.com>
2022-01-30 22:50:28 +1100

    my solution

--- src/rules/isSubsequence/index.js
+++ src/rules/isSubsequence/index.js
@@ -3,8 +3,18 @@
  * @param {string} t
  * @return {boolean}
  */
+
+/***********************************************************************************************
+ * Runtime: 82 ms, faster than 50.64% of JavaScript online submissions for Is Subsequence.     *
+ * Memory Usage: 43.7 MB, less than 5.12% of JavaScript online submissions for Is Subsequence. *
+ ***********************************************************************************************/
+
 var isSubsequence = function(s, t) {
-  return null;
+  const sArray = s.split('');
+  for (const character of t) {
+    if (character === sArray[0]) sArray.shift();
+  }
+  return sArray.length === 0;
 };
 
 export default isSubsequence;

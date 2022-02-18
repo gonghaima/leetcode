2d779c2e66e3511aa6a070d09b3ec1d78944c8f4
Steven <gonghaima@hotmail.com>
2022-02-18 17:37:08 +1100

    my thoughts

--- src/rules/letterCombinations/index.js
+++ src/rules/letterCombinations/index.js
@@ -2,6 +2,14 @@
  * @param {string} digits
  * @return {string[]}
  */
+
+/*****************************************************************
+ * Thoughts: is it just all permutation of the number of digits? *
+ * for example, if it's single digit, it would be 3^1.
+ * if it's two digit, it would be 3^2,
+ * if it's three digit, it would be 3^3,                         *
+ *****************************************************************/
+
 var letterCombinations = function(digits) {
   return [];
 };

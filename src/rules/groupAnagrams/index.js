8f5c9ea743cc1e269d8847c8cd09615e47816de3
Steven <gonghaima@hotmail.com>
2022-02-26 14:12:51 +1100

    49. Group Anagrams - initial setup

---
+++ src/rules/groupAnagrams/index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+  return null;
+};
+export default groupAnagrams;
---
+++ test/data/groupAnagramsData.js
@@ -0,0 +1,11 @@
+export const inputData = {
+  case1: ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'],
+  case2: [''],
+  case3: ['a'],
+};
+
+export const expected = {
+  case1: [['bat'], ['nat', 'tan'], ['ate', 'eat', 'tea']],
+  case2: [['']],
+  case3: [['a']],
+};

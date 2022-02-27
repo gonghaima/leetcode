11dea9d23cd56f090a4a08ea75c21a64cb98c282
Steven <gonghaima@hotmail.com>
2022-02-27 18:55:05 +1100

    thoughts

--- src/rules/groupAnagrams/index.js
+++ src/rules/groupAnagrams/index.js
@@ -2,6 +2,13 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+
+/***********************************************************************************************************************
+ * My Thought: create a Map to stort result. For each item in strs, sort it as a key,                                  *
+ * if the key already exist in Map, add the item in the value Array, otherwise, create a new entry with the Key,
+ * an array containing the item as the first item *
+ ***********************************************************************************************************************/
+
 var groupAnagrams = function(strs) {
   return null;
 };

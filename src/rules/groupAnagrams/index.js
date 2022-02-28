3175237785f903ee23d8bea1a088004a98fe7ec6
Steven <gonghaima@hotmail.com>
2022-02-28 16:58:47 +1100

    O(n*klog(k))

--- src/rules/groupAnagrams/index.js
+++ src/rules/groupAnagrams/index.js
@@ -9,7 +9,24 @@
  * an array containing the item as the first item *
  ***********************************************************************************************************************/
 
+// https://leetcode.com/problems/group-anagrams/discuss/718955/Three-JS-Solutions
+
+//  O(n*klog(k))
+
+/**********************************************************************************************
+ * Runtime: 223 ms, faster than 39.24% of JavaScript online submissions for Group Anagrams.   *
+ * Memory Usage: 53 MB, less than 42.80% of JavaScript online submissions for Group Anagrams. *
+ **********************************************************************************************/
+
 var groupAnagrams = function(strs) {
-  return null;
+  let obj = {};
+  for (let str of strs) {
+    let letters = str
+      .split('')
+      .sort()
+      .join('');
+    obj[letters] ? obj[letters].push(str) : (obj[letters] = [str]);
+  }
+  return Object.values(obj);
 };
 export default groupAnagrams;

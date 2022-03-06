df435b0bfd57e09450fde4614f9da74e510218f0
Steven <gonghaima@hotmail.com>
2022-03-06 17:03:13 +1100

    some thoughts - O(n)

--- src/rules/isInterleave/index.js
+++ src/rules/isInterleave/index.js
@@ -4,6 +4,16 @@
  * @param {string} s3
  * @return {boolean}
  */
+
+/**
+ * for each char in s3, 
+ * remove it from s1, if match
+ * remove it from 22, if match
+ * otherwise return false
+ * 
+ * same process, but check s2 first, then s1
+ */
+
 var isInterleave = function(s1, s2, s3) {
   return null;
 };

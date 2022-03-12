f18ca047899d76df3dc87ef1a189f5752d2aa00a
Steven <gonghaima@hotmail.com>
2022-03-13 09:39:41 +1100

    backtrack

--- test/rules/palindromePartitioning.test.js
+++ test/rules/palindromePartitioning.test.js
@@ -3,10 +3,12 @@ import { inputData, expected } from '../data/palindromePartitioningData';
 
 test('palindromePartitioning should return the expected result for case1', () => {
   const result = palindromePartitioning(inputData.case1);
-  expect(result).toEqual(expected.case1);
+  expect(result.sort()).toEqual(expected.case1.sort());
+  // expect(result.sort()[0].sort()).toEqual(expected.case1.sort()[0].sort());
+
 });
 
 test('palindromePartitioning should return the expected result for case2', () => {
   const result = palindromePartitioning(inputData.case2);
-  expect(result).toEqual(expected.case2);
+  expect(result.sort()).toEqual(expected.case2.sort());
 });

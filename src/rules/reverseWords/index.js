426782a7424a3c3f4a598472784cb8de17b627f3
Steven <gonghaima@hotmail.com>
2022-03-17 21:00:44 +1100

    a simple solution

--- src/rules/reverseWords/index.js
+++ src/rules/reverseWords/index.js
@@ -3,7 +3,15 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-  return null;
+  // 1. split the string with a space to form an array of words
+  // 2. reverse the words
+  // 3. filter out the words that are empty
+  // 4. join the array to form the resultant string
+  return s
+    .split(' ')
+    .reverse()
+    .filter((w) => w !== '')
+    .join(' ');
 };
 
 export default reverseWords;

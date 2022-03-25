3b7b21be813dd8e601df0c0c0f1a6f348c05b01c
Steven <gonghaima@hotmail.com>
2022-03-25 18:56:20 +1100

    use compareFunc

--- test/rules/largestNumber179.test.js
+++ test/rules/largestNumber179.test.js
@@ -2,12 +2,10 @@ import largestNumber179 from '../../src/rules/largestNumber179';
 import { inputData, expected } from '../data/largestNumber179Data';
 
 test('largestNumber179 should return expected result for case1', () => {
-  const { cost, target } = inputData.case1;
-  const result = largestNumber179(cost, target);
+  const result = largestNumber179(inputData.case1);
   expect(result).toEqual(expected.case1);
 });
 test('largestNumber179 should return expected result for case2', () => {
-  const { cost, target } = inputData.case2;
-  const result = largestNumber179(cost, target);
+  const result = largestNumber179(inputData.case2);
   expect(result).toEqual(expected.case2);
 });

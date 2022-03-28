45d3f9f7e82fdca10440fab12c48523aea4208e3
Steven <gonghaima@hotmail.com>
2022-03-28 19:14:09 +1100

    rolling hash

--- src/rules/findRepeatedDnaSequences/index.js
+++ src/rules/findRepeatedDnaSequences/index.js
@@ -2,6 +2,15 @@
  * @param {string} s
  * @return {string[]}
  */
+
+// rolling hash
+// https://leetcode.com/problems/repeated-dna-sequences/discuss/899317/JavaScript-Rolling-Hash-Solutions
+
+/*****************************************************************************************************
+ * Runtime: 268 ms, faster than 9.45% of JavaScript online submissions for Repeated DNA Sequences.   *
+ * Memory Usage: 69 MB, less than 8.94% of JavaScript online submissions for Repeated DNA Sequences. *
+ *****************************************************************************************************/
+
 var findRepeatedDnaSequences = function(s) {
   let curr = s.slice(0, 10);
   const seen = new Set([curr]);

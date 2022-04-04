226c779f34fdb7acc7da232af293a38600f18784
Steven <gonghaima@hotmail.com>
2022-04-04 19:56:14 +1000

    trie

--- src/rules/wordDictionary/index.js
+++ src/rules/wordDictionary/index.js
@@ -1,22 +1,57 @@
-var WordDictionary = function() {};
+/***********************************************************************************************************************
+ * Runtime: 1874 ms, faster than 81.47% of JavaScript online submissions for Design Add and Search Words Data Structure. *
+ * Memory Usage: 99.5 MB, less than 51.07% of JavaScript online submissions for Design Add and Search Words Data Structure. *
+ ***********************************************************************************************************************/
+
+// https://leetcode.com/problems/design-add-and-search-words-data-structure/discuss/499657/JavaScript-Solution-Trie-%2B-DFS
+
+// The idea
+// 1. Store words as trie
+// 2. Traverse trie using dfs
 
 /**
+ * Initialize your data structure here.
+ */
+var WordDictionary = function() {
+  this.trie = {};
+};
+
+/**
+ * Adds a word into the data structure.
  * @param {string} word
  * @return {void}
  */
-WordDictionary.prototype.addWord = function(word) {};
+WordDictionary.prototype.addWord = function(word) {
+  let root = this.trie;
+  for (let i = 0; i < word.length; i++) {
+    if (root[word[i]] == null) root[word[i]] = {};
+    root = root[word[i]];
+  }
+  root.isEnd = true;
+};
 
 /**
+ * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
  * @param {string} word
  * @return {boolean}
  */
-WordDictionary.prototype.search = function(word) {};
+WordDictionary.prototype.search = function(word) {
+  return this.dfs(word, 0, this.trie);
+};
 
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * var obj = new WordDictionary()
- * obj.addWord(word)
- * var param_2 = obj.search(word)
- */
+WordDictionary.prototype.dfs = function(word, index, node) {
+  if (index == word.length) return node.isEnd == true;
+
+  if (word[index] == '.') {
+    for (let key in node) {
+      if (this.dfs(word, index + 1, node[key])) return true;
+    }
+  } else {
+    if (node[word[index]] != null) {
+      return this.dfs(word, index + 1, node[word[index]]);
+    }
+  }
+  return false;
+};
 
 export default WordDictionary;
--- test/rules/wordDictionary.test.js
+++ test/rules/wordDictionary.test.js
@@ -5,18 +5,17 @@ test('WordDictionary should return expected result for case1', () => {
   const wordDictionary = new WordDictionary();
   wordDictionary.addWord('bad');
   wordDictionary.addWord('dad');
-  const result1 = wordDictionary.addWord('mad');
+  wordDictionary.addWord('mad');
+
+  const result1 = wordDictionary.search('pad'); // return False
   expect(result1).toEqual(false);
 
-  const result2 = wordDictionary.search('pad'); // return False
-  expect(result2).toEqual(false);
+  const result2 = wordDictionary.search('bad'); // return True
+  expect(result2).toEqual(true);
 
-  const result3 = wordDictionary.search('bad'); // return True
+  const result3 = wordDictionary.search('.ad'); // return True
   expect(result3).toEqual(true);
 
-  const result4 = wordDictionary.search('.ad'); // return True
+  const result4 = wordDictionary.search('b..'); // return True
   expect(result4).toEqual(true);
-
-  const result5 = wordDictionary.search('b..'); // return True
-  expect(result5).toEqual(true);
 });

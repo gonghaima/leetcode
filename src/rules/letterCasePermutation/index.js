46ec801ead42bb12b38cf9c936ecb8f185d6ebc3
Steven <gonghaima@hotmail.com>
2022-05-09 19:31:53 +1000

    dfs

--- src/rules/letterCasePermutation/index.js
+++ src/rules/letterCasePermutation/index.js
@@ -3,25 +3,32 @@
  * @return {string[]}
  */
 
-// https://leetcode.com/problems/letter-case-permutation/discuss/508080/JavaScript-iterative-BFS-solution
+// https://leetcode.com/problems/letter-case-permutation/discuss/1069003/JS-Python-Java-C%2B%2B-or-Recursive-DFS-Solution-w-Explanation-or-beats-100-100
 
 /*********************************************************************************************************
- * Runtime: 87 ms, faster than 75.03% of JavaScript online submissions for Letter Case Permutation.      *
- * Memory Usage: 46.2 MB, less than 39.07% of JavaScript online submissions for Letter Case Permutation. *
+ * Runtime: 84 ms, faster than 79.17% of JavaScript online submissions for Letter Case Permutation.      *
+ * Memory Usage: 45.1 MB, less than 66.11% of JavaScript online submissions for Letter Case Permutation. *
  *********************************************************************************************************/
+/**********************************************************************************************************************
+ * Idea:                                                                                                              *
+ * When the problem asks us to deal with permutations, one of the obvious approaches is via recursion because recursion will allow us to fire off our helper function down each branching possibility. *
+ * Recursion also naturally favors a DFS approach, which is also good because it ensures that our recursion stack never gets too deep. *
+ * Our recursive helper function (dfs) is actually quite simple. If we start with the input string (S) fully lowercased, then we just need to make sure that each version of dfs calls itself down two branches: one in which the current character is left unchanged, and a second in which the character has been uppercased, but only if the character is a letter. *
+ * Then, whenever we reach the end of S, we can add the permutation to our answer array (ans).                        *
+ **********************************************************************************************************************/
 
 var letterCasePermutation = function(S) {
-  const queue = [S];
-  for (let i = 0; i < S.length; i++) {
-    if (/[0-9]/.test(S[i])) continue;
-    const size = queue.length;
-    for (let j = 0; j < size; j++) {
-      const node = queue.shift();
-      queue.push(node.slice(0, i) + node[i].toLowerCase() + node.slice(i + 1));
-      queue.push(node.slice(0, i) + node[i].toUpperCase() + node.slice(i + 1));
-    }
-  }
-  return queue;
+  S = S.toLowerCase();
+  let len = S.length,
+    ans = [];
+  const dfs = (i, res = '') => {
+    if (i < len) {
+      dfs(i + 1, res + S[i]);
+      if (S[i] >= 'a') dfs(i + 1, res + S[i].toUpperCase());
+    } else ans.push(res);
+  };
+  dfs(0);
+  return ans;
 };
 
 export default letterCasePermutation;

7778ceb19c106b30f4c25f70aee20e8893bc8e26
Steven <gonghaima@hotmail.com>
2022-05-15 20:54:17 +1000

    my thoughts - O(n)

--- src/rules/customSortString/index.js
+++ src/rules/customSortString/index.js
@@ -3,6 +3,13 @@
  * @param {string} s
  * @return {string}
  */
+/**
+ * my thought: O(n)
+ * build a set from s
+ * for each char in order, if exists in s-Set,
+ *      append to result string && remove the char from s-Set
+ * for each char in s-Set, append it to result string
+ */
 var customSortString = function(order, s) {
   return null;
 };

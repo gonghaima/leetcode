8ef59d02256fa35f42c0f74c5980fc571edff2e6
Steven <gonghaima@hotmail.com>
2022-05-16 18:27:14 +1000

    hash

--- src/rules/customSortString/index.js
+++ src/rules/customSortString/index.js
@@ -3,15 +3,19 @@
  * @param {string} s
  * @return {string}
  */
-/**
- * my thought: O(n)
- * build a set from s
- * for each char in order, if exists in s-Set,
- *      append to result string && remove the char from s-Set
- * for each char in s-Set, append it to result string
- */
-var customSortString = function(order, s) {
-  return null;
+var customSortString = function(order, string) {
+  let map = {};
+  // iterate throuhg our string and place  map[string[i]] = i
+  for (let i = 0; i < order.length; i++) {
+    map[order[i]] = i;
+  }
+  for (let i = 0; i < string.length; i++) {
+    if (map[string[i]] == undefined) {
+      map[string[i]] = Infinity;
+    }
+  }
+  string = string.split('');
+  return string.sort((a, b) => map[a] - map[b]).join(''); // if not in map then the order will stay and the char will not be moved from orignal string
 };
 
 export default customSortString;

1fe0f5025bd4fb86b262c2f6e263cae6ed2dcc46
Steven <gonghaima@hotmail.com>
2022-05-27 19:08:37 +1000

    792. Number of Matching Subsequences - initial setup

---
+++ test/rules/numMatchingSubseq.test.js
@@ -0,0 +1,14 @@
+import numMatchingSubseq from '../../src/rules/numMatchingSubseq';
+import { inputData, expected } from '../data/numMatchingSubseqData';
+
+test('numMatchingSubseq should return expected result for case1', () => {
+  const { s, words } = inputData.case1;
+  const result = numMatchingSubseq(s, words);
+  expect(result).toEqual(expected.case1[1]);
+});
+
+test('numMatchingSubseq should return expected result for case2', () => {
+  const { s, words } = inputData.case2;
+  const result = numMatchingSubseq(s, words);
+  expect(result).toEqual(expected.case1[2]);
+});

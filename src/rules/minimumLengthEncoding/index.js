1de813f5aaa0780661d555b06734bfd8091f8808
Steven <gonghaima@hotmail.com>
2022-06-15 18:40:53 +1000

    820. Short Encoding of Words - initial setup

---
+++ src/rules/minimumLengthEncoding/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var minimumLengthEncoding = function(words) {
+  return NaN;
+};
+
+export default minimumLengthEncoding;
---
+++ test/data/minimumLengthEncodingData.js
@@ -0,0 +1,9 @@
+export const inputData = {
+  case1: ['time', 'me', 'bell'],
+  case2: ['t'],
+};
+
+export const expected = {
+  case1: 10,
+  case2: 2,
+};

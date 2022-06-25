4b22d97cbe8d770d27cd69ad1bc5ce0002ec02bf
Steven <gonghaima@hotmail.com>
2022-06-25 21:41:19 +1000

    trie - 1

--- src/rules/minimumLengthEncoding/index.js
+++ src/rules/minimumLengthEncoding/index.js
@@ -4,20 +4,28 @@
  */
 
 /*********************************************************************************************************
- * Runtime: 84 ms, faster than 100.00% of JavaScript online submissions for Short Encoding of Words.     *
- * Memory Usage: 48.4 MB, less than 75.00% of JavaScript online submissions for Short Encoding of Words. *
+ * Runtime: 164 ms, faster than 60.28% of JavaScript online submissions for Short Encoding of Words.     *
+ * Memory Usage: 60.1 MB, less than 32.35% of JavaScript online submissions for Short Encoding of Words. *
  *********************************************************************************************************/
 
-// https://leetcode.com/problems/short-encoding-of-words/discuss/1095818/Javascript-Hashmap-Concise-beats-100
+// https://leetcode.com/problems/short-encoding-of-words/discuss/1095894/JS-Python-Java-C%2B%2B-or-(Updated)-Set-and-Trie-Solutions-w-Visual-Explanation
 
-var minimumLengthEncoding = function(words) {
-  const suits = new Set(words);
-  suits.forEach((word) => {
-    for (let i = 1; i < word.length; i++) {
-      suits.delete(word.slice(i));
+var minimumLengthEncoding = function(W) {
+  let len = W.length,
+    trie = new Map(),
+    ans = 1;
+  for (let word of W) {
+    let curr = trie,
+      newWord = false;
+    for (let j = word.length - 1; ~j; j--) {
+      let char = word.charAt(j);
+      if (!curr.size && !newWord) ans -= word.length - j;
+      if (!curr.has(char)) (newWord = true), curr.set(char, new Map());
+      curr = curr.get(char);
     }
-  });
-  return [...suits].join().length + 1;
+    if (newWord) ans += word.length + 1;
+  }
+  return ans;
 };
 
 export default minimumLengthEncoding;

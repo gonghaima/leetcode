310759284c705b56ddafe9c9c956bc67e5419ea3
Steven <gonghaima@hotmail.com>
2022-06-30 19:29:02 +1000

    three utils

--- src/rules/maskPII/index.js
+++ src/rules/maskPII/index.js
@@ -3,30 +3,42 @@
  * @return {string}
  */
 
-// regex repeat
-// https://leetcode.com/problems/masking-personal-information/discuss/1136228/intuitive-13-line-JS-solution-or-97.22-faster-86.11-memory-usage
+// https://leetcode.com/problems/masking-personal-information/discuss/1004179/JavaScript-Solution
 
 /*************************************************************************************************************
- * Runtime: 67 ms, faster than 69.23% of JavaScript online submissions for Masking Personal Information.     *
+ * Runtime: 100 ms, faster than 23.08% of JavaScript online submissions for Masking Personal Information.    *
  * Memory Usage: 42.3 MB, less than 7.69% of JavaScript online submissions for Masking Personal Information. *
  *************************************************************************************************************/
 
 var maskPII = function(S) {
-  if (S.includes('@') && S.includes('.')) {
-    // is email
-    const elements = S.split('@');
-    const name = elements[0];
-    return `${name[0].toLowerCase()}*****${name[
-      name.length - 1
-    ].toLowerCase()}@${elements[1].toLowerCase()}`;
-  } else {
-    const stripped = S.replace(/\D/g, '');
-    const lastFour = stripped.slice(stripped.length - 4);
-    if (stripped.length === 10) {
-      return `***-***-${lastFour}`;
-    } else {
-      return `+${'*'.repeat(stripped.length - 10)}-***-***-${lastFour}`;
-    }
+  if (isEmail(S)) return maskEmail(S);
+  return maskPhone(S);
+
+  function isEmail(str) {
+    return /^[A-Za-z]{2,}@[A-Za-z]{2,}.[A-Za-z]{2,}$/.test(str);
+  }
+
+  function maskEmail(str) {
+    let res = '';
+    const [local, domain] = str.split('@');
+
+    res +=
+      local.charAt(0).toLowerCase() +
+      '*****' +
+      local.charAt(local.length - 1).toLowerCase();
+    return res + '@' + domain.toLowerCase();
+  }
+
+  function maskPhone(str) {
+    const onlyDigits = str.replace(/\D/g, '');
+
+    const localNum = '***-***-' + onlyDigits.substring(onlyDigits.length - 4);
+
+    if (onlyDigits.length === 10) return localNum;
+
+    let countryNum = '+' + '*'.repeat(onlyDigits.length - 10) + '-';
+
+    return countryNum + localNum;
   }
 };
 

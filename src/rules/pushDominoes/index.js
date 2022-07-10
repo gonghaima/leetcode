59abf83dd57ad6973de0acf287fb40a92814fb6e
Steven <gonghaima@hotmail.com>
2022-07-11 00:24:33 +1000

    my solution - test passes

--- src/rules/pushDominoes/index.js
+++ src/rules/pushDominoes/index.js
@@ -12,7 +12,50 @@
  *************/
 
 var pushDominoes = function(dominoes) {
-  return null;
+  let result = '';
+  const dominoesChars = dominoes.split('');
+  for (let i = 0; i < dominoesChars.length; i++) {
+    const currentChar = dominoesChars[i];
+    const prev = i - 1 >= 0 ? dominoesChars[i - 1] : null;
+    const next =
+      i + 1 <= dominoesChars.length - 1 ? dominoesChars[i + 1] : null;
+
+    if (currentChar === 'L' || currentChar === 'R') {
+      result += currentChar;
+      continue;
+    }
+
+    // R.L , keep it as .
+    if (prev && next && prev === 'R' && next === 'L') {
+      result += '.';
+      continue;
+    }
+
+    // L.R , keep it as .
+    if (prev && next && prev === 'L' && next === 'R') {
+      result += '.';
+      continue;
+    }
+
+    // ... , keep it as .
+    if (prev && next && prev === '.' && next === '.') {
+      result += '.';
+      continue;
+    }
+
+    if (prev && prev === 'R') {
+      result += 'R';
+      continue;
+    }
+
+    if (next && next === 'L') {
+      result += 'L';
+      continue;
+    }
+
+    result += currentChar;
+  }
+  return result;
 };
 
 export default pushDominoes;

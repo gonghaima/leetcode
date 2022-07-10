7e9aa473cc0f5dd98f2e3a74211a44c8b788f61d
Steven <gonghaima@hotmail.com>
2022-07-10 13:25:32 +1000

    thoughts

--- src/rules/pushDominoes/index.js
+++ src/rules/pushDominoes/index.js
@@ -2,6 +2,15 @@
  * @param {string} dominoesturn
  * @return {string}
  */
+
+/*************
+ * Thoughts:
+ * O(n) , for each char,
+ * if L or R, keep it as it is, keep next interation*
+ * if ".", check left and right char, hash for four possible combinations
+ *    LL LR RL RR
+ *************/
+
 var pushDominoes = function(dominoes) {
   return null;
 };

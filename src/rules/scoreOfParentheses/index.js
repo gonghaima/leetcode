40b67b27d93599089fe2ef3a5296795f982c5547
Steven <gonghaima@hotmail.com>
2022-08-03 19:51:26 +1000

    iterative solution

--- src/rules/scoreOfParentheses/index.js
+++ src/rules/scoreOfParentheses/index.js
@@ -3,19 +3,28 @@
  * @return {number}
  */
 
-/***********************************************************************************************************************
- * My thoughts                                                                                                         *
- * - a while loop with a pointer initial position 0 , a current accumulated counter start with 0, a score start with 0 *
- * - keep increase pointer and accumulated counter, util see ')'                                                       *
- * - if accumulated counter is 1 , score += 1;                                                                         *
- * - else score += 2 power by accumulatedCounter                                                                       *
- * - pointer += accumulatedCounter                                                                                     *
- * return scroe                                                                                                        *
- ***********************************************************************************************************************/
+// https://leetcode.com/problems/score-of-parentheses/discuss/1081329/Score-of-Parentheses-Easy-to-Understand-Explanation-Javascript-code
 
+/*******************************************************************************************************
+ * Runtime: 99 ms, faster than 19.23% of JavaScript online submissions for Score of Parentheses.       *
+ * Memory Usage: 41.1 MB, less than 100.00% of JavaScript online submissions for Score of Parentheses. *
+ *******************************************************************************************************/
 
 var scoreOfParentheses = function(s) {
-  return NaN;
+  let score = 0,
+    depth = 0;
+  for (let i = 0, j = s.length; i < j; i++) {
+    // linear left to right iteration
+    if (s.charAt(i) == '(')
+      // found ( so increment depth
+      depth++;
+    else if (s.charAt(i - 1) == '(')
+      // found () a core, so time to add its calculated value to score
+      score += 1 << --depth;
+    //binary bit shift operator, doubling value by depth
+    else --depth; // not a core, but need to unwrap a layer of nesting
+  }
+  return score;
 };
 
 export default scoreOfParentheses;

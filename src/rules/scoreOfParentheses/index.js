7dfddd9dc024fbdaa1c3280af59ed7649507a12f
Steven <gonghaima@hotmail.com>
2022-08-06 18:57:37 +1000

    stack

--- src/rules/scoreOfParentheses/index.js
+++ src/rules/scoreOfParentheses/index.js
@@ -3,28 +3,25 @@
  * @return {number}
  */
 
-// https://leetcode.com/problems/score-of-parentheses/discuss/1081329/Score-of-Parentheses-Easy-to-Understand-Explanation-Javascript-code
+// https://leetcode.com/problems/score-of-parentheses/discuss/1856454/Beginner-friendly-javascript-solution
 
-/*******************************************************************************************************
- * Runtime: 99 ms, faster than 19.23% of JavaScript online submissions for Score of Parentheses.       *
- * Memory Usage: 41.1 MB, less than 100.00% of JavaScript online submissions for Score of Parentheses. *
- *******************************************************************************************************/
+/****************************************************************************************************
+ * Runtime: 96 ms, faster than 23.08% of JavaScript online submissions for Score of Parentheses.    *
+ * Memory Usage: 42 MB, less than 58.65% of JavaScript online submissions for Score of Parentheses. *
+ ****************************************************************************************************/
 
 var scoreOfParentheses = function(s) {
-  let score = 0,
-    depth = 0;
-  for (let i = 0, j = s.length; i < j; i++) {
-    // linear left to right iteration
-    if (s.charAt(i) == '(')
-      // found ( so increment depth
-      depth++;
-    else if (s.charAt(i - 1) == '(')
-      // found () a core, so time to add its calculated value to score
-      score += 1 << --depth;
-    //binary bit shift operator, doubling value by depth
-    else --depth; // not a core, but need to unwrap a layer of nesting
+  let stack = [];
+  let cur = 0;
+  for (let c of s) {
+    if (c == '(') {
+      stack.push(cur);
+      cur = 0;
+    } else {
+      cur = stack.pop() + Math.max(1, cur * 2);
+    }
   }
-  return score;
+  return cur;
 };
 
 export default scoreOfParentheses;

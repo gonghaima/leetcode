77c4e7a744fb18fd008462351247fac605f51eb0
Steven <gonghaima@hotmail.com>
2022-08-07 14:43:49 +1000

    iterative 1

--- src/rules/scoreOfParentheses/index.js
+++ src/rules/scoreOfParentheses/index.js
@@ -3,25 +3,28 @@
  * @return {number}
  */
 
-// https://leetcode.com/problems/score-of-parentheses/discuss/1856454/Beginner-friendly-javascript-solution
+// https://leetcode.com/problems/score-of-parentheses/discuss/1979330/JavaScript-Solution-oror-O(n)-Time-oror-O(n)-Space
 
-/****************************************************************************************************
- * Runtime: 96 ms, faster than 23.08% of JavaScript online submissions for Score of Parentheses.    *
- * Memory Usage: 42 MB, less than 58.65% of JavaScript online submissions for Score of Parentheses. *
- ****************************************************************************************************/
+/******************************************************************************************************
+ * Runtime: 117 ms, faster than 8.65% of JavaScript online submissions for Score of Parentheses.      *
+ * Memory Usage: 41.9 MB, less than 76.92% of JavaScript online submissions for Score of Parentheses. *
+ ******************************************************************************************************/
 
 var scoreOfParentheses = function(s) {
-  let stack = [];
-  let cur = 0;
-  for (let c of s) {
-    if (c == '(') {
-      stack.push(cur);
-      cur = 0;
+  let stack = [0];
+  for (let char of s) {
+    if (char === '(') {
+      stack.push(0);
     } else {
-      cur = stack.pop() + Math.max(1, cur * 2);
+      let popped = stack.pop();
+      if (popped === 0) {
+        stack[stack.length - 1] += 1;
+      } else {
+        stack[stack.length - 1] += popped * 2;
+      }
     }
   }
-  return cur;
+  return stack[0];
 };
 
 export default scoreOfParentheses;

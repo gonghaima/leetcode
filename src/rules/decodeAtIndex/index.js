861d7086f890772b177311b20e5426138a18f586
Steven <gonghaima@hotmail.com>
2022-08-15 19:31:47 +1000

    my thoughts - O(n)

--- src/rules/decodeAtIndex/index.js
+++ src/rules/decodeAtIndex/index.js
@@ -4,6 +4,26 @@
  * @return {string}
  */
 var decodeAtIndex = function(s, k) {
+  let startToCount = false;
+  let countNumber = '';
+  let accumulativeChars = '';
+  let result = '';
+  for (let i = 0; i < s.length; i++) {
+    //early terminate, when result[k]!==undefined
+    const currentCar = s[i];
+    // start to count if it is a number
+    if (!isNaN(Number(currentCar))) {
+      startToCount = true;
+      countNumber = countNumber + '' + currentCar;
+    } else {
+      // it is a char
+      // if startToCount is true,
+      //      build the string with accumulativeChars, append to the result,
+      //      reset accumulativeChars to currentChar, startToCount to false
+      // else startToCount is false
+      //     append currentChar to accumulativeChars
+    }
+  }
   return null;
 };
 

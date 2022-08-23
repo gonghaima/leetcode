af319e6392b78a34103f5d5cdebdd4f228309ce9
Steven <gonghaima@hotmail.com>
2022-08-23 18:55:07 +1000

    my thoughts

--- src/rules/wordSubsets/index.js
+++ src/rules/wordSubsets/index.js
@@ -3,6 +3,24 @@
  * @param {string[]} words2
  * @return {string[]}
  */
+
+/*****************************************************************************
+ * my thoughts:                                                              *
+ * for each word in words1, build a map key is CharacterData, value is Count *
+ * for each word in words2, build a map key is CharacterData, value is Count *
+ *
+ * declare result Array
+ *                                                    *
+ * for each word collection in words1Count,
+ *                                 *
+ * if words2Count contains & smaller than CountQueuingStrategy,              *
+ * push it into result Array
+ *                                       *
+ * return result
+ *                                       *
+ * time complexity words1.length * words2.length                             *
+ *****************************************************************************/
+
 var wordSubsets = function(words1, words2) {
   return null;
 };

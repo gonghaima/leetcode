e171be81fbf369c4cf4da76f6facc0048c7dd2da
Steven <gonghaima@hotmail.com>
2022-09-24 19:35:39 +1000

    454. 4Sum II - intial setup:

---
+++ src/rules/fourSumCount/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @return {number}
+ */
+var fourSumCount = function(nums1, nums2, nums3, nums4) {
+  return NaN;
+};
+
+export default fourSumCount;

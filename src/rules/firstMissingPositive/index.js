e1841396e8ab2f9331a879b6159ba7bf3e0ed4f5
Steven <gonghaima@hotmail.com>
2022-11-01 09:20:47 +1100

    using sort

--- src/rules/firstMissingPositive/index.js
+++ src/rules/firstMissingPositive/index.js
@@ -3,25 +3,19 @@
  * @return {number}
  */
 
-// https://leetcode.com/problems/first-missing-positive/discuss/927112/Three-JS-Solutions
+// https://leetcode.com/problems/first-missing-positive/discuss/2329831/JavaScript-solution-in-2-line
 
 // Time Complexity: O(nlog(n))
 // Space Complexity: O(n)
 
 /********************************************************************************************************
- * Runtime: 160 ms, faster than 23.70% of JavaScript online submissions for First Missing Positive.     *
- * Memory Usage: 58.9 MB, less than 22.66% of JavaScript online submissions for First Missing Positive. *
+ * Runtime: 128 ms, faster than 60.18% of JavaScript online submissions for First Missing Positive.     *
+ * Memory Usage: 48.2 MB, less than 65.92% of JavaScript online submissions for First Missing Positive. *
  ********************************************************************************************************/
 
 var firstMissingPositive = function(nums) {
-  nums = Array.from(new Set(nums)).filter((num) => num > 0);
-  nums.sort((a, b) => a - b);
-  let j = 1;
-  for (let i = 0; i < nums.length; i++) {
-    if (j == nums[i]) j++;
-    else return j;
-  }
-  return j;
+  const arr = nums.sort((a, b) => a - b);
+  return arr.reduce((min, num) => (num == min ? min + 1 : min), 1);
 };
 
 export default firstMissingPositive;

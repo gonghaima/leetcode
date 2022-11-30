048b4a7a935cf83a9e85851742726c590c5ffcd0
Steven <gonghaima@hotmail.com>
2022-11-30 18:51:51 +1100

    one line solution

--- src/rules/findDuplicate/index.js
+++ src/rules/findDuplicate/index.js
@@ -3,32 +3,25 @@
  * @return {number}
  */
 
-// https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
+/***********************************************************************************************************
+ * Runtime: 427 ms, faster than 14.84% of JavaScript online submissions for Find the Duplicate Number.     *
+ * Memory Usage: 57.3 MB, less than 27.70% of JavaScript online submissions for Find the Duplicate Number. *
+ ***********************************************************************************************************/
 
-/*******************************************************************************************************
- * Runtime: 113 ms, faster than 86.56% of JavaScript online submissions for Find the Duplicate Number. *
- * Memory Usage: 50 MB, less than 60.62% of JavaScript online submissions for Find the Duplicate       *
- *******************************************************************************************************/
+// time O(n logn) space O(1)
 
-// time O(1) space O(1)
+// var findDuplicate = function(nums) {
+//   return nums.sort().reduce(
+//     (acc, item) => {
+//       if (!acc.currentItem) return { result: -1, currentItem: item };
+//       if (acc.currentItem === item) return { result: item, currentItem: item };
+//       if (acc.result === -1) return { result: -1, currentItem: item };
+//       return acc;
+//     },
+//     { result: -1, currentItem: null }
+//   ).result;
+// };
 
-var findDuplicate = function(nums) {
-  if (nums.length > 1) {
-    let slow = nums[0];
-    let fast = nums[nums[0]];
-    while (slow != fast) {
-      slow = nums[slow];
-      fast = nums[nums[fast]];
-    }
-
-    fast = 0; // slow = 4, fast = 0
-    while (fast != slow) {
-      fast = nums[fast];//f(0)1     f(1)3       f(3)2
-      slow = nums[slow];//f(4)2     f(2)4       f(4)2
-    }
-    return slow;
-  }
-  return -1;
-};
+var findDuplicate = function(nums) {return nums.sort().reduce((r,e)=>r.currentItem?r.currentItem===e?{result:e,currentItem:e}:-1===r.result?{result:-1,currentItem:e}:r:{result:-1,currentItem:e},{result:-1,currentItem:null}).result}
 
 export default findDuplicate;

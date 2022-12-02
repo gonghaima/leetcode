6fd855370eb7a28c6430bb673f91cb58e043a2c7
Steven <gonghaima@hotmail.com>
2022-12-02 18:37:31 +1100

    stack

--- test/rules/calculate2.test.js
+++ test/rules/calculate2.test.js
@@ -3,25 +3,16 @@ import calculate2 from '../../src/rules/calculate2';
 import { inputData, expected } from '../data/calculate2Data';
 
 test('calculate2 should return expected result for case1', () => {
-  const result = calculate2(
-    inputData.case1.numCourses,
-    inputData.case1.prerequisites
-  );
+  const result = calculate2(inputData.case1);
   expect(result).toEqual(expected.case1);
 });
 
 test('calculate2 should return expected result for case2', () => {
-  const result = calculate2(
-    inputData.case2.numCourses,
-    inputData.case2.prerequisites
-  );
+  const result = calculate2(inputData.case2);
   expect(result).toEqual(expected.case2);
 });
 
 test('calculate2 should return expected result for case3', () => {
-  const result = calculate2(
-    inputData.case3.numCourses,
-    inputData.case3.prerequisites
-  );
+  const result = calculate2(inputData.case3);
   expect(result).toEqual(expected.case3);
 });

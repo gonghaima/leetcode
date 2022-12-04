f616dede9f9d3dfc7232f0e5457d018222dcbd3c
Steven <gonghaima@hotmail.com>
2022-12-04 21:28:23 +1100

    23. Merge k Sorted Lists - initial setup

---
+++ test/data/mergeKListsData.js
@@ -0,0 +1,15 @@
+export const inputData = {
+  case1: [
+    [1, 4, 5],
+    [1, 3, 4],
+    [2, 6],
+  ],
+  case2: [],
+  case3: [[]],
+};
+
+export const expected = {
+  case1: [1, 1, 2, 3, 4, 4, 5, 6],
+  case2: [],
+  case3: [],
+};

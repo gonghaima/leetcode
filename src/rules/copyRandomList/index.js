bbf2412891ad41c8aa54daef8402f75cc2d482db
Steven Gong <gonghaima@hotmail.com>
2022-12-17 09:25:15 +1100

    use map

--- src/rules/copyRandomList/index.js
+++ src/rules/copyRandomList/index.js
@@ -17,6 +17,23 @@ function Node(val, next, random) {
  * @param {Node} head
  * @return {Node}
  */
-var copyRandomList = function(head) {};
+var copyRandomList = function(head) {
+  if (!head) {
+    return null;
+  }
+  const clones = new Map();
+  let n = head;
+  while (n) {
+    clones.set(n, new Node(n.val));
+    n = n.next;
+  }
+  n = head;
+  while (n) {
+    clones.get(n).next = clones.get(n.next) || null;
+    clones.get(n).random = clones.get(n.random) || null;
+    n = n.next;
+  }
+  return clones.get(head);
+};
 
 export default copyRandomList;

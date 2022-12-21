8bab91a18f4586ca93d7e5af6ca83d40488c81fb
Steven Gong <gonghaima@hotmail.com>
2022-12-21 21:07:02 +1100

    127. Word Ladder - initial setup

---
+++ src/rules/ladderLength/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+    
+};
+
+export default ladderLength;

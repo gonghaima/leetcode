d12c8d1bf4b80b1f7be4b2b4c1aae98f3852c302
Steven Gong <gonghaima@hotmail.com>
2022-12-27 17:01:01 +1100

    dfs

--- src/rules/lowestCommonAncestor/index.js
+++ src/rules/lowestCommonAncestor/index.js
@@ -11,7 +11,14 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+
 var lowestCommonAncestor = function(root, p, q) {
-    
+  // If p or q is found, return it back up the stack.
+  if (!root || root === p || root === q) return root;
+  var resL = lowestCommonAncestor(root.left, p, q); // Return first instance of p or q in left subtree
+  var resR = lowestCommonAncestor(root.right, p, q); // Return first instance of p or q in right subtree
+
+  // If we found p in left and q in right, the node we are currently at is LCA. If we found one of them, the one we found is LCA.
+  return resL && resR ? root : resL || resR;
 };
 export default lowestCommonAncestor;

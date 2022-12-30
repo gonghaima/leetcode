ae29a6e9c75eca587a344b0711ee7e7c4c680722
Steven Gong <gonghaima@hotmail.com>
2022-12-30 17:01:01 +1100

    bfs

--- src/rules/maxPathSum/index.js
+++ src/rules/maxPathSum/index.js
@@ -10,6 +10,41 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxPathSum = function(root) {};
+
+/***********
+ * Runtime *
+ * 88 ms   *
+ * Beats   *
+ * 84.38%  *
+ * Memory  *
+ * 51.9 MB *
+ * Beats   *
+ * 31.86%  *
+ ***********/
+
+var maxPathSum = function(root) {
+  let max = -Infinity;
+
+	const findSums = (node) => {
+		// Base case / hit a null
+		if (!node) return 0;
+
+		let left = findSums(node.left),
+			right = findSums(node.right),
+			allSum = left + right + node.val,
+			leftNodeSum = left + node.val,
+			rightNodeSum = right + node.val;
+
+		// Max is all possible combinations
+		max = Math.max(max, node.val, allSum, leftNodeSum, rightNodeSum);
+		
+		// Return the MAX path, which can be node.val, left + node.val, or right + node.val
+		return Math.max(leftNodeSum, rightNodeSum, node.val);
+	};
+
+	findSums(root);
+
+	return max;
+};
 
 export default maxPathSum;

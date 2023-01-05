5f3adf3e702ea1e61e5e857f5f560dbf5c04eacb
Steven Gong <gonghaima@hotmail.com>
2023-01-06 10:09:51 +1100

    my solution - brute force

--- src/rules/countSmaller/index.js
+++ src/rules/countSmaller/index.js
@@ -2,8 +2,26 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var countSmaller = function(nums) {
-    
-};
 
-export default countSmaller;
+const findVar = (nums, curPosition) => {
+    const curVal = nums[curPosition];
+    let notFound = true;
+    for (let i = curPosition + 1; notFound && i < nums.length; i++) {
+      if (curVal > nums[i]) {
+        notFound = false;
+        return nums[i];
+      }
+    }
+    return 0;
+  };
+  
+  var countSmaller = function(nums) {
+    const result = [];
+    for (let i = 0; i < nums.length; i++) {
+      result.push(findVar(nums, i));
+    }
+    return result;
+  };
+  
+  export default countSmaller;
+  

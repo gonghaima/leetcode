707a9fe57cdbb0608a5e4872c6da07b4067c1600
Steven Gong <gonghaima@hotmail.com>
2023-02-15 20:52:52 +1100

    dp + memo

--- src/rules/numDecodings/index.js
+++ src/rules/numDecodings/index.js
@@ -3,22 +3,23 @@
  * @return {number}
  */
 
-/*********************************************************************************************
- * Runtime: 100 ms, faster than 30.34% of JavaScript online submissions for Decode Ways.     *
- * Memory Usage: 38.8 MB, less than 86.72% of JavaScript online submissions for Decode Ways. *
- *********************************************************************************************/
-
-// dp & memoization
-// https://leetcode.com/problems/decode-ways/discuss/1060702/5-lines-of-code-DP-(memoiazation)-Javascript-solution
-// https://www.youtube.com/watch?v=OjEHST4SXfE
-//https://www.youtube.com/watch?v=qli-JCrSwuk
-
-var numDecodings = function (s, i = 0, memo = {}) {
-    if (i in memo) return memo[i];
-    if (s[i] === '0') return 0;
-    if (i >= s.length - 1) return 1;
-    memo[i] = numDecodings(s, i + 1, memo) + (s[i] + s[i + 1] < 27 ? numDecodings(s, i + 2, memo) : 0);
-    return memo[i];
-};
+var numDecodings = function(s) {
+    return count(s,0);
+  };
+  
+  var count = function(str, i, memo=[]) {
+    if (str[i] == "0") return 0;
+    if (i >= str.length - 1) return 1;
+    if (memo[i]) return memo[i];
+  
+    // try to take 1 and 2
+    let res = count(str, i+1, memo);
+    if (parseInt(str[i] + str[i+1]) <= 26) {
+        res += count(str, i+2, memo);
+    }
+  
+    memo[i] = res;
+    return res;
+  }
 
 export default numDecodings;

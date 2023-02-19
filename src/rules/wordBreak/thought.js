9aefb973f6d3a42f013ea8e3d6b3ac37fe55ccc9
Steven Gong <gonghaima@hotmail.com>
2023-02-19 13:26:02 +1100

    review & add thought

---
+++ src/rules/wordBreak/thought.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+
+// take next word
+
+// take sub string from s,
+// if match, keep going
+// else return false
+
+// return true
+
+// O(n) time complexity, O(1) space complexity
+
+var wordBreak = function(s, wordDict) {};
+export default wordBreak;

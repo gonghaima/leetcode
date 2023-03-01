10230e0e6cc94fdc0bab20979d967df9486495c1
Steven Gong <gonghaima@hotmail.com>
2023-03-01 19:38:37 +1100

    dfs

--- src/rules/wordBreakII/index.js
+++ src/rules/wordBreakII/index.js
@@ -3,6 +3,16 @@
  * @param {string[]} wordDict
  * @return {string[]}
  */
-var wordBreak = function(s, wordDict) {};
+
+var wordBreak = function(s, wordDict, cur = [], res = []) {
+  if (!s.length) return res.push(cur.join(' '));
+  for (let word of wordDict) {
+    if (!s.startsWith(word)) continue;
+    cur.push(word);
+    wordBreak(s.slice(word.length), wordDict, cur, res);
+    cur.pop();
+  }
+  return res;
+};
 
 export default wordBreak;

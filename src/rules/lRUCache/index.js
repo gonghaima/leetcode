3bc5bceb2920641020814b199ba5f9a53c4ae8db
Steven Gong <gonghaima@hotmail.com>
2023-03-06 18:24:53 +1100

    146. LRU Cache - initial setup

---
+++ src/rules/lRUCache/index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} capacity
+ */
+export var LRUCache = function(capacity) {};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */

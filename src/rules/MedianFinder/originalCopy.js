0c90726a212e4e9853d8d5f8a1ce62a8782b2099
Steven Gong <gonghaima@hotmail.com>
2023-03-17 17:05:38 +1100

    min max heap

---
+++ src/rules/MedianFinder/originalCopy.js
@@ -0,0 +1,15 @@
+var MedianFinder = function() {};
+
+/**
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function(num) {};
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function() {};
+
+export default MedianFinder;
+

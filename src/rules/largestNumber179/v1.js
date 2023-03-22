d1f05f65e6c1b76d1bbcbc979339466668bd85bc
Steven Gong <gonghaima@hotmail.com>
2023-03-22 18:15:39 +1100

    v2 version solution

--- src/rules/largestNumber179/v1.js
+++ src/rules/largestNumber179/v1.js
@@ -10,9 +10,24 @@
  * Memory Usage: 43.5 MB, less than 94.26% of JavaScript online submissions for Largest Number. *
  ************************************************************************************************/
 
-var largestNumber179 = (nums) =>
-  nums
-    .sort((a, b) => `${b}${a}` - `${a}${b}`)
-    .reduce((p, c) => (p === `0` ? '0' : `${p}${c}`), '');
+var largestNumber179 = function(nums) {
+  nums.sort(compareFunc);
+
+  let res = '';
+
+  for (let j = 0; j < nums.length; j++) {
+    res += nums[j];
+  }
+
+  if (res.charAt(0) === '0') return '0';
+  return res;
+
+  function compareFunc(numStr1, numStr2) {
+    let combinedNumStr1 = numStr1 + '' + numStr2;
+    let combinedNumStr2 = numStr2 + '' + numStr1;
+
+    return combinedNumStr2 - combinedNumStr1;
+  }
+};
 
 export default largestNumber179;

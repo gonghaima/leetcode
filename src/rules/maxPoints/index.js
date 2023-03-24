d530eec8a2359780fefc79114ae7ea9a1bbdae3f
Steven Gong <gonghaima@hotmail.com>
2023-03-24 17:02:35 +1100

    slop calc - n ^2

--- src/rules/maxPoints/index.js
+++ src/rules/maxPoints/index.js
@@ -3,6 +3,23 @@
  * @return {number}
  */
 var maxPoints = function(points) {
-    
+    let max = 0;
+    for (const x of points) {
+      const slopes = new Map();
+    for (const y of points) {
+      if (x === y) continue;
+      let slope = Infinity;
+      if (y[0] - x[0] !== 0) {
+        slope = (y[1] - x[1]) / (y[0] - x[0]);
+      }
+      if (slopes.has(slope)) {
+        slopes.set(slope, slopes.get(slope) + 1);
+      } else {
+        slopes.set(slope, 1);
+      }
+      max = Math.max(max, slopes.get(slope));
+    }
+  }
+  return max + 1;
 };
 export default maxPoints;

f3ecaddc535f4fe36fb2c377785c21b05097c1d7
Steven Gong <gonghaima@hotmail.com>
2023-04-09 19:22:04 +1000

    two pointers

--- src/rules/trap/index.js
+++ src/rules/trap/index.js
@@ -5,32 +5,33 @@
 
 // https://leetcode.com/problems/trapping-rain-water/discuss/17357/Sharing-my-simple-c%2B%2B-code%3A-O(n)-time-O(1)-space
 
-/***************************************************************************************************
- *  Runtime: 84 ms, faster than 74.58% of JavaScript online submissions for Trapping Rain Water.   *
- * Memory Usage: 40 MB, less than 97.90% of JavaScript online submissions for Trapping Rain Water. *
- ***************************************************************************************************/
-
-// Stack
+// Two pointers
 
 var trap = function(height) {
+  if (height == null || height.length === 0) return 0;
+
+  let l = 0;
+  let r = height.length - 1;
+
+  let lMax = 0;
+  let rMax = 0;
+
   let res = 0;
-  let i = 0;
-  const st = [];
 
-  while (i < height.length) {
-    while (st.length !== 0 && height[i] > height[st[st.length - 1]]) {
-      const top = st[st.length - 1];
-      st.pop();
-
-      if (st.length === 0) break;
-
-      const dist = i - st[st.length - 1] - 1;
-      const h = Math.min(height[i], height[st[st.length - 1]]) - height[top];
-      res += dist * h;
+  while (l < r) {
+    lMax = Math.max(lMax, height[l]);
+    if (height[l] < lMax) {
+      res += lMax - height[l];
     }
-    st.push(i);
-    i++;
+
+    rMax = Math.max(rMax, height[r]);
+    if (height[r] < rMax) {
+      res += rMax - height[r];
+    }
+
+    height[l] < height[r] ? l++ : r--;
   }
+
   return res;
 };
 

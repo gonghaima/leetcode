538358c02d6d10d4275bcc747f2f4ca39d3d2e57
Steven Gong <gonghaima@hotmail.com>
2023-04-13 10:37:04 +1000

    brute force

--- src/rules/getSkyline/index.js
+++ src/rules/getSkyline/index.js
@@ -3,7 +3,20 @@
  * @return {number[][]}
  */
 var getSkyline = function(buildings) {
-    
+  var res = [],
+    x = [],
+    t = 0;
+  buildings.forEach((v) => (x.push(v[0]), x.push(v[1])));
+  for (let c of x.sort((a, b) => a - b))
+    if (
+      res[res.length - 1]?.[1] !==
+      (t = buildings.reduce(
+        (m, [b, e, h]) => (b <= c && c < e && h > m ? h : m),
+        0
+      ))
+    )
+      res.push([c, t]);
+  return res;
 };
 
 export default getSkyline;

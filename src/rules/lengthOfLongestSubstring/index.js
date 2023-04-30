cd37c01235dde0a208703eefdbd141c0c642dcf5
Steven Gong <gonghaima@hotmail.com>
2023-04-30 10:34:01 +1000

    two pointers

--- src/rules/lengthOfLongestSubstring/index.js
+++ src/rules/lengthOfLongestSubstring/index.js
@@ -2,6 +2,24 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {};
+var lengthOfLongestSubstring = function(s) {
+  let set = new Set();
+  let left = 0;
+  let maxSize = 0;
+
+  if (s.length === 0) return 0;
+  if (s.length === 1) return 1;
+
+  for (let i = 0; i < s.length; i++) {
+
+      while (set.has(s[i])) {
+          set.delete(s[left])
+          left++;
+      }
+      set.add(s[i]);
+      maxSize = Math.max(maxSize, i - left + 1)
+  }
+  return maxSize;
+};
 
 export default lengthOfLongestSubstring;

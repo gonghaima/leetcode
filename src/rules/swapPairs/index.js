5c63029cfc9ee3997188ec1647ba0271ef92f186
Steven Gong <gonghaima@hotmail.com>
2023-05-16 18:51:30 +1000

    iterative

--- src/rules/swapPairs/index.js
+++ src/rules/swapPairs/index.js
@@ -5,18 +5,39 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-  if (!head || !head.next) return head;
-  const v1 = head,
-    v2 = head.next,
-    v3 = v2.next;
-  v2.next = v1;
-  v1.next = swapPairs(v3);
-  return v2;
+  // Store head to return list, Only instance of new data so space is O(1)
+  let dummy = new ListNode(-1);
+  dummy.next = head;
+
+  let prev = dummy;
+
+  // Traverse list
+  while (head && head.next) {
+    // Declare
+    let n1 = head;
+    let n2 = head.next;
+
+    // Swap
+    prev.next = n2;
+    n1.next = n2.next;
+    n2.next = n1;
+
+    // Assign
+    prev = n1;
+    head = n1.next;
+  }
+
+  // The variable prev has been used to create the dummy list. dummy.next is still the head of new list
+  return dummy.next;
 };
 
 export default swapPairs;

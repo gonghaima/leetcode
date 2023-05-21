2e8af4b6bc2a1337821422cef3679d9e943ef5fc
Steven Gong <gonghaima@hotmail.com>
2023-05-21 10:10:33 +1000

    31. Next Permutation - initial setup

---
+++ test/rules/nextPermutation.test.js
@@ -0,0 +1,18 @@
+import nextPermutation from '../../src/rules/nextPermutation';
+
+import { inputData, expected } from '../data/nextPermutationData';
+
+test('nextPermutation should return expected result for case1', () => {
+  const result = nextPermutation(inputData.case1);
+  expect(result).toEqual(expected.case1);
+});
+
+test('nextPermutation should return expected result for case2', () => {
+  const result = nextPermutation(inputData.case2);
+  expect(result).toEqual(expected.case2);
+});
+
+test('nextPermutation should return expected result for case3', () => {
+  const result = nextPermutation(inputData.case3);
+  expect(result).toEqual(expected.case3);
+});

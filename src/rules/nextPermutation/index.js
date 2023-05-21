d00a74e29ffabfb3999a6c23b808c1aedb3d5ebc
Steven Gong <gonghaima@hotmail.com>
2023-05-21 10:10:33 +1000

    31. Next Permutation - initial setup

---
+++ src/rules/nextPermutation/index.js
@@ -0,0 +1,6 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {};
+export default nextPermutation;
---
+++ test/data/nextPermutationData.js
@@ -0,0 +1,11 @@
+export const inputData = {
+  case1: [1, 2, 3],
+  case2: [3, 2, 1],
+  case3: [1, 1, 5],
+};
+
+export const expected = {
+  case1: [1, 3, 2],
+  case2: [1, 2, 3],
+  case3: [1, 5, 1],
+};

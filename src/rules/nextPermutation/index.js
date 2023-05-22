d5a9c5791fa277b1f053e9eb0b8f2c59b2477ddd
Steven Gong <gonghaima@hotmail.com>
2023-05-23 01:26:20 +1000

    iterative - v1

--- src/rules/nextPermutation/index.js
+++ src/rules/nextPermutation/index.js
@@ -6,33 +6,18 @@
 // T.C: O(N)
 // S.C: O(1)
 
-var nextPermutation = function(nums) {
-  let pivot = -1;
-  for (let i = nums.length - 1; i > 0; i--) {
-    if (nums[i] > nums[i - 1]) {
-      pivot = i - 1;
-      break;
-    }
+var nextPermutation = function(N) {
+  const swap = (a, b) => ([N[a], N[b]] = [N[b], N[a]]);
+  let len = N.length - 1,
+    i,
+    j;
+  for (i = len - 1; N[i] >= N[i + 1]; ) i--;
+  for (let k = i + 1; len > k; k++, len--) swap(k, len);
+  if (~i) {
+    for (j = i + 1; N[i] >= N[j]; ) j++;
+    swap(i, j);
   }
-  if (pivot === -1) {
-    // array is in descending order
-    nums.reverse();
-    return;
-  }
-  for (let i = nums.length - 1; i > pivot; i--) {
-    if (nums[i] > nums[pivot]) {
-      swap(nums, i, pivot);
-      break;
-    }
-  }
-  for (let i = pivot + 1, j = nums.length - 1; i < j; i++, j--) {
-    swap(nums, i, j);
-  }
-  return nums;
+  return N;
 };
-
-function swap(nums, i, j) {
-  [nums[i], nums[j]] = [nums[j], nums[i]];
-}
 
 export default nextPermutation;

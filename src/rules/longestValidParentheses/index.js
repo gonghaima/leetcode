1efa375d192ff9917f85d6ef84cf5ec972f78413
Steven Gong <gonghaima@hotmail.com>
2023-05-26 09:01:50 +1000

    iterative solution

--- src/rules/longestValidParentheses/index.js
+++ src/rules/longestValidParentheses/index.js
@@ -2,7 +2,13 @@
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function(s) {
-    
+var longestValidParentheses = function(S) {
+  let stack = [-1], ans = 0
+  for (let i = 0; i < S.length; i++)
+      if (S[i] === '(') stack.push(i)
+      else if (stack.length === 1) stack[0] = i
+      else {
+        stack.pop(); ans = Math.max(ans, i - stack[stack.length-1])}
+  return ans
 };
 export default longestValidParentheses;

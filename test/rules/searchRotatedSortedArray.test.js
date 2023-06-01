c2c177cec3cf4061bff8da3b7cdefafef26a5002
Steven Gong <gonghaima@hotmail.com>
2023-06-01 20:40:13 +1000

    binary search

--- test/rules/searchRotatedSortedArray.test.js
+++ test/rules/searchRotatedSortedArray.test.js
@@ -2,19 +2,19 @@ import searchRotatedSortedArray from '../../src/rules/searchRotatedSortedArray';
 import { inputData, expected } from '../data/searchRotatedSortedArrayData';
 
 test('searchRotatedSortedArray should return expected result for case1', () => {
-  const { ums, target } = inputData.case1;
-  const result = searchRotatedSortedArray(ums, target);
+  const { nums, target } = inputData.case1;
+  const result = searchRotatedSortedArray(nums, target);
   expect(result).toEqual(expected.case1);
 });
 
 test('searchRotatedSortedArray should return expected result for case2', () => {
-  const { ums, target } = inputData.case2;
-  const result = searchRotatedSortedArray(ums, target);
+  const { nums, target } = inputData.case2;
+  const result = searchRotatedSortedArray(nums, target);
   expect(result).toEqual(expected.case2);
 });
 
 test('searchRotatedSortedArray should return expected result for case3', () => {
-  const { ums, target } = inputData.case3;
-  const result = searchRotatedSortedArray(ums, target);
+  const { nums, target } = inputData.case3;
+  const result = searchRotatedSortedArray(nums, target);
   expect(result).toEqual(expected.case3);
 });

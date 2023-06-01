eb0bf3cd8bb36cb371193e724c5e32b399db7f4b
Steven Gong <gonghaima@hotmail.com>
2023-06-01 20:40:13 +1000

    binary search

--- src/rules/searchRotatedSortedArray/index.js
+++ src/rules/searchRotatedSortedArray/index.js
@@ -4,7 +4,42 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    // When dividing the roated array into two halves, one must be sorted.
+
+    // Check if the left side is sorted
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target <= nums[mid]) {
+        // target is in the left
+        right = mid - 1;
+      } else {
+        // target is in the right
+        left = mid + 1;
+      }
+    }
+
+    // Otherwise, the right side is sorted
+    else {
+      if (nums[mid] <= target && target <= nums[right]) {
+        // target is in the right
+        left = mid + 1;
+      } else {
+        // target is in the left
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
 };
 
 export default search;

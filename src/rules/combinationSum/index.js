96d6a0f3683758cca3d965fbed6d7c8aab03af38
Steven Gong <gonghaima@hotmail.com>
2023-06-04 17:02:30 +1000

    backtracking

--- src/rules/combinationSum/index.js
+++ src/rules/combinationSum/index.js
@@ -3,5 +3,20 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function(candidates, target) {};
+var combinationSum = function(candidates, target) {
+  var buffer = [];
+  var result = [];
+  search(0, target);
+  return result;
+
+  function search(startIdx, target) {
+    if (target === 0) return result.push(buffer.slice());
+    if (target < 0) return;
+    if (startIdx === candidates.length) return;
+    buffer.push(candidates[startIdx]);
+    search(startIdx, target - candidates[startIdx]);
+    buffer.pop();
+    search(startIdx + 1, target);
+  }
+};
 export default combinationSum;

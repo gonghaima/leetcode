0e47aa5c47c1e5d876e069b58ad96749199eed0f
Steven Gong <gonghaima@hotmail.com>
2023-06-09 17:46:48 +1000

    bfs

--- src/rules/permutations/index.js
+++ src/rules/permutations/index.js
@@ -3,32 +3,29 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-  const output = [];
-  /**
-   * The goal is break down the problem by finding permutations in subarrays.
-   * So we will maintain a subarray of fixed elements and a subarray for 
-   * exploring permutations.
-   *
-   *                  [1], [2, 3]    [1, 2], [3]    [1, 2, 3]
-   * [], [1, 2, 3] -> [2], [1, 3] -> [1, 3], [2] -> [1, 3, 2]
-   *                  [3], [1, 2]    [2, 1], [1]    [2, 1, 3]
-   *                                 [2, 3], [1]    [2, 3, 1]
-   *                                 [3, 1], [2]    [3, 1, 2]
-   *                                 [3, 2], [1]    [3, 2, 1]                                 
-   */
-  const dfs = (curr, rest) => {
-    // base case. Found a permutation because there's nothing else to explore.
-    if (rest.length === 0) {
-      output.push(curr);
-      return;
+  const result = [];
+  const queue = [];
+
+  queue.push([[], nums]);
+
+  while (queue.length) {
+    const [currentSequence, availableNumbers] = queue.shift();
+
+    if (availableNumbers.length === 0) {
+      result.push(currentSequence);
+      continue;
     }
-    for (let i = 0; i < rest.length; i++) {
-      dfs([...curr, rest[i]], [...rest.slice(0, i), ...rest.slice(i + 1)]);
+
+    for (let i = 0; i < availableNumbers.length; i++) {
+      const number = availableNumbers[i];
+      queue.push([
+        [...currentSequence, number],
+        [...availableNumbers.slice(0, i), ...availableNumbers.slice(i + 1)],
+      ]);
     }
   }
-  dfs([], nums);
-  
-  return output;
+
+  return result;
 };
 
 export default permute;

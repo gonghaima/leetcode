de386dd235e71d278f4f556d3d02413a2a4ff226
Steven Gong <gonghaima@hotmail.com>
2023-06-10 12:18:14 +1000

    dp

--- src/rules/permutations/index.js
+++ src/rules/permutations/index.js
@@ -2,30 +2,17 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
-  const result = [];
-  const queue = [];
-
-  queue.push([[], nums]);
-
-  while (queue.length) {
-    const [currentSequence, availableNumbers] = queue.shift();
-
-    if (availableNumbers.length === 0) {
-      result.push(currentSequence);
-      continue;
-    }
-
-    for (let i = 0; i < availableNumbers.length; i++) {
-      const number = availableNumbers[i];
-      queue.push([
-        [...currentSequence, number],
-        [...availableNumbers.slice(0, i), ...availableNumbers.slice(i + 1)],
-      ]);
+var permute = function(nums, n = 0) {
+  if (n >= nums.length) return [[]];
+  const res = [];
+  const prevs = permute(nums, n + 1); // permutations of elements after n
+  for (let prev of prevs) {
+    for (let i = 0; i <= prev.length; i++) {
+      let p = prev.slice(0);
+      p.splice(i, 0, nums[n]); // successively insert element n
+      res.push(p);
     }
   }
-
-  return result;
+  return res;
 };
-
 export default permute;

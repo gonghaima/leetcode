3ece433b689e06d14f391df998f533cd77f53b54
Steven Gong <gonghaima@hotmail.com>
2023-06-21 17:48:52 +1000

    iterative

--- src/rules/mergeIntervals/index.js
+++ src/rules/mergeIntervals/index.js
@@ -2,6 +2,18 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var mergeIntervals = function(intervals) {};
+var mergeIntervals = function(intervals) {
+  if (!intervals.length) return [];
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  const result = [intervals[0]];
+
+  for (let [start, end] of intervals) {
+    const endPrev = result.at(-1)[1];
+    if (start <= endPrev) result.at(-1)[1] = Math.max(end, endPrev);
+    else result.push([start, end]);
+  }
+  return result;
+};
 
 export default mergeIntervals;

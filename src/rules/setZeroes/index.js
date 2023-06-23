a49e77090a83a177546764ad78344b642ae7cab5
Steven Gong <gonghaima@hotmail.com>
2023-06-23 10:33:09 +1000

    my solution

--- src/rules/setZeroes/index.js
+++ src/rules/setZeroes/index.js
@@ -2,6 +2,33 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var setZeroes = function(matrix) {};
+var setZeroes = function(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    const element = matrix[i];
+    for (let j = 0; j < element.length; j++) {
+      const currentVal = element[j];
+      if (currentVal === 0) {
+        matrix[i][j] = 'Original';
+        //set row
+        for (let x = 0; x < matrix[i].length; x++) {
+          if (x !== j && matrix[i][x] !== 0) matrix[i][x] = 'Updated';
+        }
+        // set col
+        for (let y = 0; y < matrix.length; y++) {
+          if (y !== i && matrix[y][j] !== 0) matrix[y][j] = 'Updated';
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      const element = matrix[i][j];
+      if (element === 'Original' || element === 'Updated') matrix[i][j] = 0;
+    }
+  }
+
+  return matrix;
+};
 
 export default setZeroes;

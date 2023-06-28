1acc590753db11d3140fcd033dcfef3c62883ce4
Steven Gong <gonghaima@hotmail.com>
2023-06-28 13:41:12 +1000

    binary search - O(mlog n)

--- src/rules/searchMatrix/index.js
+++ src/rules/searchMatrix/index.js
@@ -4,17 +4,18 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-  if (!matrix.length || !matrix[0].length) return false;
+  for (let i = 0; i < matrix.length; i++) {
+    let start = 0,
+      end = matrix[0].length - 1;
 
-  let row = 0;
-  let col = matrix[0].length - 1;
+    while (start <= end) {
+      let mid = Math.floor((start + end) / 2);
+      if (matrix[i][mid] === target) return true;
 
-  while (col >= 0 && row <= matrix.length - 1) {
-    if (matrix[row][col] === target) return true;
-    else if (matrix[row][col] > target) col--;
-    else if (matrix[row][col] < target) row++;
+      if (matrix[i][mid] > target) end = mid - 1;
+      else start = mid + 1;
+    }
   }
-
   return false;
 };
 

e83322b3a1d3110ebb50829f88fd7548dce0cd09
Steven Gong <gonghaima@hotmail.com>
2023-06-29 15:24:57 +1000

    O (logMN)

--- src/rules/searchMatrix/index.js
+++ src/rules/searchMatrix/index.js
@@ -4,17 +4,17 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-  for (let i = 0; i < matrix.length; i++) {
-    let start = 0,
-      end = matrix[0].length - 1;
+  let start = 0,
+    end = matrix.length * matrix[0].length - 1;
 
-    while (start <= end) {
-      let mid = Math.floor((start + end) / 2);
-      if (matrix[i][mid] === target) return true;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    let midNum =
+      matrix[Math.floor(mid / matrix[0].length)][mid % matrix[0].length];
 
-      if (matrix[i][mid] > target) end = mid - 1;
-      else start = mid + 1;
-    }
+    if (midNum === target) return true;
+    else if (midNum < target) start = mid + 1;
+    else end = mid - 1;
   }
   return false;
 };

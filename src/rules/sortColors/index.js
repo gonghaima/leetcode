1031f5feca26b40f0a2fbdcb942503f2f264edb9
Steven Gong <gonghaima@hotmail.com>
2023-07-03 12:38:19 +1000

    One pass algorithm

--- src/rules/sortColors/index.js
+++ src/rules/sortColors/index.js
@@ -3,25 +3,27 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(arr) {
-  let one = 0,
-    zero = 0,
-    two = 0;
+  let low = 0,
+    mid = 0,
+    high = arr.length - 1;
 
-  // step1
-  for (let elem of arr) {
-    if (elem == 0) zero++;
-    else if (elem == 1) one++;
-    else two++;
+  while (mid <= high) {
+    if (arr[mid] == 0) {
+      swap(low, mid);
+      mid++;
+      low++;
+    } else if (arr[mid] == 1) {
+      mid++;
+    } else if (arr[mid] == 2) {
+      swap(mid, high);
+      high--;
+    }
   }
 
-  // step2
-  arr.length = 0;
-
-  // step3
-  for (let i = 0; i < zero; i++) arr.push(0);
-  for (let i = 0; i < one; i++) arr.push(1);
-  for (let i = 0; i < two; i++) arr.push(2);
-
   return arr;
+
+  function swap(a, b) {
+    [arr[b], arr[a]] = [arr[a], arr[b]];
+  }
 };
 export default sortColors;

f6654830354b0a688460caa8aa56216f227dd895
Steven Gong <gonghaima@hotmail.com>
2023-07-06 17:36:19 +1000

    bubble sort

--- src/rules/sortColors/index.js
+++ src/rules/sortColors/index.js
@@ -3,14 +3,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-  for (let i = nums.length - 1; i > 0; i--) {
-    for (let j = 0; j <= i - 1; j++) {
-      if (nums[j] > nums[j + 1]) {
-        let temp = nums[j + 1];
-        nums[j + 1] = nums[j];
-        nums[j] = temp;
-      }
-    }
+  let zero = 0,
+    one = 0,
+    two = 0;
+  for (let n of nums) {
+    n === 0 ? zero++ : n === 1 ? one++ : two++;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    i < zero ? (nums[i] = 0) : i < zero + one ? (nums[i] = 1) : (nums[i] = 2);
   }
   return nums;
 };

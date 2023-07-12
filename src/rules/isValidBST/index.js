ad55ba72f5984c88aa2ca2034fda2d2305040249
Steven Gong <gonghaima@hotmail.com>
2023-07-12 13:13:47 +1000

    my solution

--- src/rules/isValidBST/index.js
+++ src/rules/isValidBST/index.js
@@ -10,5 +10,18 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {};
+var isValidBST = function(root) {
+  //compare left with right
+  // if not the same return false
+  if (root?.left?.val > root.val || root?.right?.val < root.val) {
+    return false;
+  }
+
+  // otherwise do the isValidBST for both left and right
+  if (root?.left?.val) isValidBST(root.left);
+  if (root?.right?.val) isValidBST(root.right);
+
+  // return true
+  return true;
+};
 export default isValidBST;

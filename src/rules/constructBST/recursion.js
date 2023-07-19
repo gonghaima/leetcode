ff6d8310a6fcf983185854458b485f115d0f4fc6
Steven Gong <gonghaima@hotmail.com>
2023-07-19 11:38:39 +1000

    recursion 1

--- src/rules/constructBST/recursion.js
+++ src/rules/constructBST/recursion.js
@@ -16,13 +16,18 @@ function TreeNode(val, left, right) {
  * @param {number[]} inorder
  * @return {TreeNode}
  */
-const buildTree = (preorder, inorder) => {
-  if (!preorder.length || !inorder.length) return null;
-
-  let root = new TreeNode(preorder[0]);
-  let mid = inorder.indexOf(preorder[0]);
-  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
-  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+var buildTree = function(P, I) {
+  let M = new Map();
+  for (let i = 0; i < I.length; i++) M.set(I[i], i);
+  return splitTree(P, M, 0, 0, I.length - 1);
+};
+var splitTree = function(P, M, pix, ileft, iright) {
+  let rval = P[pix],
+    root = new TreeNode(rval),
+    imid = M.get(rval);
+  if (imid > ileft) root.left = splitTree(P, M, pix + 1, ileft, imid - 1);
+  if (imid < iright)
+    root.right = splitTree(P, M, pix + imid - ileft + 1, imid + 1, iright);
   return root;
 };
 export default buildTree;

5be90c97af6a52a1bab127b57ea8ff4a4dcbbfc4
Steven Gong <gonghaima@hotmail.com>
2023-07-31 14:35:16 +1000

    fast & slow pointer

--- src/rules/detectCycle/index.js
+++ src/rules/detectCycle/index.js
@@ -10,5 +10,21 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var detectCycle = function(head) {};
+var detectCycle = function(head) {
+    let slow = head;
+    let fast = head;
+    while(fast && fast.next && fast.next.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow === fast){
+            slow = head;
+            while(slow !== fast){
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
+    return null;
+};
 export default detectCycle;

ac6654b6105eea18723e58f61a22feb69075079a
Steven Gong <gonghaima@hotmail.com>
2023-07-31 14:35:16 +1000

    fast & slow pointer

--- test/rules/detectCycle.test.js
+++ test/rules/detectCycle.test.js
@@ -1,21 +1,21 @@
-import detectCycle from '../../src/rules/detectCycle/dp';
+import detectCycle from '../../src/rules/detectCycle';
 
 import { inputData, expected } from '../data/detectCycleData';
 
 test('detectCycle should return expected result for case1', () => {
   const { head } = inputData.case1;
   const result = detectCycle(head);
-  expect(result).toEqual(expected.case1);
+  expect(result).toEqual(null);
 });
 
 test('detectCycle should return expected result for case2', () => {
   const { head } = inputData.case2;
   const result = detectCycle(head);
-  expect(result).toEqual(expected.case2);
+  expect(result).toEqual(null);
 });
 
 test('detectCycle should return expected result for case3', () => {
   const { head } = inputData.case2;
   const result = detectCycle(head);
-  expect(result).toEqual(expected.case2);
+  expect(result).toEqual(null);
 });

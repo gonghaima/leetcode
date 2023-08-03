235e0b70023ca6114f72e5a394cfaed25a4aacea
Steven Gong <gonghaima@hotmail.com>
2023-08-03 19:00:07 +1000

    binary search

--- src/rules/findMin/index.js
+++ src/rules/findMin/index.js
@@ -2,5 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {};
+var findMin = function(nums) {
+  var left = 0,
+    right = nums.length - 1;
+
+  while (left < right) {
+    var mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) left = mid + 1;
+    else right = mid;
+  }
+  return nums[left];
+};
 export default findMin;

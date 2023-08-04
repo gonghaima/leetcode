2dff9612a5755533a3d098553afc7c4fe4f73e74
Steven Gong <gonghaima@hotmail.com>
2023-08-05 09:16:33 +1000

    dfs

--- src/rules/rightSideView/index.js
+++ src/rules/rightSideView/index.js
@@ -10,5 +10,17 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var rightSideView = function(root) {};
+var rightSideView = function(root) {
+  if (!root) return [];
+  let res = [];
+  pre(root, 0);
+  return res;
+
+  function pre(node, h) {
+    if (!node) return;
+    res[h] = node.val;
+    pre(node.left, h + 1);
+    pre(node.right, h + 1);
+  }
+};
 export default rightSideView;

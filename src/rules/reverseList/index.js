59d85ccc838d59a34101dd5ce8f766e1672b2f3a
Steven Gong <gonghaima@hotmail.com>
2023-08-09 13:32:05 +1000

    dfs

--- src/rules/reverseList/index.js
+++ src/rules/reverseList/index.js
@@ -9,6 +9,16 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {};
+var reverseList = function(head) {
+  // Special case...
+  if (head == null || head.next == null) return head;
+  // Create a new node to call the function recursively and we get the reverse linked list...
+  var res = reverseList(head.next);
+  // Set head node as head.next.next...
+  head.next.next = head;
+  //set head's next to be null...
+  head.next = null;
+  return res; // Return the reverse linked list...
+};
 
 export default reverseList;

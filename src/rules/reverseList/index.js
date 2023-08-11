a6741e8b2c56d7b53feae54bf57d08f816c51605
Steven Gong <gonghaima@hotmail.com>
2023-08-11 11:16:01 +1000

    iterative

--- src/rules/reverseList/index.js
+++ src/rules/reverseList/index.js
@@ -10,15 +10,11 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-  // Special case...
-  if (head == null || head.next == null) return head;
-  // Create a new node to call the function recursively and we get the reverse linked list...
-  var res = reverseList(head.next);
-  // Set head node as head.next.next...
-  head.next.next = head;
-  //set head's next to be null...
-  head.next = null;
-  return res; // Return the reverse linked list...
+  let [prev, current] = [null, head];
+  while (current) {
+    [current.next, prev, current] = [prev, current, current.next];
+  }
+  return prev;
 };
 
 export default reverseList;

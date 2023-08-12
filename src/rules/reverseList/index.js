3f6c4dc091f356640328454a3a1b4872032a8bdc
Steven Gong <gonghaima@hotmail.com>
2023-08-12 17:20:58 +1000

    interative - v1

--- src/rules/reverseList/index.js
+++ src/rules/reverseList/index.js
@@ -10,10 +10,18 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-  let [prev, current] = [null, head];
-  while (current) {
-    [current.next, prev, current] = [prev, current, current.next];
+  // End of the reversed linked list set to null
+  let prev = null;
+
+  // Traverse through the given linked list
+  while (head) {
+    const temp = head.next; // References the next Node of given linked list
+    head.next = prev; // head.next point to previous Node, instead of the next Node of the given linked list
+    prev = head; // Move the prev Node pointer up to head
+    head = temp; // Move the head up to next Node of the given linked list
   }
+
+  // Prev is the reversed linked list
   return prev;
 };
 

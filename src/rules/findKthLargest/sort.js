d944eaf3c6be1b887b2367592261f6d651d0d3f0
Steven Gong <gonghaima@hotmail.com>
2023-08-19 09:10:21 +1000

    minHeap solution

---
+++ src/rules/findKthLargest/sort.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+  nums.sort((a, b) => b - a);
+  return nums[k - 1];
+};
+
+export default findKthLargest;

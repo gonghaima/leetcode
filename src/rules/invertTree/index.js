d7f89019598fdba23cf56b4cb1a030210ecb7675
Steven Gong <gonghaima@hotmail.com>
2023-08-22 19:43:01 +1000

    dfs

--- src/rules/invertTree/index.js
+++ src/rules/invertTree/index.js
@@ -10,5 +10,21 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = function(root) {};
+var invertTree = function(root) {
+  // Base case...
+  if (root == null) {
+    return root;
+  }
+
+  // Call the function recursively for the left subtree...
+  invertTree(root.left);
+  // Call the function recursively for the right subtree...
+  invertTree(root.right);
+
+  // swapping process...
+  const curr = root.left;
+  root.left = root.right;
+  root.right = curr;
+  return root; // Return the root...
+};
 export default invertTree;

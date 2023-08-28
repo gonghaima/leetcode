e155d60b543805276a3857cc43b95e575bb337f4
Steven Gong <gonghaima@hotmail.com>
2023-08-28 18:49:55 +1000

    bfs

--- src/rules/kthSmallestBST/inOrder.js
+++ src/rules/kthSmallestBST/inOrder.js
@@ -12,20 +12,16 @@
  * @return {number}
  */
 var kthSmallest = function(root, k) {
-  const stack = [];
-  let count = 1;
-  let node = root;
-
-  while (node || stack.length) {
-    while (node) {
-      stack.push(node);
-      node = node.left;
-    }
-    node = stack.pop();
-    if (count === k) return node.val;
-    else count++;
-    node = node.right;
-  }
+  let n = 0;
+  let res;
+  const inorder = (root) => {
+    if (!root) return;
+    inorder(root.left);
+    if (n++ < k) res = root.val;
+    inorder(root.right);
+  };
+  inorder(root);
+  return res;
 };
 
 export default kthSmallest;

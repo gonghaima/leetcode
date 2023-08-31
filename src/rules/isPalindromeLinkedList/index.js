4b90f4a8facb4cae769988d7e515b7856d465f8d
Steven Gong <gonghaima@hotmail.com>
2023-09-01 07:13:47 +1000

    backtrack

--- src/rules/isPalindromeLinkedList/index.js
+++ src/rules/isPalindromeLinkedList/index.js
@@ -10,19 +10,20 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-  let slow = head,
-    fast = head,
-    prev,
-    temp;
-  while (fast && fast.next) (slow = slow.next), (fast = fast.next.next);
-  (prev = slow), (slow = slow.next), (prev.next = null);
-  while (slow)
-    (temp = slow.next), (slow.next = prev), (prev = slow), (slow = temp);
-  (fast = head), (slow = prev);
-  while (slow)
-    if (fast.val !== slow.val) return false;
-    else (fast = fast.next), (slow = slow.next);
-  return true;
+  let curr = head;
+  const traverse = (node) => {
+    if (node === null) {
+      return true;
+    }
+    // traverse to the end of the list first
+    const prevIsSame = traverse(node.next);
+    // when the call stack bounces back, compare the values
+    // from the head and from the bottom up
+    const currIsSame = curr.val === node.val;
+    curr = curr.next;
+    return prevIsSame && currIsSame;
+  };
+  return traverse(head);
 };
 
 export default isPalindrome;

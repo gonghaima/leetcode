a73dc3591955b3ba3ec772c791289012bffd61fc
Steven Gong <gonghaima@hotmail.com>
2023-09-09 09:51:21 +1000

    347. Top K Frequent Elements - initial setup

---
+++ test/data/topKFrequentElementData.js
@@ -0,0 +1,12 @@
+export const inputData = {
+  case1: { nums: [1, 1, 1, 2, 2, 3], k: 2 },
+  case2: {
+    nums: [1],
+    k: 1,
+  },
+};
+
+export const expected = {
+  case1: [1, 2],
+  case2: [1],
+};

abee8b121fe8db4b1a3fd97ff01f5a2f86f7f2ea
Steven Gong <gonghaima@hotmail.com>
2023-09-11 18:32:53 +1000

    use freqMap

--- test/rules/topKFrequentElement.test.js
+++ test/rules/topKFrequentElement.test.js
@@ -2,12 +2,12 @@ import topKFrequentElement from '../../src/rules/topKFrequentElement';
 import { inputData, expected } from '../data/topKFrequentElementData';
 
 test('topKFrequentElement should return expected result for case1', () => {
-  const { words, k } = inputData.case1;
-  const result = topKFrequentElement(words, k);
+  const { nums, k } = inputData.case1;
+  const result = topKFrequentElement(nums, k);
   expect(result).toEqual(expected.case1);
 });
 test('topKFrequentElement should return expected result for case2', () => {
-  const { words, k } = inputData.case2;
-  const result = topKFrequentElement(words, k);
+  const { nums, k } = inputData.case2;
+  const result = topKFrequentElement(nums, k);
   expect(result).toEqual(expected.case2);
 });

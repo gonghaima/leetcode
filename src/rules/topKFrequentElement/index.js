5659765bff372fc5ccd89f6762c03b1a376733c0
Steven Gong <gonghaima@hotmail.com>
2023-09-12 21:11:33 +1000

    hashmap

--- src/rules/topKFrequentElement/index.js
+++ src/rules/topKFrequentElement/index.js
@@ -7,23 +7,16 @@
 // put it to max heap, then take the top n
 
 var topKFrequentElement = function(nums, k) {
-  const freqMap = new Map();
-  const bucket = [];
-  const result = [];
-
+  const map = new Map(); //! map to count the frequency of the number
   for (let num of nums) {
-    freqMap.set(num, (freqMap.get(num) || 0) + 1);
+    map.set(num, map.get(num) + 1 || 1);
   }
-
-  for (let [num, freq] of freqMap) {
-    bucket[freq] = (bucket[freq] || new Set()).add(num);
+  const result = [];
+  for (let [key, value] of map) {
+    result.push([key, value]); //! we will add the number and its frequency
   }
-
-  for (let i = bucket.length - 1; i >= 0; i--) {
-    if (bucket[i]) result.push(...bucket[i]);
-    if (result.length === k) break;
-  }
-  return result;
+  result.sort((a, b) => b[1] - a[1]); //! we will solve with respect to the frequency of the number
+  return result.slice(0, k).map((x) => x[0]); //! we will slice the list with respect to length of k
 };
 
 export default topKFrequentElement;

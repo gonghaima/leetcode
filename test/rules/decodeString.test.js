a72da984c8341b875e7d5e981cbecf81c152b32f
Steven Gong <gonghaima@hotmail.com>
2023-09-15 13:29:39 +1000

    stack

--- test/rules/decodeString.test.js
+++ test/rules/decodeString.test.js
@@ -3,19 +3,16 @@ import decodeString from '../../src/rules/decodeString';
 import { inputData, expected } from '../data/decodeStringData';
 
 test('decodeString should return expected result for case1', () => {
-  const { s, k } = inputData.case1;
-  const result = decodeString(s, k);
+  const result = decodeString(inputData.case1);
   expect(result).toEqual(expected.case1);
 });
 
 test('decodeString should return expected result for case2', () => {
-  const { s, k } = inputData.case2;
-  const result = decodeString(s, k);
+  const result = decodeString(inputData.case2);
   expect(result).toEqual(expected.case2);
 });
 
 test('decodeString should return expected result for case3', () => {
-  const { s, k } = inputData.case3;
-  const result = decodeString(s, k);
+  const result = decodeString(inputData.case3);
   expect(result).toEqual(expected.case3);
 });

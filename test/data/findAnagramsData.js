e2bdbfb9c1d01701274890686fcf51302435b0d6
Steven Gong <gonghaima@hotmail.com>
2023-09-19 17:29:21 +1000

    two pointers

--- test/data/findAnagramsData.js
+++ test/data/findAnagramsData.js
@@ -1,9 +1,9 @@
 export const inputData = {
   case1: { s: 'cbaebabacd', p: 'abc' },
-  case1: { s: 'abab', p: 'ab' },
+  case2: { s: 'abab', p: 'ab' },
 };
 
 export const expected = {
   case1: [0, 6],
-  case1: [0, 1, 2],
+  case2: [0, 1, 2],
 };

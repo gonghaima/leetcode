d48ce095365f124f35ef647c439af8c12a2d8ec3
Steven Gong <gonghaima@hotmail.com>
2023-09-25 17:50:57 +1000

    hashmap

--- src/rules/subarraySum/index.js
+++ src/rules/subarraySum/index.js
@@ -3,6 +3,18 @@
  * @param {number} k
  * @return {number}
  */
-var subarraySum = function(nums, k) {};
+var subarraySum = function(nums, k) {
+  let map = new Map();
+  let sum = 0;
+  let count = 0;
+  map.set(0, 1);
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (map.has(sum - k)) count += map.get(sum - k);
+    if (map.has(sum)) map.set(sum, map.get(sum) + 1);
+    else map.set(sum, 1);
+  }
+  return count;
+};
 
 export default subarraySum;

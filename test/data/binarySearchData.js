c7e47ab2abf7df600ab9e1409284fd20d9328185
Steven Gong <gonghaima@hotmail.com>
2023-09-29 19:59:06 +1000

    704. Binary Search - initial setup

---
+++ test/data/binarySearchData.js
@@ -0,0 +1,9 @@
+export const inputData = {
+  case1: { nums: [-1, 0, 3, 5, 9, 12], target: 9 },
+  case2: { nums: [-1, 0, 3, 5, 9, 12], target: 2 },
+};
+
+export const expected = {
+  case1: 4,
+  case2: -1,
+};

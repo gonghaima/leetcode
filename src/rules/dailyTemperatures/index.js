4c73bdacf558d8a885ef6b11dacd14cdaf2f2c07
Steven Gong <gonghaima@hotmail.com>
2023-10-02 08:25:22 +1100

    thoughts

--- src/rules/dailyTemperatures/index.js
+++ src/rules/dailyTemperatures/index.js
@@ -3,7 +3,15 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    
+  // thoughts
+  // declare result array
+  // declare tracking array
+  // loop the temperatures from the end to the start position
+  // the last temperature is zero, add the zero the result, add temperate to the tracking array
+  // for each temperature, add find the index of first greater value, compare current index, add the diff to the result
+  // return result
+
+  // question? how to make tracking array more effecient, to find out greater value with min index
 };
 
 export default dailyTemperatures;

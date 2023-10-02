b6ff17b402dfed356267f456d340a8758a4952b9
Steven Gong <gonghaima@hotmail.com>
2023-10-03 09:27:38 +1100

    Monotonic Queues

--- src/rules/dailyTemperatures/index.js
+++ src/rules/dailyTemperatures/index.js
@@ -2,16 +2,17 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-  // thoughts
-  // declare result array
-  // declare tracking array
-  // loop the temperatures from the end to the start position
-  // the last temperature is zero, add the zero the result, add temperate to the tracking array
-  // for each temperature, add find the index of first greater value, compare current index, add the diff to the result
-  // return result
-
-  // question? how to make tracking array more effecient, to find out greater value with min index
+var dailyTemperatures = function(T) {
+  let stack = [];
+  let result = new Array(T.length).fill(0);
+  for (let i = 0; i < T.length; i++) {
+    while (stack.length && T[i] > T[stack[stack.length - 1]]) {
+      let temp = stack.pop();
+      result[temp] = i - temp;
+    }
+    stack.push(i);
+  }
+  return result;
 };
 
 export default dailyTemperatures;

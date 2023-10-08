1a728e9a36f356d774974fd095886d04678d85d9
Steven Gong <gonghaima@hotmail.com>
2023-10-09 08:57:26 +1100

    122. Best Time to Buy and Sell Stock II - dp

--- src/rules/maxProfitII/index.js
+++ src/rules/maxProfitII/index.js
@@ -1,14 +1,17 @@
-/********************************************************************************************************************
- * Runtime: 84 ms, faster than 54.28% of JavaScript online submissions for Best Time to Buy and Sell Stock II.      *
- * Memory Usage: 39.3 MB, less than 50.21% of JavaScript online submissions for Best Time to Buy and Sell Stock II. *
- ********************************************************************************************************************/
+export default (prices) => {
+  // It is impossible to sell stock on first day, set -infinity as initial value for curHold
+  let [curHold, curNotHold] = [-Infinity, 0];
 
-export default prices => {
-  let mp = 0;
-  for (let i = 0; i < prices.length; i++) {
-    if (prices[i] > prices[i - 1]) {
-      mp += prices[i] - prices[i - 1];
-    }
+  for (const stockPrice of prices) {
+    let [prevHold, prevNotHold] = [curHold, curNotHold];
+
+    // either keep hold, or buy in stock today at stock price
+    curHold = Math.max(prevHold, prevNotHold - stockPrice);
+
+    // either keep not-hold, or sell out stock today at stock price
+    curNotHold = Math.max(prevNotHold, prevHold + stockPrice);
   }
-  return mp;
+
+  // Max profit must come from notHold state finally.
+  return curNotHold;
 };

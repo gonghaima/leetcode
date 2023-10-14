ebbe296a66ad57bba60cc26f0c524cca4e139438
Steven Gong <gonghaima@hotmail.com>
2023-10-14 11:50:43 +1100

    27. Remove Element - initial setup

---
+++ test/data/removeElementData.js
@@ -0,0 +1,9 @@
+export const inputData = {
+  case1: { nums: [3, 2, 2, 3], val: 3 },
+  case2: { nums: [0, 1, 2, 2, 3, 0, 4, 2], val: 2 },
+};
+
+export const expected = {
+  case1: 2,
+  case2: 5,
+};

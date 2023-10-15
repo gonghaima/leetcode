8cb7532748ac4d5792bb60619ca0a1bb60692a5c
Steven Gong <gonghaima@hotmail.com>
2023-10-15 18:09:21 +1100

    my simple solution

--- src/rules/removeElement/index.js
+++ src/rules/removeElement/index.js
@@ -3,6 +3,12 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {};
+var removeElement = function(nums, val) {
+  let result = 0;
+  nums.map((num) => {
+    if (num !== val) result++;
+  });
+  return result;
+};
 
 export default removeElement;

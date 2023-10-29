2ae170c87e7a7451ce9b48d372d5e4533765e985
Steven Gong <gonghaima@hotmail.com>
2023-10-30 00:32:43 +1100

    simple sort

--- src/rules/hIndex/index.js
+++ src/rules/hIndex/index.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var hIndex = function(citations) {
-  let a = 0;
-  let b = citations.length - 1;
   citations.sort((a, b) => b - a);
-  while (a <= b) {
-    let mid = a + Math.floor((b - a) / 2);
-    if (citations[mid] > mid) a = mid + 1;
-    else b = mid - 1;
-  }
-  return a;
+  let i = 0;
+  while (citations[i] > i) i++;
+
+  return i;
 };
 
 export default hIndex;

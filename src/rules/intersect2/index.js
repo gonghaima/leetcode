d848a8c3c90a87b48a7f7f8e97405ed099dbfec2
Steven Gong <gonghaima@hotmail.com>
2023-11-05 11:45:26 +1100

    simple solution

--- src/rules/intersect2/index.js
+++ src/rules/intersect2/index.js
@@ -4,6 +4,16 @@
  * @return {number[]}
  */
 var intersect2 = function(nums1, nums2) {
-    
+  let long = nums1.length > nums2.length ? nums1 : nums2;
+  let short = nums1.length > nums2.length ? nums2 : nums1;
+
+  let ans = [];
+  for (let i = 0; i < long.length; i++) {
+    if (short.indexOf(long[i]) !== -1) {
+      ans.push(long[i]);
+      short.splice(short.indexOf(long[i]), 1);
+    }
+  }
+  return ans;
 };
 export default intersect2;

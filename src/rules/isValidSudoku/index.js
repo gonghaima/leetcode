608cf8f17fb4f8d42b064fa01e3a8abff290c495
Steven Gong <gonghaima@hotmail.com>
2023-11-15 08:24:07 +1100

    modular solution

--- src/rules/isValidSudoku/index.js
+++ src/rules/isValidSudoku/index.js
@@ -2,32 +2,66 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function(board) {
-  const set = new Set();
 
-  //i rows, j columns
-  //check valid columns and rows O(9*9)
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[0].length; j++) {
-      const value = board[i][j];
-      if (value !== '.') {
-        const rowString = `${value} at row ${i}`;
-        const colString = `${value} at col ${j}`;
-        const boxString = `${value} at box ${Math.floor(i / 3)}, ${Math.floor(
-          j / 3
-        )}`;
+function isValidRow(board, row, col, value) {
+  // j represents on col
+  for (let j = 0; j < 8; j++) {
+    // check first to see if the current col matches the passed in col
+    if (j !== col) {
+      if (board[row][j] === value) {
+        return false;
+      }
+    }
+  }
 
-        if (set.has(rowString) || set.has(colString) || set.has(boxString)) {
+  return true;
+}
+
+function isValidCol(board, row, col, value) {
+  // j represents on row
+  for (let i = 0; i < 8; i++) {
+    // check first to see if the current row matches the passed in row
+    if (i !== row) {
+      if (board[i][col] === value) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+function isValidBox(board, row, col, value) {
+  const startRow = row - (row % 3);
+  const startCol = col - (col % 3);
+
+  for (let i = startRow; i < startRow + 3; i++) {
+    for (let j = startCol; j < startCol + 3; j++) {
+      if (i !== row && j !== col) {
+        if (board[i][j] === value) {
           return false;
-        } else {
-          set.add(rowString);
-          set.add(colString);
-          set.add(boxString);
         }
       }
     }
   }
 
+  return true;
+}
+
+var isValidSudoku = function(board) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const value = board[i][j];
+      if (value !== '.') {
+        if (
+          !isValidRow(board, i, j, value) ||
+          !isValidCol(board, i, j, value) | !isValidBox(board, i, j, value)
+        ) {
+          return false;
+        }
+      }
+    }
+  }
   return true;
 };
 

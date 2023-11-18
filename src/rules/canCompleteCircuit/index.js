a38b5fb9b1440862ccdfe009e6c092d92724f6df
Steven Gong <gonghaima@hotmail.com>
2023-11-18 12:41:39 +1100

    simple iteration

--- src/rules/canCompleteCircuit/index.js
+++ src/rules/canCompleteCircuit/index.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
-    
+  let curTank = 0,
+    totalTank = 0,
+    pos = 0;
+  for (let i = 0; i < gas.length; i++) {
+    curTank += gas[i] - cost[i];
+    totalTank += gas[i] - cost[i];
+    if (curTank < 0) {
+      curTank = 0;
+      pos = i + 1;
+    }
+  }
+  return totalTank < 0 ? -1 : pos;
 };
 
 export default canCompleteCircuit;

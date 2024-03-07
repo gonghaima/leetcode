575209a017e98d37e4e551e3ebe1cf5238824d76
Steven Gong <gonghaima@hotmail.com>
2024-03-08 00:06:36 +1100

    short solution

--- src/rules/isIsomorphic/index.js
+++ src/rules/isIsomorphic/index.js
@@ -4,17 +4,12 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-  if (s.length != t.length) return false;
-  // Create two maps for s & t strings...
-  const map1 = [256];
-  const map2 = [256];
-  // Traverse all elements through the loop...
-  for (let idx = 0; idx < s.length; idx++) {
-    // Compare the maps, if not equal, return false...
-    if (map1[s.charAt(idx)] != map2[t.charAt(idx)]) return false;
-    // Insert each character if string s and t into seperate map...
-    map1[s.charAt(idx)] = idx + 1;
-    map2[t.charAt(idx)] = idx + 1;
+  var obj = {};
+
+  for (var i = 0; i < s.length; i++) {
+    if (!obj['s' + s[i]]) obj['s' + s[i]] = t[i];
+    if (!obj['t' + t[i]]) obj['t' + t[i]] = s[i];
+    if (t[i] != obj['s' + s[i]] || s[i] != obj['t' + t[i]]) return false;
   }
   return true;
 };

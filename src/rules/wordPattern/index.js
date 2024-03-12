d3800a30eaefa58417edb67cd11cdb08a9c4507a
Steven Gong <gonghaima@hotmail.com>
2024-03-12 18:49:48 +1100

    clean solution

--- src/rules/wordPattern/index.js
+++ src/rules/wordPattern/index.js
@@ -1,23 +1,18 @@
-
 /**
  * @param {string} pattern
  * @param {string} s
  * @return {boolean}
  */
 var wordPattern = function(pattern, s) {
-  const sSplitted = s.split(" ");
-  const matchMap = {};
-  for (let i = 0; i < sSplitted.length; i++) {
-    const patternChar = pattern[i];
-    const element = sSplitted[i];
+  const words = s.split(/\s+/);
+  const map = new Map();
 
-    if(!matchMap[patternChar]) {
-      matchMap[patternChar] = element;
-    } else {
-      if(matchMap[patternChar] !== element) {
-        return false;
-      }
-    }
+  if (words.length !== pattern.length) return false;
+  if (new Set(words).size !== new Set(pattern).size) return false;
+
+  for (let i = 0; i < pattern.length; i++) {
+    if (map.has(pattern[i]) && map.get(pattern[i]) !== words[i]) return false;
+    map.set(pattern[i], words[i]);
   }
   return true;
 };

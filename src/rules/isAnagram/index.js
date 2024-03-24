8102726e1bbccd56a9b357c27627727dfc105196
Steven Gong <gonghaima@hotmail.com>
2024-03-24 20:27:57 +1100

    hashmap

--- src/rules/isAnagram/index.js
+++ src/rules/isAnagram/index.js
@@ -4,14 +4,20 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  if (t.length !== s.length) return false;
-  const counts = {};
-  for (let c of s) {
-    counts[c] = (counts[c] || 0) + 1;
+  if (s.length !== t.length) return false;
+
+  let letters = {};
+  //create hashmap for both words, based on a counter
+  for (let i = 0; i < s.length; i++) {
+    letters[s[i]] = letters[s[i]] ? letters[s[i]] + 1 : 1;
+    letters[t[i]] = letters[t[i]] ? letters[t[i]] - 1 : -1;
   }
-  for (let c of t) {
-    if (!counts[c]) return false;
-    counts[c]--;
+
+  for (let letter in letters) {
+    //this check for duplicates since if all letters are the same, we will end up with 0
+    if (letters[letter] !== 0) {
+      return false;
+    }
   }
   return true;
 };

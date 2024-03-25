e0ac576b804740137a131c388cc7403c009d0f1e
Steven Gong <gonghaima@hotmail.com>
2024-03-26 09:04:09 +1100

    my solution - bfs

--- src/rules/isHappy/index.js
+++ src/rules/isHappy/index.js
@@ -3,7 +3,35 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-    
+  const toSplittedDigits = (param) => param.toString().split('');
+  let foundisHappy = false;
+  let splittedDigits = toSplittedDigits(n);
+  let rootSum = n;
+
+  const getNewSplittedDigits = (digits) => {
+    let sum = 0;
+    let foundIt = false;
+    for (let i = 0; i < digits.length; i++) {
+      const element = digits[i];
+      sum += Number(element) ** 2;
+    }
+    if (sum === 1) {
+      foundIt = true;
+    }
+
+    return { digits: sum.toString(), foundIt };
+  };
+
+  while (!foundisHappy && rootSum > 10) {
+    const { digits, foundIt } = getNewSplittedDigits(splittedDigits);
+    if (foundIt) {
+      foundisHappy = true;
+    }
+    splittedDigits = toSplittedDigits(digits);
+    rootSum = Number(digits);
+  }
+
+  return foundisHappy;
 };
 
 export default isHappy;

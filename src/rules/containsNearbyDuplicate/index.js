45675f293328300f6872f420a98da9fd19746b3e
Steven Gong <gonghaima@hotmail.com>
2024-04-12 17:51:52 +1000

    intuitive solution

--- src/rules/containsNearbyDuplicate/index.js
+++ src/rules/containsNearbyDuplicate/index.js
@@ -4,7 +4,15 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    
+  const hasmap = new Map();
+  for (let idx = 0; idx < nums.length; idx++) {
+      // Check if the difference betweend duplicates is less than k
+      if (idx - hasmap.get(nums[idx]) <= k) {
+          return true;
+      }
+      hasmap.set(nums[idx], idx);
+  }
+  return false;
 };
 
 export default containsNearbyDuplicate;

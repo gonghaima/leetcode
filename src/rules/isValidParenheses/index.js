4a43909163a4ab1aba39be5e19bee3c964ec6e6d
Steven Gong <gonghaima@hotmail.com>
2024-06-08 19:26:45 +1000

    stack

--- src/rules/isValidParenheses/index.js
+++ src/rules/isValidParenheses/index.js
@@ -3,6 +3,23 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+  // Initialize stack to store the closing brackets expected...
+  let stack = [];
+  // Traverse each charater in input string...
+  for (let idx = 0; idx < s.length; idx++) {
+    // If open parentheses are present, push it to stack...
+    if (s[idx] == '{') {
+      stack.push('}');
+    } else if (s[idx] == '[') {
+      stack.push(']');
+    } else if (s[idx] == '(') {
+      stack.push(')');
+    }
+    // If a close bracket is found, check that it matches the last stored open bracket
+    else if (stack.pop() !== s[idx]) {
+      return false;
+    }
+  }
+  return !stack.length;
 };
 export default isValid;

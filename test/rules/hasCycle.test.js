da83666b715b4dc24da0c0a516f1f52b3c02e1d1
Steven Gong <gonghaima@hotmail.com>
2024-08-12 08:56:17 +1000

    fast & slow pointers

--- test/rules/hasCycle.test.js
+++ test/rules/hasCycle.test.js
@@ -2,16 +2,16 @@ import hasCycle from '../../src/rules/hasCycle';
 import { inputData, expected } from '../data/hasCycleData';
 
 test('hasCycle should return a grouped Ids in case1', () => {
-  const result = hasCycle(inputData.case1);
+  const result = hasCycle(inputData.case1.head);
   expect(result).toEqual(expected.case1);
 });
 
 test('hasCycle should return a grouped Ids in case2', () => {
-  const result = hasCycle(inputData.case2);
+  const result = hasCycle(inputData.case2.head);
   expect(result).toEqual(expected.case2);
 });
 
 test('hasCycle should return a grouped Ids in case3', () => {
-  const result = hasCycle(inputData.case3);
+  const result = hasCycle(inputData.case3.head);
   expect(result).toEqual(expected.case3);
 });

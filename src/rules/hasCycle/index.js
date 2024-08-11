ad8d82d49faf8f115b5b1c396085882d71de5cea
Steven Gong <gonghaima@hotmail.com>
2024-08-11 18:40:57 +1000

    141. Linked List Cycle - initial setup

---
+++ src/rules/hasCycle/index.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    
+};
+
+export default hasCycle;

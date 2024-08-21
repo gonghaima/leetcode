8e5bfffdbf69d6a97f164aa31b5840f66d53b081
Steven Gong <gonghaima@hotmail.com>
2024-08-21 13:59:44 +1000

    dfs - v2

--- src/rules/mergeTwoSortedLists/dfs-v1.js
+++ src/rules/mergeTwoSortedLists/dfs-v1.js
@@ -15,14 +15,17 @@ function ListNode(val, next) {
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-  if (!l1 || !l2) return l1 || l2;
-  if (l1.val < l2.val) {
-    l1.next = mergeTwoLists(l1.next, l2);
-    return l1;
+var mergeTwoLists = function(list1, list2) {
+  if (!list1 || !list2) {
+    return list1 ? list1 : list2;
   }
-  l2.next = mergeTwoLists(l1, l2.next);
-  return l2;
+
+  if (list1.val > list2.val) {
+    [list1, list2] = [list2, list1];
+  }
+
+  list1.next = mergeTwoLists(list1.next, list2);
+  return list1;
 };
 
 module.exports = mergeTwoLists;

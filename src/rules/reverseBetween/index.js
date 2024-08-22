4107bfa34bfb489b803b24881d56c91cc4ce2a78
Steven Gong <gonghaima@hotmail.com>
2024-08-23 08:12:05 +1000

    two pointers

--- src/rules/reverseBetween/index.js
+++ src/rules/reverseBetween/index.js
@@ -5,6 +5,10 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
 /**
  * @param {ListNode} head
  * @param {number} left
@@ -12,7 +16,26 @@
  * @return {ListNode}
  */
 var reverseBetween = function (head, left, right) {
-  return { val: 999 };
+  if (!head || left === right) return head;
+
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let prev = dummy;
+
+  for (let i = 0; i < left - 1; ++i) {
+    prev = prev.next;
+  }
+
+  let current = prev.next;
+
+  for (let i = 0; i < right - left; ++i) {
+    const nextNode = current.next;
+    current.next = nextNode.next;
+    nextNode.next = prev.next;
+    prev.next = nextNode;
+  }
+
+  return dummy.next;
 };
 
 export default reverseBetween;

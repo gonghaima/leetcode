ce81e57766a636dc794b95a808d3fb27966a5e91
Steven Gong <gonghaima@hotmail.com>
2024-09-09 00:31:19 +1000

    dfs

--- src/rules/reverseKGroup/interate.js
+++ src/rules/reverseKGroup/interate.js
@@ -16,25 +16,26 @@ function ListNode(val, next) {
  * @param {number} k
  * @return {ListNode}
  */
-var reverseKGroup = function (head, k) {
-  let count = 0;
-  let node = head;
-  while (node && count != k) {
-    node = node.next;
-    count++;
-  }
-  if (count == k) {
-    node = reverseKGroup(node, k);
-    while (count > 0) {
-      let temp = head.next;
-      head.next = node;
-      node = head;
-      head = temp;
-      count--;
+var reverseKGroup = function(head, k) {
+  let stack = [];
+  let newNode = new ListNode(-1);
+  let temp = newNode;
+
+  while (head) {
+    for (let i = 0; i < k && head; i++) {
+      stack.push(head);
+      head = head.next;
     }
-    head = node;
+
+    if (stack.length === k) {
+      while (stack.length > 0) {
+        temp.next = stack.pop();
+        temp = temp.next;
+      }
+      temp.next = head;
+    }
   }
-  return head;
+  return newNode.next;
 };
 
 export default reverseKGroup;

bc456b9a36c806818b39cc25894af86182440c65
Steven Gong <gonghaima@hotmail.com>
2024-10-19 14:17:22 +1100

    82. Remove Duplicates from Sorted List II -  solution v1

--- src/rules/deleteDuplicatesII/index.js
+++ src/rules/deleteDuplicatesII/index.js
@@ -9,23 +9,23 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 var deleteDuplicatesII = function (head) {
-  const dummy = new ListNode();
-  dummy.next = head;
-  let node = dummy; // the last known distinct node
-  while (node.next) {
-    if (node.next.next && node.next.val === node.next.next.val) {
-      // if the next two nodes are equal...
-      let nonValNode = node.next.next.next;
-      while (nonValNode && nonValNode.val === node.next.val) {
-        // ...find the first one that isn't...
-        nonValNode = nonValNode.next;
-      }
-      node.next = nonValNode; // ...and glue it to the last known distinct node;...
-    } else {
-      node = node.next; // ...otherwise the next node is distinct
-    }
+  if (!head || !head.next) {
+    return head;
   }
-  return dummy.next;
+  let output = new ListNode(0, head),
+    pre = output;
+  while (head) {
+    if (head.next && head.val === head.next.val) {
+      while (head.next && head.val === head.next.val) {
+        head = head.next;
+      }
+      pre.next = head.next;
+    } else {
+      pre = pre.next;
+    }
+    head = head.next;
+  }
+  return output.next;
 };
 
 export default deleteDuplicatesII;

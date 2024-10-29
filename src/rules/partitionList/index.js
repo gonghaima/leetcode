254926a2cdcbfeb2a7507f26e7a99db3dc679a6d
Steven Gong <gonghaima@hotmail.com>
2024-10-30 08:50:08 +1100

    86. Partition List - two pointers

--- src/rules/partitionList/index.js
+++ src/rules/partitionList/index.js
@@ -10,6 +10,19 @@
  * @param {number} x
  * @return {ListNode}
  */
-var partition = function (head, x) {};
+var partition = function (head, x) {
+  let fdum = new ListNode(0),
+    bdum = new ListNode(0),
+    front = fdum,
+    back = bdum,
+    curr = head;
+  while (curr) {
+    if (curr.val < x) (front.next = curr), (front = curr);
+    else (back.next = curr), (back = curr);
+    curr = curr.next;
+  }
+  (front.next = bdum.next), (back.next = null);
+  return fdum.next;
+};
 
 export default partition;

16a8e53d3e285e5d7927f69d3629e0f2ad088832
Steven Gong <gonghaima@hotmail.com>
2024-11-24 00:36:30 +1100

    101. Symmetric Tree - simple solution

--- src/rules/binaryTree/isSymmetric/index.js
+++ src/rules/binaryTree/isSymmetric/index.js
@@ -1,27 +1,21 @@
 var isSymmetric = function (root) {
-  // Special case...
-  if (!root) return true;
-  // Return the function recursively...
-  return isSame(root.left, root.right);
-};
-// A tree is called symmetric if the left subtree must be a mirror reflection of the right subtree...
-var isSame = function (leftroot, rightroot) {
-  // If both root nodes are null pointers, return true...
-  // If exactly one of them is a null node, return false...
-  // If root nodes haven't same value, return false...
-  if (
-    (!leftroot && rightroot) ||
-    (leftroot && !rightroot) ||
-    (leftroot && rightroot && leftroot.val !== rightroot.val)
-  )
-    return false;
-  // Return true if the values of root nodes are same and left as well as right subtrees are symmetric...
-  if (leftroot && rightroot)
+  const isMirror = (n1, n2) => {
+    if (!n1 && !n2) {
+      return true;
+    }
+
+    if (!n1 || !n2) {
+      return false;
+    }
+
     return (
-      isSame(leftroot.left, rightroot.right) &&
-      isSame(leftroot.right, rightroot.left)
+      n1.val === n2.val &&
+      isMirror(n1.left, n2.right) &&
+      isMirror(n1.right, n2.left)
     );
-  return true;
+  };
+
+  return isMirror(root.left, root.right);
 };
 
 export { isSymmetric };
